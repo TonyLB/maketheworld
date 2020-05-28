42e962f60bcd4a1c2a08384cade05e182910d411
Anthony Lower-Basch <anthony.lowerbasch@gmail.com>
2020-05-28 12:59:35 -0400

    Lift and shift of getCharactersInPlay into lambda

--- lambda/ephemera/charactersInPlay/app.js
+++ lambda/ephemera/charactersInPlay/app.js
@@ -3,20 +3,14 @@
 
 const AWS = require('aws-sdk')
 
-exports.getCharactersInPlay = (event) => {
+const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: AWS_REGION })
+const { TABLE_PREFIX, AWS_REGION } = process.env;
+const ephemeraTable = `${TABLE_PREFIX}_characters_in_play`
 
-    const { TABLE_PREFIX, AWS_REGION } = process.env;
-    const ephemeraTable = `${TABLE_PREFIX}_ephemera`
+const getCharactersInPlay = () => {
 
-    const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: AWS_REGION })
-
-    return documentClient.query({
-            TableName: ephemeraTable,
-            KeyConditionExpression: 'DataCategory = :Connection',
-            ExpressionAttributeValues: {
-                ":Connection": 'Connection'
-            },
-            IndexName: "DataCategoryIndex"
+    return documentClient.scan({
+            TableName: ephemeraTable
         }).promise()
         .then(({ Items }) => (Items
             .filter(({ ConnectionId }) => (ConnectionId))
@@ -26,3 +20,14 @@ exports.getCharactersInPlay = (event) => {
 
 }
 
+exports.handler = (event) => {
+
+    const { action = 'NO-OP' } = event
+
+    switch(action) {
+        case 'getCharactersInPlay':
+            return getCharactersInPlay
+        default:
+            return { statusCode: 500, error: `Unknown handler key: ${action}`}
+    }
+}
