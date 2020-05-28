b07472cce8d0c5523361af3102088d3df05da096
Anthony Lower-Basch <anthony.lowerbasch@gmail.com>
2020-05-27 22:36:01 -0400

    Removed uses of _players DynamoDB table and refactored createBackup lambda

--- lambda/createBackup/app.js
+++ lambda/createBackup/app.js
@@ -16,205 +16,159 @@ const s3Put = (filename, contents) => {
 
 const shearOffFirstTag = (item) => (item.split('#').slice(1).join('#'))
 
-const serializeV1 = (Items) => (
-    Promise.resolve(Items)
-        //
-        // Recast old-style camelCase fields into new-style PascalCase fields
-        //
-        .then((Items) => (Items.map(({
-            ancestry,
-            parentId,
-            description,
-            name,
-            type,
-            permanentId,
-            fromRoomId,
-            ...rest
-        }) => ({
-            Ancestry: ancestry,
-            ParentId: parentId,
-            Description: description,
-            Name: name,
-            Type: type,
-            PermanentId: permanentId,
-            FromRoomId: fromRoomId,
-            ...rest
-        }))))
-        //
-        // Break out the neighborhood descriptions in a permanentId keyed map
-        //
-        .then((Items) => {
-            const Neighborhoods = Items.filter(({ Type }) => (Type === 'NEIGHBORHOOD'))
-                .reduce((previous, neighborhood) => ({
-                    ...previous,
-                    [neighborhood.PermanentId]: neighborhood
-                }), {})
-            return { Neighborhoods, Items }
-        })
-        //
-        // Break out the room descriptions in a permanentId keyed map and
-        // create Rooms lists in the neighborhood entries.
-        //
-        .then(({ Neighborhoods, Items, ...rest }) => {
-            const Rooms = Items.filter(({ Type }) => (Type === 'ROOM'))
-                .reduce((previous, room) => ({
-                    ...previous,
-                    [room.PermanentId]: {
-                        ...room,
-                        Entries: [],
-                        Exits: []
-                    }
-                }), {})
-            //
-            // Create an updated Neighborhood map
-            //
-            const newNeighborhoods = Object.values(Rooms)
-                .filter(({ ParentId }) => (ParentId && Neighborhoods[ParentId]))
-                .reduce((previous, room) => ({
-                    ...previous,
-                    [room.ParentId]: {
-                        ...previous[room.ParentId],
-                        Rooms: [
-                            ...(previous[room.ParentId].Rooms || []),
-                            room.PermanentId
-                        ]
-                    }
-                }), Neighborhoods)
-            return { Neighborhoods: newNeighborhoods, Rooms, Items, ...rest }
-        })
-        //
-        // Find all entries and denormalize them into both of the rooms they impact
-        //
-        .then(({ Items, Rooms, ...rest }) => {
-            const Entries = Items.filter(({ Type }) => (Type === 'ENTRY'))
-            const newRooms = Entries.filter(({ ParentId, FromRoomId }) => (ParentId && FromRoomId))
-                .reduce((previous, entry) => {
-                    const { ParentId: ToRoomId, FromRoomId } = entry
-                    const fromRoom = previous[FromRoomId] || {}
-                    const toRoom = previous[ToRoomId] || {}
-                    return {
-                        ...previous,
-                        ...(fromRoom ? {
-                            [FromRoomId]: {
-                                ...fromRoom,
-                                Exits: [
-                                    ...fromRoom.Exits,
-                                    entry
-                                ]
-                            }
-                        } : {}),
-                        ...(toRoom ? {
-                            [ToRoomId]: {
-                                ...toRoom,
-                                Entries: [
-                                    ...toRoom.Entries,
-                                    entry
-                                ]
-                            }
-                        } : {})
-                    }
-                }, Rooms)
-            return { Items, Rooms: newRooms, ...rest }
-        })
-)
+const serializeV2 = (Items) => {
 
-const serializeV2 = (Items) => (
-    Promise.resolve(Items)
-        //
-        // Break out the neighborhood descriptions in a permanentId keyed map
-        //
-        .then((Items) => {
-            const Neighborhoods = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('NEIGHBORHOOD#') && DataCategory === 'Details'))
-                .map(({ PermanentId, ...rest }) => ({ PermanentId: shearOffFirstTag(PermanentId), ...rest }))
-                .reduce((previous, { PermanentId, DataCategory, ProgenitorId, ...rest }) => ({
-                    ...previous,
-                    [PermanentId]: {
-                        PermanentId,
-                        ...rest
+    //
+    // Break out the neighborhood descriptions in a permanentId keyed map
+    //
+
+    const Neighborhoods = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('NEIGHBORHOOD#') && DataCategory === 'Details'))
+        .map(({ PermanentId, ...rest }) => ({ PermanentId: shearOffFirstTag(PermanentId), ...rest }))
+        .reduce((previous, { PermanentId, DataCategory, ProgenitorId, ...rest }) => ({
+            ...previous,
+            [PermanentId]: {
+                PermanentId,
+                ...rest
+            }
+        }), {})
+
+    //
+    // Break out the room descriptions in a permanentId keyed map and
+    // create Rooms lists in the neighborhood entries.
+    //
+
+    const Rooms = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('ROOM#') && DataCategory === 'Details'))
+        .map(({ PermanentId, ...rest }) => ({ PermanentId: shearOffFirstTag(PermanentId), ...rest }))
+        .reduce((previous, { PermanentId, DataCategory, ...rest }) => ({
+            ...previous,
+            [PermanentId]: {
+                PermanentId,
+                ...rest,
+                Entries: [],
+                Exits: []
+            }
+        }), {})
+
+    //
+    // Create an updated Neighborhood map with rooms included
+    //
+
+    const newNeighborhoods = Object.values(Rooms)
+        .filter(({ ParentId }) => (ParentId && Neighborhoods[ParentId]))
+        .reduce((previous, room) => ({
+            ...previous,
+            [room.ParentId]: {
+                ...previous[room.ParentId],
+                Rooms: [
+                    ...(previous[room.ParentId].Rooms || []),
+                    room.PermanentId
+                ]
+            }
+        }), Neighborhoods)
+
+    //
+    // Find all entries and exits
+    //
+
+    const Entries = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('ROOM#') && (DataCategory.startsWith('ENTRY#') || DataCategory.startsWith('EXIT#'))))
+    const newRooms = Entries.map(({ PermanentId, DataCategory, ...rest }) => ({
+            ParentId: shearOffFirstTag(PermanentId),
+            RoomId: shearOffFirstTag(DataCategory),
+            Type: DataCategory.split('#')[0],
+            ...rest
+        }))
+        .filter(({ ParentId, RoomId }) => (ParentId && RoomId))
+        .reduce((previous, entry) => {
+            const { ParentId, Type, ProgenitorId, Ancestry, RoomId, ...rest } = entry
+            const toRoom = previous[ParentId] || {}
+            return {
+                ...previous,
+                ...(toRoom ? {
+                    [toRoom.PermanentId]: {
+                        ...toRoom,
+                        ...(Type === 'ENTRY'
+                            ? { Entries: [
+                                ...toRoom.Entries,
+                                {
+                                    ParentId,
+                                    FromRoomId: RoomId,
+                                    ...rest
+                                }
+                            ]}
+                            : Type === 'EXIT'
+                                ? { Exits: [
+                                    ...toRoom.Exits,
+                                    {
+                                        ParentId: RoomId,
+                                        FromRoomId: ParentId,
+                                        ...rest
+                                    }
+                                ]}
+                                : {})
                     }
-                }), {})
-            return { Neighborhoods, Items }
-        })
-        //
-        // Break out the room descriptions in a permanentId keyed map and
-        // create Rooms lists in the neighborhood entries.
-        //
-        .then(({ Neighborhoods, Items, ...rest }) => {
-            const Rooms = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('ROOM#') && DataCategory === 'Details'))
-                .map(({ PermanentId, ...rest }) => ({ PermanentId: shearOffFirstTag(PermanentId), ...rest }))
-                .reduce((previous, { PermanentId, DataCategory, ProgenitorId, ...rest }) => ({
-                    ...previous,
-                    [PermanentId]: {
-                        PermanentId,
-                        ...rest,
-                        Entries: [],
-                        Exits: []
-                    }
-                }), {})
-            //
-            // Create an updated Neighborhood map
-            //
-            const newNeighborhoods = Object.values(Rooms)
-                .filter(({ ParentId }) => (ParentId && Neighborhoods[ParentId]))
-                .reduce((previous, room) => ({
-                    ...previous,
-                    [room.ParentId]: {
-                        ...previous[room.ParentId],
-                        Rooms: [
-                            ...(previous[room.ParentId].Rooms || []),
-                            room.PermanentId
-                        ]
-                    }
-                }), Neighborhoods)
-            return { Neighborhoods: newNeighborhoods, Rooms, Items, ...rest }
-        })
-        //
-        // Find all entries and exits
-        //
-        .then(({ Items, Rooms, ...rest }) => {
-            const Entries = Items.filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('ROOM#') && (DataCategory.startsWith('ENTRY#') || DataCategory.startsWith('EXIT#'))))
-            const newRooms = Entries.map(({ PermanentId, DataCategory, ...rest }) => ({
-                    ParentId: shearOffFirstTag(PermanentId),
-                    RoomId: shearOffFirstTag(DataCategory),
-                    Type: DataCategory.split('#')[0],
-                    ...rest
-                }))
-                .filter(({ ParentId, RoomId }) => (ParentId && RoomId))
-                .reduce((previous, entry) => {
-                    const { ParentId, Type, ProgenitorId, Ancestry, RoomId, ...rest } = entry
-                    const toRoom = previous[ParentId] || {}
-                    return {
-                        ...previous,
-                        ...(toRoom ? {
-                            [toRoom.PermanentId]: {
-                                ...toRoom,
-                                ...(Type === 'ENTRY'
-                                    ? { Entries: [
-                                        ...toRoom.Entries,
-                                        {
-                                            ParentId,
-                                            FromRoomId: RoomId,
-                                            ...rest
-                                        }
-                                    ]}
-                                    : Type === 'EXIT'
-                                        ? { Exits: [
-                                            ...toRoom.Exits,
-                                            {
-                                                ParentId: RoomId,
-                                                FromRoomId: ParentId,
-                                                ...rest
-                                            }
-                                        ]}
-                                        : {})
-                            }
-                        } : {})
-                    }
-                }, Rooms)
-            return { Items, Rooms: newRooms, ...rest }
-        })
-)
+                } : {})
+            }
+        }, Rooms)
+
+    //
+    // Break out the players and create Player and Characters lists
+    //
+
+    const playerDetails = Items
+        .filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('PLAYER#') && (DataCategory === 'Details')))
+        .map(({ PermanentId, DataCategory, ...rest }) => ({
+            PlayerName: shearOffFirstTag(PermanentId),
+            ...rest
+        }))
+        .reduce((previous, { PlayerName, ...rest }) => ({ ...previous, [PlayerName]: { PlayerName, ...rest } }), {})
+
+    //
+    // Break out the characters
+    //
+    const Characters = Items
+        .filter(({ PermanentId, DataCategory }) => (PermanentId.startsWith('CHARACTER#') && (DataCategory === 'Details')))
+        .map(({ PermanentId, DataCategory, ...rest }) => ({
+            CharacterId: shearOffFirstTag(PermanentId),
+            ...rest
+        }))
+        .reduce((previous, { CharacterId, ...rest }) => ({ ...previous, [CharacterId]: { CharacterId, ...rest } }), {})
+
+    //
+    // Nest each character under its player
+    //
+
+    const Players = Items
+        .filter(({ DataCategory }) => (DataCategory.startsWith('CHARACTER#')))
+        .map(({ PermanentId, DataCategory }) => ({
+            PlayerName: shearOffFirstTag(PermanentId),
+            CharacterId: shearOffFirstTag(DataCategory)
+        }))
+        .reduce((previous, { PlayerName, CharacterId }) => ({
+            ...previous,
+            [PlayerName]: {
+                ...(previous[PlayerName] || {}),
+                Characters: [
+                    ...((previous[PlayerName] || {}).Characters || []),
+                    ...(Characters[CharacterId] ? [Characters[CharacterId]] : [])
+                ]
+            }
+        }), playerDetails)
+
+    return {
+        Neighborhoods: Object.values(newNeighborhoods),
+        Rooms: Object.values(newRooms),
+        Players: Object.values(Players)
+    }
+
+}
+
 exports.handler = (event) => {
+
+    const { objectName } = event
+
+    if (!objectName) {
+        return { statusCode: 500, body: 'No objectName provided'}
+    }
 
     const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION })
 
@@ -222,58 +176,13 @@ exports.handler = (event) => {
         .promise()
         .then(({ Items }) => (Items))
         .then(serializeV2)
-        .then(({ Neighborhoods, Rooms }) => (
-            documentClient.scan({
-                TableName: `${process.env.TABLE_PREFIX}_players`
-            }).promise()
-            .then(({ Items }) => ({ Items, Players: {} }))
-            //
-            // Break out the player records in a PlayerName keyed map
-            //
-            .then(({ Items, Players }) => ({
-                Items,
-                Players: {
-                    ...Players,
-                    ...(Items.filter(({ DataCategory }) => (DataCategory === 'Details'))
-                        .map(({ PlayerName, DataCategory, ...rest }) => ({
-                            PlayerName,
-                            ...rest
-                        }))
-                        .reduce((previous, player) => ({
-                            ...previous,
-                            [player.PlayerName]: player
-                        }), {})
-                    )
-                }
-            }))
-            //
-            // Nest each character under its player
-            //
-            .then(({ Items, Players }) => ({
-                Items,
-                Players: Items.filter(({ DataCategory }) => (DataCategory.startsWith('Character#')))
-                    .reduce((previous, { PlayerName, DataCategory, CharacterId, ...rest }) => ({
-                        ...previous,
-                        [PlayerName]: {
-                            ...(previous[PlayerName] || {}),
-                            Characters: [
-                                ...((previous[PlayerName] || {}).Characters || []),
-                                {
-                                    CharacterId: shearOffFirstTag(DataCategory),
-                                    ...rest
-                                }
-                            ]
-                        }
-                    }), Players)
-            }))
-            .then(({ Players }) => ({ Neighborhoods, Rooms, Players }))
-        ))
-        .then(({ Neighborhoods, Rooms, Players }) => (s3Put('test.json', JSON.stringify({
-            version: '2020-05-02',
+        .then(({ Neighborhoods, Rooms, Players }) => (s3Put(objectName, JSON.stringify({
+            version: '2020-05-27',
             Neighborhoods,
             Rooms,
             Players
         }, null, 4))))
         .then(() => ({ statusCode: 200, body: 'Backup complete.' }))
         .catch((err) => ({ statusCode: 500, body: err.stack }))
+
 }
