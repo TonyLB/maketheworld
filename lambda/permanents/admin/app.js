4da2d71c56c156c1ccd83b91dd82cc2d6d0f58bb
Anthony Lower-Basch <anthony.lowerbasch@gmail.com>
2020-05-29 07:50:12 -0400

    Created appsync functions for backup meta data

--- lambda/permanents/admin/app.js
+++ lambda/permanents/admin/app.js
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT-0
 
 const AWS = require('aws-sdk')
+const { v4: uuidv4 } = require('/opt/uuid')
 
 const { TABLE_PREFIX, AWS_REGION } = process.env;
 const permanentTable = `${TABLE_PREFIX}_permanents`
@@ -35,12 +36,83 @@ const putSettings = (payload) => {
 
 }
 
+//
+// TODO:  (1) Create handlers for storing backup meta-data in the permanents database
+// under the Admin heading.
+//
+//   (2) Create a REST API that can accept uploads and serve downloads from the
+// StorageBucket S3 area, in order to give people client-based ability to download
+// and upload backup files.
+//
+const getBackups = async () => {
+    const { Items = [] } = await documentClient.query({
+            TableName: permanentTable,
+            KeyConditionExpression: 'PermanentId = :Admin and begins_with(DataCategory, :Backup)',
+            ExpressionAttributeValues: {
+                ":Admin": "ADMIN",
+                ":Backup": "BACKUP#"
+            }
+        }).promise()
+
+    return Items
+        .map(({ DataCategory = '', Name, Description, Status }) => ({
+            PermanentId: DataCategory.split('#').slice(1).join('#'),
+            Name,
+            Description,
+            Status
+        }))
+}
+
+const getBackup = async ({ PermanentId }) => {
+    const { Item = {} } = await documentClient.get({
+            TableName: permanentTable,
+            Key: {
+                PermanentId: 'ADMIN',
+                DataCategory: `BACKUP#${PermanentId}`
+            }
+        }).promise()
+
+    const { DataCategory = '', Name, Description, Status } = Item
+    return {
+        PermanentId: DataCategory.split('#').slice(1).join('#'),
+        Name,
+        Description,
+        Status
+    }
+}
+
+const putBackup = async ({ PermanentId, Name, Description, Status }) => {
+    const oldBackup = PermanentId ? await getBackup({ PermanentId }) : {}
+    const newBackup = {
+        ...oldBackup,
+        PermanentId: PermanentId || uuidv4(),
+        ...(Name !== undefined ? { Name } : {}),
+        ...(Description !== undefined ? { Description } : {}),
+        ...(Status !== undefined ? { Status } : {}),
+    }
+
+    console.log(newBackup)
+    await documentClient.put({
+        TableName: permanentTable,
+        Item: {
+            ...newBackup,
+            PermanentId: `ADMIN`,
+            DataCategory: `BACKUP#${newBackup.PermanentId}`
+        }
+    }).promise()
+    return [{ Backup: newBackup }]
+}
+
 exports.handler = (event) => {
     const { action, ...payload } = event
 
     switch(action) {
         case "getSettings":
             return getSettings()
+        case "getBackups":
+            return getBackups()
+        case "putBackup":
+            return putBackup(payload)
         case "putSettings":
             return putSettings(payload)
         default:
