7fe3dd9024f099a5dd8fda45a2f02e074e4cf2f4
Anthony Lower-Basch <anthony.lowerbasch@gmail.com>
2020-06-18 20:03:22 -0400

    Created a controlChannel websocket thread to manage tracking disconnected characters

--- charcoal-client/src/actions/webSocket.js
+++ charcoal-client/src/actions/webSocket.js
@@ -1,12 +1,67 @@
+import { WSS_ADDRESS } from '../config'
+import { getWebSocket } from '../selectors/webSocket'
+
+export const ESTABLISH_WEB_SOCKET_ATTEMPT = 'ESTABLISH_WEB_SOCKET_ATTEMPT'
+export const ESTABLISH_WEB_SOCKET_ERROR = 'ESTABLISH_WEB_SOCKET_ERROR'
 export const REGISTER_WEB_SOCKET = 'REGISTER_WEB_SOCKET'
 
-export const registerWebSocket = (webSocket) => {
-    return {
-        type: REGISTER_WEB_SOCKET,
-        payload: {
-            webSocket
+const establishWebSocketAttempt = {
+    type: ESTABLISH_WEB_SOCKET_ATTEMPT
+}
+
+const establishWebSocketError = {
+    type: ESTABLISH_WEB_SOCKET_ERROR
+}
+
+export const establishWebSocket = (CharacterId) => (dispatch) => {
+
+    dispatch(establishWebSocketAttempt)
+    console.log(`Establishing Web Socket (Character Id: ${CharacterId}`)
+
+    let setupSocket = new WebSocket(WSS_ADDRESS)
+    setupSocket.onopen = () => {
+        console.log(`Socket open`)
+        setupSocket.send(JSON.stringify({
+            message: 'registercharacter',
+            CharacterId
+        }))
+    }
+    setupSocket.onmessage = (event) => {
+        console.log(`Event: ${JSON.stringify(event, null, 4)}`)
+        const { message } = JSON.parse(event.data || {})
+        switch(message) {
+            case 'Registered':
+                console.log(`Registration message received`)
+                dispatch(registerWebSocket({ webSocket: setupSocket, CharacterId }))
+                break
+            default:
+                break
         }
     }
+    setupSocket.onerror = (event) => {
+        console.log(`WebSocket Error: ${JSON.stringify(event, null, 4)}`)
+        dispatch(establishWebSocketError)
+    }
+}
+
+export const registerWebSocket = ({ webSocket, CharacterId }) => (dispatch, getState) => {
+    const { webSocket: oldWebSocket, pingInterval: oldPingInterval } = getWebSocket(getState()) || {}
+    if (oldPingInterval) {
+        clearInterval(oldPingInterval)
+    }
+    if (oldWebSocket) {
+        oldWebSocket.close()
+    }
+    const pingInterval = setInterval(() => { webSocket.send(JSON.stringify({ message: 'ping'}))}, 300000)
+    const refreshTimeout = setTimeout(() => { dispatch(establishWebSocket(CharacterId)) }, 3600000 )
+    dispatch({
+        type: REGISTER_WEB_SOCKET,
+        payload: {
+            webSocket,
+            pingInterval,
+            refreshTimeout
+        }
+    })
 }
 
 //
