70aea3e82e9fe1adf87e1457bdc40e529813ad0f
TonyLB <anthony.lowerbasch@gmail.com>
2020-07-05 18:01:16 -0400

    Removed obsolete separate command channel lambdas

--- lambda/onconnect/app.js
+++
@@ -1,6 +0,0 @@
-// Copyright 2020, Tony Lower-Basch. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
-exports.handler = () => {
-  return Promise.resolve({ statusCode: 200, body: 'Connected.' })
-}
