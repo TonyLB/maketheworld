c490f595f34e547bff7affe88470f4408520b68c
TonyLB <anthony.lowerbasch@gmail.com>
2021-01-10 17:46:32 -0500

    Added skeletal state transitions to activeCharacters reducer

--- charcoal-client/src/reducers/UI/activeCharacters.js
+++ charcoal-client/src/reducers/UI/activeCharacters.js
@@ -1,7 +1,19 @@
 import {
     ACTIVATE_CHARACTER,
     DEACTIVATE_CHARACTER,
-    ACTIVE_CHARACTER_FSM_INITIAL
+    ACTIVE_CHARACTER_FSM_INITIAL,
+    ACTIVE_CHARACTER_SUBSCRIBE_ATTEMPT,
+    ACTIVE_CHARACTER_FSM_SUBSCRIBING,
+    ACTIVE_CHARACTER_SUBSCRIBE_SUCCESS,
+    ACTIVE_CHARACTER_FSM_SUBSCRIBED,
+    ACTIVE_CHARACTER_SUBSCRIBE_FAIL,
+    ACTIVE_CHARACTER_CONNECT_ATTEMPT,
+    ACTIVE_CHARACTER_FSM_CONNECTING,
+    ACTIVE_CHARACTER_CONNECT_SUCCESS,
+    ACTIVE_CHARACTER_FSM_CONNECTED,
+    ACTIVE_CHARACTER_CONNECT_FAIL,
+    ACTIVE_CHARACTER_RECONNECT_ATTEMPT,
+    ACTIVE_CHARACTER_FSM_RECONNECTING
 } from '../../actions/UI/activeCharacters'
 
 export const reducer = (state = {}, action = {}) => {
@@ -9,6 +21,23 @@ export const reducer = (state = {}, action = {}) => {
         type,
         CharacterId = ''
     } = action
+
+    const transitionHelper = (endState, props = {}) => {
+        if (CharacterId && state[CharacterId]) {
+            return {
+                ...state,
+                [CharacterId]: {
+                    ...state[CharacterId],
+                    state: endState,
+                    ...props
+                }
+            }
+        }
+        else {
+            return state
+        }
+
+    }
 
     switch (type) {
 
@@ -35,6 +64,27 @@ export const reducer = (state = {}, action = {}) => {
             else {
                 return state
             }
+
+        case ACTIVE_CHARACTER_SUBSCRIBE_ATTEMPT:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_SUBSCRIBING)
+
+        case ACTIVE_CHARACTER_SUBSCRIBE_SUCCESS:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_SUBSCRIBED, { subscriptions: { messages: action.subscription } })
+
+        case ACTIVE_CHARACTER_SUBSCRIBE_FAIL:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_INITIAL)
+
+        case ACTIVE_CHARACTER_CONNECT_ATTEMPT:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_CONNECTING)
+
+        case ACTIVE_CHARACTER_CONNECT_SUCCESS:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_CONNECTED, { connection: action.connection })
+
+        case ACTIVE_CHARACTER_CONNECT_FAIL:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_SUBSCRIBED)
+
+        case ACTIVE_CHARACTER_RECONNECT_ATTEMPT:
+            return transitionHelper(ACTIVE_CHARACTER_FSM_RECONNECTING)
 
         default:
             return state
