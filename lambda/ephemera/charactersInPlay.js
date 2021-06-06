d5d6e132333bc48ea541ac397aaf7741249f2622
TonyLB <anthony.lowerbasch@gmail.com>
2021-06-05 22:30:34 -0400

    Ephemera put denormalizes Character values as default if needed

--- lambda/ephemera/charactersInPlay.js
+++ lambda/ephemera/charactersInPlay.js
@@ -80,34 +80,48 @@ const putCharacterInPlay = async ({ CharacterId, RoomId, Connected, ConnectionId
         ReturnValues: 'ALL_NEW'
     }))
     let newRecord = unmarshall(Attributes)
-    if (!newRecord.RoomId) {
+    //
+    // Check that the existing ephemera record has all data that should be populated in an
+    // ephemera record.  If not, go to the Permanents table to fetch the data that needs to
+    // be denormalized.
+    //
+    const requiredFields = ['RoomId', 'Name']
+    const optionalFields = ['FirstImpression', 'Pronouns', 'Outfit', 'OneCoolThing']
+    const allFields = [...requiredFields, ...optionalFields]
+    if (
+        requiredFields.filter((key) => (!newRecord[key])) ||
+        optionalFields.filter((key) => (newRecord[key] === undefined))
+    ) {
         const { Item } = await dbClient.send(new GetItemCommand({
             TableName: permanentsTable,
             Key: marshall({
                 PermanentId: `CHARACTER#${CharacterId}`,
                 DataCategory: 'Details'
             }),
-            ProjectionExpression: "RoomId"
+            ProjectionExpression: allFields.map((key) => (key === 'Name' ? '#name' : key)).join(', '),
+            ExpressionAttributeNames: { '#name': 'Name' }
         }))
-        const { RoomId = 'VORTEX' } = unmarshall(Item)
+        const fromCharacterPermanent = unmarshall(Item)
+        const newExpressionAttributes = Object.assign({}, ...(allFields.filter((key) => fromCharacterPermanent[key]).map((key) => ({ [`:${key}`]: fromCharacterPermanent[key] }))))
+        const newUpdateExpression = allFields.filter((key) => fromCharacterPermanent[key]).map((key) => (`${key === 'Name' ? '#name' : key} = :${key}`)).join(', ')
         const { Attributes: newAttributes } = await dbClient.send(new UpdateItemCommand({
             TableName: ephemeraTable,
             Key: marshall({
                 EphemeraId,
                 DataCategory: 'Connection'
             }),
-            UpdateExpression: 'SET RoomId = :RoomId',
-            ExpressionAttributeValues: marshall({
-                ':RoomId': RoomId
-            }),
+            UpdateExpression: `SET ${newUpdateExpression}`,
+            ExpressionAttributeValues: marshall(newExpressionAttributes),
+            ExpressionAttributeNames: { '#name': 'Name' },
             ReturnValues: 'ALL_NEW'
         }))
         newRecord = unmarshall(newAttributes)
     }
+    const remap = Object.assign({}, ...(allFields.map((key) => ({ [key]: newRecord[key] }))))
     return [ { CharacterInPlay: {
         CharacterId,
-        RoomId: newRecord.RoomId,
-        Connected: newRecord.Connected
+        Connected: newRecord.Connected,
+        ...remap
     }}];
 }
 
