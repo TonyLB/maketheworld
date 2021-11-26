72a55bb667652ba1d519fb846dbc65600c672e33
TonyLB <anthony.lowerbasch@gmail.com>
2021-11-26 13:39:03 -0500

    Added mergeRooms processing stage

--- lambda/assetHandler/wml/index.js
+++ lambda/assetHandler/wml/index.js
@@ -46,6 +46,91 @@ const flattenToElements = (includeFunction) => (node) => {
     )
 }
 
+const mergeToRooms = (elements) => {
+    const shallowEqual = (listA, listB) => {
+        if (listA.length !== listB.length) {
+            return false
+        }
+        return !listA.find((condition, index) => (listB[index] !== condition))
+    }
+    //
+    // TODO: Consider replacing render and name lists with DRY elements that manage the
+    // complications of conditional lists internally
+    //
+    const reduceInRoomContext = ({ render: previousRender = [], exitsByConditionList: previousExits = [], name: previousName = [] }, element) => {
+        const { to, display = 'replace', conditions = [], render = "", name = "" } = element
+        switch(element.tag) {
+            case 'Room':
+                //
+                // TODO: Handle modes of display other than replace
+                //
+
+                //
+                // TODO: Build more sophisticated merge functionality that can tell when
+                // two elements can be combined (e.g. their conditions are identical)
+                //
+                return {
+                    render: [
+                        ...previousRender,
+                        ...(render ? [{ conditions, display, render }] : [])
+                    ],
+                    name: [
+                        ...previousName,
+                        ...(name ? [{ conditions, display, name }] : [])
+                    ],
+                    exitsByConditionList: previousExits
+                }
+            case 'Exit':
+                const matchedExitIndex = previousExits.findIndex(({ conditions: probeConditions }) => (shallowEqual(conditions, probeConditions)))
+                if (matchedExitIndex !== -1) {
+                    const matchedExitConditional = previousExits[matchedExitIndex]
+                    return {
+                        render: previousRender,
+                        name: previousName,
+                        exitsByConditionList: [
+                            {
+                                ...matchedExitConditional,
+                                exits: [
+                                    ...matchedExitConditional.exits,
+                                    {
+                                        to
+                                    }
+                                ]
+                            },
+                            ...previousExits.slice(0, matchedExitIndex),
+                            ...previousExits.slice(matchedExitIndex + 1)
+                        ]
+                    }
+                }
+                else {
+                    return {
+                        render: previousRender,
+                        name: previousName,
+                        exitsByConditionList: [
+                            {
+                                conditions,
+                                exits: [{ to }]
+                            },
+                            ...previousExits
+                        ]
+                    }
+                }
+            default:
+                return { render: previousRender, exitsByConditionList: previousExits, name: previousName }
+        }
+    }
+    const roomsById = elements.reduce(
+        (previous, element) => {
+            const roomId = (element.tag === 'Room' && element.key) || (element.tag === 'Exit' && element.from)
+            return {
+                ...previous,
+                [roomId]: reduceInRoomContext(previous[roomId] || { render: [], exitsByToAndFrom: [], name: [] }, element)
+            }
+        }, {}
+    )
+    return roomsById
+}
+
 const dbEntries = (match) => {
     const firstPass = wmlSemantics(match).schema()
     const secondPass = wmlProcessDown([
@@ -61,7 +146,7 @@ const dbEntries = (match) => {
             aggregateErrors
         ])(secondPass)
     const dbSchema = flattenToElements(tagCondition(['Room', 'Exit']))(thirdPass)
-    return dbSchema
+    return mergeToRooms(dbSchema)
 }
 
 exports.wmlGrammar = wmlGrammar
