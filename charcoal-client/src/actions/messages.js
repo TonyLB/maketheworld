29bc706a4374dfc3acf1c5cd9a785b5630aeb34a
TonyLB <anthony.lowerbasch@gmail.com>
2021-12-07 21:51:52 -0500

    Removed AppSync

--- charcoal-client/src/actions/messages.js
+++ charcoal-client/src/actions/messages.js
@@ -1,4 +1,4 @@
-import { syncMessages as syncMessagesGQL } from '../graphql/queries'
+// import { syncMessages as syncMessagesGQL } from '../graphql/queries'
 import { v4 as uuidv4 } from 'uuid'
 import { socketDispatch } from './communicationsLayer/lifeLine'
 
