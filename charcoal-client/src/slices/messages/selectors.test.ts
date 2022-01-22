d383b9d0601b1331c45f5dce4a8b3104a0099b0e
TonyLB <anthony.lowerbasch@gmail.com>
2022-01-22 10:12:24 -0500

    ISS66: Added getMessagesByRoom selector

--- charcoal-client/src/slices/messages/selectors.test.ts
+++ charcoal-client/src/slices/messages/selectors.test.ts
@@ -2,7 +2,8 @@ import { MessageState } from './baseClasses'
 import { RootState } from '../../store'
 
 import {
-    getMessages
+    getMessages,
+    getMessagesByRoom
 } from './selectors'
 
 const testState = {
@@ -82,6 +83,111 @@ describe('messages selectors', () => {
                 CreatedTime: 1,
                 Target: 'TESS'
             }]]])
+        })
+    })
+
+    describe('getMessagesByRoom', () => {
+
+        const testState = {
+            messages: {
+                TESS: [{
+                    DisplayProtocol: 'RoomHeader',
+                    MessageId: 'Test1',
+                    CreatedTime: 1,
+                    Target: 'TESS',
+                    RoomId: 'TEST',
+                    Description: 'Test1',
+                    Name: 'Test1',
+                    Exits: [],
+                    RoomCharacters: []
+                }, {
+                    DisplayProtocol: 'World',
+                    MessageId: 'Test2',
+                    Message: 'Test2',
+                    CreatedTime: 2,
+                    Target: 'TESS'
+                }, {
+                    DisplayProtocol: 'World',
+                    MessageId: 'Test3',
+                    Message: 'Test3',
+                    CreatedTime: 3,
+                    Target: 'TESS'
+                }, {
+                    DisplayProtocol: 'RoomHeader',
+                    MessageId: 'Test4',
+                    CreatedTime: 4,
+                    Target: 'TESS',
+                    RoomId: 'TEST4',
+                    Description: 'Test4',
+                    Name: 'Test4',
+                    Exits: [],
+                    RoomCharacters: []
+                }, {
+                    DisplayProtocol: 'World',
+                    MessageId: 'Test5',
+                    Message: 'Test5',
+                    CreatedTime: 5,
+                    Target: 'TESS'
+                }]
+            } as MessageState
+        } as unknown as RootState
+
+        it('should return empty when no messages exist', () => {
+            expect(getMessagesByRoom('SAIONJI')(testState)).toEqual({
+                Messages: [],
+                Groups: []
+            })
+        })
+
+        it('should return groups when all groups have headers', () => {
+            expect(getMessagesByRoom('TESS')(testState)).toEqual({
+                Messages: [{
+                        DisplayProtocol: 'World',
+                        MessageId: 'Test2',
+                        Message: 'Test2',
+                        CreatedTime: 2,
+                        Target: 'TESS'
+                    }, {
+                        DisplayProtocol: 'World',
+                        MessageId: 'Test3',
+                        Message: 'Test3',
+                        CreatedTime: 3,
+                        Target: 'TESS'
+                    }, {
+                        DisplayProtocol: 'World',
+                        MessageId: 'Test5',
+                        Message: 'Test5',
+                        CreatedTime: 5,
+                        Target: 'TESS'
+                }],
+                Groups: [{
+                        header: {
+                            DisplayProtocol: 'RoomHeader',
+                            MessageId: 'Test1',
+                            CreatedTime: 1,
+                            Target: 'TESS',
+                            RoomId: 'TEST',
+                            Description: 'Test1',
+                            Name: 'Test1',
+                            Exits: [],
+                            RoomCharacters: []
+                        },
+                        messageCount: 2
+                    }, {
+                        header: {
+                            DisplayProtocol: 'RoomHeader',
+                            MessageId: 'Test4',
+                            CreatedTime: 4,
+                            Target: 'TESS',
+                            RoomId: 'TEST4',
+                            Description: 'Test4',
+                            Name: 'Test4',
+                            Exits: [],
+                            RoomCharacters: []
+                        },
+                        messageCount: 1
+                }]
+            })
         })
     })
 
--- charcoal-client/src/slices/messages/selectors.ts
+++ charcoal-client/src/slices/messages/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit'
 
-import { Message, MessageState } from './baseClasses'
+import { Message, MessageState, RoomHeader } from './baseClasses'
 import { Selector } from '../../store'
 
 
@@ -22,3 +22,102 @@ export const getMessages: Selector<MessageState> = (state) => {
     })
 
 }
+
+type MessageRoomBreakdownHeader = {
+    header: RoomHeader;
+    messageCount: number;
+}
+
+export type MessageRoomBreakdown = {
+    Messages: Message[];
+    Groups: MessageRoomBreakdownHeader[];
+}
+
+type MessageRoomInProgress = {
+    Messages: Message[];
+    Groups: MessageRoomBreakdownHeader[];
+    currentGroup: MessageRoomBreakdownHeader;
+}
+
+export const getMessagesByRoom: (CharacterId: string) => Selector<MessageRoomBreakdown> = (CharacterId) => createSelector(
+    getMessages,
+    (allMessages) => {
+        let messages = [] as Message[]
+        let initialHeader = undefined as MessageRoomBreakdownHeader | undefined
+        const probeMessages = allMessages[CharacterId]
+        if (!probeMessages.length) {
+            return {
+                Messages: [],
+                Groups: []
+            }
+        }
+        if (probeMessages[0].DisplayProtocol === 'RoomHeader') {
+            initialHeader = {
+                header: probeMessages[0],
+                messageCount: 0
+            }
+            messages = probeMessages.slice(1)
+        }
+        else {
+            initialHeader = {
+                header: {
+                    DisplayProtocol: 'RoomHeader',
+                    MessageId: 'NONE',
+                    Target: CharacterId,
+                    RoomId: 'NONE',
+                    CreatedTime: probeMessages[0].CreatedTime,
+                    Name: 'Unknown',
+                    Description: '??????',
+                    Exits: [],
+                    RoomCharacters: []
+                },
+                messageCount: 0
+            }
+            messages = probeMessages
+        }
+        const { Messages, Groups, currentGroup }: MessageRoomInProgress = messages.reduce<MessageRoomInProgress>((previous, message) => {
+                switch(message.DisplayProtocol) {
+                    case 'RoomHeader':
+                        return {
+                            Messages: previous.Messages,
+                            Groups: [
+                                ...previous.Groups,
+                                previous.currentGroup
+                            ],
+                            currentGroup: {
+                                header: message,
+                                messageCount: 0
+                            }
+                        }
+                    default:
+                        return {
+                            Messages: [
+                                ...previous.Messages,
+                                message
+                            ],
+                            Groups: previous.Groups,
+                            currentGroup: {
+                                header: previous.currentGroup.header,
+                                messageCount: previous.currentGroup.messageCount + 1
+                            }
+                        }
+                }
+            }, {
+                Messages: [],
+                Groups: [],
+                currentGroup: initialHeader
+            } as MessageRoomInProgress)
+        if (currentGroup.messageCount > 0) {
+            return {
+                Messages,
+                Groups: [...Groups, currentGroup]
+            }
+        }
+        else {
+            return {
+                Messages,
+                Groups
+            }
+        }
+    }
+)
