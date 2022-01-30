55fd012ac074653b1b732c24094c8d6bde035933
TonyLB <anthony.lowerbasch@gmail.com>
2022-01-30 11:26:19 -0500

    ISS207: Created first-draft moveAsset function

---
+++ lambda/assets/moveAsset/index.js
@@ -0,0 +1,77 @@
+import { CopyObjectCommand, DeleteObjectCommand, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
+
+import { streamToString } from '../utilities/stream.js'
+import { wmlGrammar, validatedSchema, assetRegistryEntries } from "../wml/index.js"
+import { wmlQueryFactory } from '../wml/wmlQuery/index.js'
+
+const { TABLE_PREFIX, S3_BUCKET } = process.env;
+
+export const moveAsset = ({ s3Client, dbClient }) => async ({ fromPath, fileName, toPath }) => {
+    // try {
+
+        //
+        // TODO: Right now this is repeating a lot of s3Assets getAssets code.
+        // See what can be done to reduce that repetition and abstract the right bits
+        //
+        const { Body: contentStream } = await s3Client.send(new GetObjectCommand({
+            Bucket: S3_BUCKET,
+            Key: `${fromPath}${fileName}.wml`
+        }))
+        const contents = await streamToString(contentStream)
+        //
+        // TODO: Error-handling in case the files have become corrupt
+        //
+        const match = wmlGrammar.match(contents)    
+        if (!match.succeeded()) {
+            console.log('ERROR: Schema failed validation')
+            return []
+        }
+        const schema = validatedSchema(match)
+    
+        const assetRegistryItems = assetRegistryEntries(schema)
+        
+        if (assetRegistryItems.length) {
+            //
+            // TODO: Use wml parser to pull relevant properties from the
+            // WML file.
+            //
+
+            const asset = assetRegistryItems.find(({ tag }) => (['Asset', 'Character'].includes(tag)))
+
+            const [zone, ...rest] = toPath.split('/')
+            const subFolder = rest.join('/')
+            const wmlQuery = wmlQueryFactory(contents)
+            const Body = wmlQuery(asset.tag)
+                .prop('zone', zone)
+                .prop('subFolder', subFolder)
+                .source()
+            // console.log(`contents: ${contents}`)
+            // console.log(`wmlQuery: ${JSON.stringify(wmlQuery('Character').source(), null, 4)}`)
+            // console.log(`Body: ${Body}`)
+            // console.log(`Source path: ${fromPath}${fileName}`)
+            // console.log(`Destination path: ${toPath}${fileName}`)
+
+            await s3Client.send(new CopyObjectCommand({
+                Bucket: S3_BUCKET,
+                CopySource: `${S3_BUCKET}/${fromPath}${fileName}.translate.json`,
+                Key: `${toPath}${fileName}.translate.json`
+            }))
+            await s3Client.send(new PutObjectCommand({
+                Bucket: S3_BUCKET,
+                Key: `${toPath}${fileName}.wml`,
+                Body
+            }))
+            // await s3Client.send(new DeleteObjectCommand({
+            //     Bucket: S3_BUCKET,
+            //     Key: `${fromPath}${fileName}.wml`,
+            // }))
+            // await s3Client.send(new DeleteObjectCommand({
+            //     Bucket: S3_BUCKET,
+            //     Key: `${fromPath}${fileName}.translate.json`,
+            // }))
+        }
+    // }
+    // catch {
+    //     console.log('Error!')
+    // }
+}
