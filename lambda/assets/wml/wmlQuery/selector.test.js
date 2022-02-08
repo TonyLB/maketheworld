fc18f37cac635d8651e7e35238391165361d73c5
TonyLB <anthony.lowerbasch@gmail.com>
2022-02-08 17:36:50 -0500

    ISS261: Updated Assets lambda to ohm-js/cli 1.1.0 and ESM ohm-bundles

--- lambda/assets/wml/wmlQuery/selector.test.js
+++ lambda/assets/wml/wmlQuery/selector.test.js
@@ -1,4 +1,4 @@
-import wmlGrammar from '../wmlGrammar/wml.ohm-bundle.cjs'
+import wmlGrammar from '../wmlGrammar/wml.ohm-bundle.js'
 import { wmlSelectorFactory, wmlSelectorSemantics } from './selector.js'
 
 describe('wmlQuery selector', () => {
