1242c3d835b1fa4c8b1bfdec8d248fb0aac2885d
TonyLB <anthony.lowerbasch@gmail.com>
2022-02-25 07:07:44 -0500

    ISS394: Added unit test for importAssets

---
+++ lambda/assets/serialize/importAssets.test.js
@@ -0,0 +1,77 @@
+import { jest, describe, it, expect } from '@jest/globals'
+
+import { assetDB } from '/opt/utilities/dynamoDB/index.js'
+
+import { importedAssetIds, assetIdsFromTree } from './importedAssets'
+
+describe('importedAssetIds', () => {
+    it('should return scopedIds and aggregate of import trees for dependencies', async () => {
+        assetDB.query.mockImplementation(async ({ scopedId }) => {
+            switch(scopedId) {
+                case 'VORTEX': return [{ AssetId: 'ROOM#VORTEX' }]
+                case 'Welcome': return [{ AssetId: 'ROOM#12345' }]
+            }
+        })
+        assetDB.getItem.mockResolvedValue({
+            importTree: {}
+        })
+        const output = await importedAssetIds({
+            VORTEX: { asset: 'BASE', scopedId: 'VORTEX' },
+            Welcome: { asset: 'BASE', scopedId: 'Welcome' }
+        })
+        expect(output).toEqual({
+            scopeMap: {
+                VORTEX: 'VORTEX',
+                Welcome: '12345'
+            },
+            importTree: {
+                BASE: {}
+            }
+        })
+    })
+
+    it('should nest import trees of dependencies', async () => {
+        assetDB.query.mockImplementation(async ({ scopedId }) => {
+            switch(scopedId) {
+                case 'VORTEX': return [{ AssetId: 'ROOM#VORTEX' }]
+                case 'Welcome': return [{ AssetId: 'ROOM#12345' }]
+            }
+        })
+        assetDB.getItem.mockImplementation(async ({ AssetId }) => {
+            switch(AssetId) {
+                case 'ASSET#LayerA': return { importTree: { BASE: {} } }
+                case 'ASSET#BASE': return { importTree: {} }
+            }
+        })
+        const output = await importedAssetIds({
+            VORTEX: { asset: 'BASE', scopedId: 'VORTEX' },
+            Welcome: { asset: 'LayerA', scopedId: 'Welcome' }
+        })
+        expect(output).toEqual({
+            scopeMap: {
+                VORTEX: 'VORTEX',
+                Welcome: '12345'
+            },
+            importTree: {
+                BASE: {},
+                LayerA: { BASE: {} }
+            }
+        })
+    })
+
+})
+
+describe('assetIdsFromTree', () => {
+    it('should return empty list from empty tree', () => {
+        expect(assetIdsFromTree({})).toEqual([])
+    })
+
+    it('should parse a nested tree', () => {
+        expect(assetIdsFromTree({
+            MixedLayerA: {
+                LayerA: { BASE: {} },
+                LayerB: { BASE: {} }
+            }
+        })).toEqual(['MixedLayerA', 'LayerA', 'BASE', 'LayerB'])
+    })
+})
