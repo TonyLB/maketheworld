4da3d548a836d543f46ae36f80cc16fc1a0efe9f
TonyLB <anthony.lowerbasch@gmail.com>
2022-02-27 09:52:38 -0500

    ISS431: Extended instantiateAsset to store scopeMap in Ephemera

--- lambda/assets/cache/instantiate/index.js
+++ lambda/assets/cache/instantiate/index.js
@@ -1,3 +1,11 @@
+import recalculateComputes from '/opt/utilities/executeCode/recalculateComputes.js'
+import parseWMLFile from '../parseWMLFile.js'
+import localizeDBEntries from './localize.js'
+import initializeRooms from '../initializeRooms.js'
+import AssetMetaData from '../assetMetaData.js'
+import mergeEntries from '../mergeEntries.js'
+import StateSynthesizer from '../stateSynthesis.js'
+
 export const instantiateAsset = async ({
     assetId,
     instanceId,
@@ -5,25 +13,41 @@ export const instantiateAsset = async ({
 } = {}) => {
     const { check = false, recursive = false, forceCache = false } = options
 
-    // if (check) {
-    //     const alreadyPresent = await checkEphemeraMetaData(assetId)
-    //     if (alreadyPresent) {
-    //         return
-    //     }
-    // }
-    // const { fileName, importTree, instance } = await fetchAssetMetaData(assetId)
-    // //
-    // // Instanced stories are not directly cached, they are instantiated ... so
-    // // this would be a miscall, and should be ignored.
-    // //
-    // if (instance) {
-    //     return
-    // }
-    // if (recursive) {
-    //     await Promise.all(Object.keys(importTree || {}).map((assetId) => (cacheAsset(assetId, { recursive: true, check: !forceCache }))))
-    // }
-    // const firstPassNormal = await parseWMLFile(fileName)
-    // const secondPassNormal = await globalizeDBEntries(assetId, firstPassNormal)
+    if (!assetId) {
+        return
+    }
+    const assetMetaData = new AssetMetaData(assetId)
+    if (check) {
+        const alreadyPresent = await assetMetaData.checkEphemera()
+        if (alreadyPresent) {
+            return
+        }
+    }
+    const { fileName, importTree, instance } = await assetMetaData.fetch()
+    if (!instance) {
+        return
+    }
+    //
+    // TODO: Cascading instantiation, rather than assuming any imports will be cached
+    //
+    if (recursive) {
+        await Promise.all(Object.keys(importTree || {}).map((assetId) => (cacheAsset(assetId, { recursive: true, check: !forceCache }))))
+    }
+    const firstPassNormal = await parseWMLFile(fileName)
+    console.log(`First Pass Normal: ${JSON.stringify(firstPassNormal, null, 4)}`)
+    const { scopeMap } = await localizeDBEntries({
+        assetId,
+        normalizedDBEntries: firstPassNormal
+    })
+
+    assetMetaData.scopeMap = scopeMap
+
+    // const stateSynthesizer = new StateSynthesizer(assetId, secondPassNormal)
+
+    await Promise.all([
+        assetMetaData.pushEphemera(),
+        // stateSynthesizer.updateImportedDependencies()
+    ])
 
 }
 
