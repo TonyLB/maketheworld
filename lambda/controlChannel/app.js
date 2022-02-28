ded905f9d5bbe1d164cb7842925fbe286cf114bc
TonyLB <anthony.lowerbasch@gmail.com>
2022-02-27 21:17:25 -0500

    ISS464: Added CharacterId to Link outlet and me primitive to executeAction

--- lambda/controlChannel/app.js
+++ lambda/controlChannel/app.js
@@ -385,7 +385,7 @@ export const handler = async (event, context) => {
         case 'action':
             return executeAction(request)
         case 'link':
-            const { executeMessageQueue = [] } = await executeActionFromDB({ action: request.Action, assetId: request.AssetId, RoomId: request.RoomId })
+            const { executeMessageQueue = [] } = await executeActionFromDB({ action: request.Action, assetId: request.AssetId, RoomId: request.RoomId, CharacterId: request.CharacterId })
             const epochTime = Date.now()
             await Promise.all(executeMessageQueue
                 .map((message, index) => ({
