fe363e6083e171a7fd6296718e226adff8d35feb
TonyLB <anthony.lowerbasch@gmail.com>
2022-03-12 20:21:59 -0500

    ISS612: Corrected Character dbRegister to include zone, and added handleLibraryEvents to assets dynamoEvents

---
+++ lambda/assets/dynamoEvents/library.js
@@ -0,0 +1,32 @@
+import { generateLibrary } from "/opt/utilities/selfHealing/index.js"
+import { SocketQueue } from '/opt/utilities/apiManagement/index.js'
+import { ephemeraDB } from "/opt/utilities/dynamoDB/index.js"
+
+export const handleLibraryEvents = async ({ events }) => {
+    const anyLibraryUpdates = (events
+            .filter(({ oldImage, newImage }) => (oldImage.zone === 'Library' || newImage.zone === 'Library'))
+            .filter(({ oldImage, newImage }) => ((oldImage.zone || '') !== (newImage.zone || '')))).length > 0
+
+    if (anyLibraryUpdates) {
+        const { ConnectionIds: librarySubscriptions } = await ephemeraDB.getItem({
+            EphemeraId: 'Library',
+            DataCategory: 'Subscriptions',
+            ProjectionFields: ['ConnectionIds']
+        })
+        if (librarySubscriptions.length > 0) {
+            const { Assets, Characters } = await generateLibrary()
+            const socketQueue = new SocketQueue()
+            librarySubscriptions.forEach((ConnectionId) => {
+                socketQueue.send({
+                    ConnectionId,
+                    Message: {
+                        messageType: 'Library',
+                        Assets,
+                        Characters
+                    }
+                })
+            })
+            await socketQueue.flush()
+        }    
+    }
+}
