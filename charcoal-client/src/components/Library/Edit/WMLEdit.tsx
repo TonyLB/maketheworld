eca0fd287cdd3887c1066b31912514ef6f3dc591
TonyLB <anthony.lowerbasch@gmail.com>
2022-03-14 19:29:44 -0400

    ISS623: Extended WMLEdit with Slate editor

--- charcoal-client/src/components/Library/Edit/WMLEdit.tsx
+++ charcoal-client/src/components/Library/Edit/WMLEdit.tsx
@@ -1,5 +1,11 @@
-import React, { FunctionComponent } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { FunctionComponent, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { BaseEditor, Descendant, createEditor } from 'slate'
+import { ReactEditor, Slate, Editable, withReact } from 'slate-react'
+
+import {
+    Box
+} from '@mui/material'
 
 import {
     getCurrentWML
@@ -9,9 +15,35 @@ interface WMLEditProps {
     AssetId: string;
 }
 
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
 export const WMLEdit: FunctionComponent<WMLEditProps> = ({ AssetId }) => {
-    const currentWML = useSelector(getCurrentWML(`ASSET#${AssetId}`))
-    return <div>Test</div>
+    const currentWML = useSelector(getCurrentWML(AssetId))
+    const initialValue: CustomElement[] = [{
+        type: 'paragraph',
+        children: [{ text: currentWML }]
+    }]
+    const [editor] = useState(() => withReact(createEditor()))
+    const [value, setValue] = useState<Descendant[]>(initialValue)
+    return <Box sx={{ height: "100%", width: "100%" }}>
+        <Box sx={{ margin: "0.25em", padding: "0.5em",  border: "1px solid", borderRadius: "0.5em" }}>
+            <Slate
+                editor={editor}
+                value={value}
+                onChange={newValue => setValue(newValue)}
+            >
+                <Editable {...({ spellCheck: "false" } as any)} />
+            </Slate>
+        </Box>
+    </Box>
 }
 
 export default WMLEdit
