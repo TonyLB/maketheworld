a8195e09d159d873f86c10561321a7a9000151b7
TonyLB <anthony.lowerbasch@gmail.com>
2022-03-17 18:52:51 -0400

    ISS643: Converted wmlQuery to class in front end client

--- charcoal-client/src/wml/wmlQuery/index.d.ts
+++ charcoal-client/src/wml/wmlQuery/index.d.ts
@@ -1,19 +1,24 @@
 import { NormalForm } from '../normalize';
 import { Matcher } from 'ohm-js'
 
-export interface WMLQuery {
-    (search: string): {
-        matcher: () => Matcher;
-        nodes?: any;
-        source: () => string;
-        contents: (value?: string) => WMLQuery;
-        contents: () => string;
-        prop: (key: string, value: string) => WMLQuery;
-        prop: (key: string) => string;
-        removeProp: (key: string) => WMLQuery;
-        normalize: () => NormalForm;
-        replaceInputRange: (startIdx: number, endIdx: number, str: string) => void;
-    }
+declare class WMLQueryResult {
+    constructor(WMLQuery, string);
+    nodes: () => any[];
+    source: string;
+    contents: (value?: string) => WMLQuery;
+    contents: () => string;
+    prop: (key: string, value: string) => WMLQuery;
+    prop: (key: string) => string;
+    removeProp: (key: string) => WMLQuery;
+
 }
 
-export function wmlQueryFactory(sourceString: string): WMLQuery
+export class WMLQuery {
+    constructor(string);
+    matcher: Matcher;
+    source: string;
+    setInput: (string) => void;
+    normalize: () => NormalForm;
+    replaceInputRange: (startIdx: number, endIdx: number, str: string) => void;
+    search: (string) => WMLQueryResult;
+}
