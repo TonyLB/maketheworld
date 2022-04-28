75e4254228502d1f62a7f9bc644c37770583b7db
TonyLB <anthony.lowerbasch@gmail.com>
2022-04-27 21:02:24 -0400

    ISS827: Added unit tests for LibraryAsset

---
+++ charcoal-client/src/components/Library/Edit/LibraryAsset.test.tsx
@@ -0,0 +1,179 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import LibraryAsset, { useLibraryAsset, AssetRoom, AssetFeature } from './LibraryAsset'
+
+jest.mock('../../../cacheDB')
+
+const mockStore = configureStore()
+const currentWML = `
+<Asset key=(Test) fileName="test">
+    <Import from=(BASE)>
+        <Use key=(basePower) type="Variable" as=(power) />
+        <Use key=(DEF) type="Room" />
+    </Import>
+    <Feature key=(clockTower)>
+        <Description>
+            A worn stone clock tower.
+        </Description>
+    </Feature>
+    <Room key=(ABC)>
+        <Name>Vortex</Name>
+        <Description>
+            Vortex
+            <Link key=(switchToggle) to=(toggleOpen)>(toggle)</Link>
+        </Description>
+        <Exit from=(DEF)>vortex</Exit>
+    </Room>
+    <Condition if={open}>
+        <Depend on=(open) />
+        <Room key=(ABC)>
+            <Exit to=(DEF)>welcome</Exit>
+        </Room>
+    </Condition>
+    <Room key=(DEF)>
+        <Name>Welcome</Name>
+    </Room>
+    <Variable key=(open) default={false} />
+    <Action key=(toggleOpen) src={open = !open} />
+</Asset>
+`
+
+const store = mockStore({
+   personalAssets: {
+       byId: {
+           ['ASSET#Test']: {
+               publicData: {
+                   originalWML: currentWML,
+                   currentWML,
+                   defaultAppearances: {
+                       DEF: {
+                           render: [{
+                               tag: 'String',
+                               value: 'A welcome area'
+                           }],
+                           contents: []
+                       }
+                   }
+               },
+               meta: {
+                   currentState: 'FRESH',
+                   desiredStates: ['FRESH', 'DIRTY']
+               }
+           }
+       }
+   }
+})
+
+describe('LibraryAsset context provider', () => {
+
+    beforeEach(() => {
+        store.clearActions()
+    })
+
+    it('should provide currentWML', () => {
+        const TestComponent = () => {
+            const { currentWML } = useLibraryAsset()
+            return <div>{ currentWML }</div>
+        }
+        const { container } = render(
+            <Provider store={store}>
+                <LibraryAsset assetKey='Test' >
+                    <TestComponent />
+                </LibraryAsset>
+            </Provider>
+        )
+        expect(container.textContent).toMatchSnapshot()
+
+    })
+
+    it('should provide normalForm', () => {
+        const TestComponent = () => {
+            const { normalForm } = useLibraryAsset()
+            return <div>{ JSON.stringify(normalForm, null, 4) }</div>
+        }
+        const { container } = render(
+            <Provider store={store}>
+                <LibraryAsset assetKey='Test' >
+                    <TestComponent />
+                </LibraryAsset>
+            </Provider>
+        )
+        expect(container.textContent).toMatchSnapshot()
+
+    })
+
+    it('should provide rooms', ()=>{
+        const RoomComponent = ({ value }: { value: AssetRoom }) => {
+            const { name, render } = value
+            return <div>
+                <span>{name}</span>
+                { render.map((room, index) => {
+                    switch(room.tag) {
+                        case 'String':
+                            return <React.Fragment key={index}>
+                                <span>String</span>
+                                <span>{ room.value }</span>
+                            </React.Fragment>
+                        default: 
+                            return <React.Fragment key={index}>
+                                <span>Link</span>
+                                <span>{ room.key }</span>
+                            </React.Fragment>
+                    }
+                })}
+            </div>
+        }
+        const TestComponent = () => {
+            const { rooms } = useLibraryAsset()
+            return <div>{ Object.entries(rooms).map(([key, value]) => (<RoomComponent key={key} value={value} />)) }</div>
+        }
+        const { container } = render(
+            <Provider store={store}>
+                <LibraryAsset assetKey='Test' >
+                    <TestComponent />
+                </LibraryAsset>
+            </Provider>
+        )
+        expect(container).toMatchSnapshot()
+
+    })
+
+    it('should provide features', ()=>{
+        const FeatureComponent = ({ value }: { value: AssetFeature }) => {
+            const { name, render } = value
+            return <div>
+                <span>{name}</span>
+                { render.map((room, index) => {
+                    switch(room.tag) {
+                        case 'String':
+                            return <React.Fragment key={index}>
+                                <span>String</span>
+                                <span>{ room.value }</span>
+                            </React.Fragment>
+                        default: 
+                            return <React.Fragment key={index}>
+                                <span>Link</span>
+                                <span>{ room.key }</span>
+                            </React.Fragment>
+                    }
+                })}
+            </div>
+        }
+        const TestComponent = () => {
+            const { features } = useLibraryAsset()
+            return <div>{ Object.entries(features).map(([key, value]) => (<FeatureComponent key={key} value={value} />)) }</div>
+        }
+        const { container } = render(
+            <Provider store={store}>
+                <LibraryAsset assetKey='Test' >
+                    <TestComponent />
+                </LibraryAsset>
+            </Provider>
+        )
+        expect(container).toMatchSnapshot()
+
+    })
+
+})
