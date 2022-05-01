6ccbc12fdb71b3baa287165e3844bab88e18d5fc
TonyLB <anthony.lowerbasch@gmail.com>
2022-05-01 11:00:46 -0400

    ISS851: Refactored AddWMLComponent to switch between ListItem and ListItemButton

--- charcoal-client/src/components/Library/Edit/AddWMLComponent.tsx
+++ charcoal-client/src/components/Library/Edit/AddWMLComponent.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 
 import {
+    ListItem,
     ListItemButton,
     ListItemText,
     ListItemIcon,
@@ -28,12 +29,9 @@ export const AddWMLComponent: FunctionComponent<WMLComponentHeaderProps> = ({ ty
         if (key.search(/^[\w\d\_]*$/) === -1) {
             return `Keys must be made of up letters, digits, and the "_" character`
         }
-        //
-        // TODO: Decipher WML key format, and require a valid key
-        //
         return ''
     }, [key, normalForm])
-    return <ListItemButton onClick={enteringKey ? () => {} : () => { setEnteringKey(true) }}>
+    const contents = <React.Fragment>
         <ListItemIcon>
             <AddIcon />
         </ListItemIcon>
@@ -47,6 +45,7 @@ export const AddWMLComponent: FunctionComponent<WMLComponentHeaderProps> = ({ ty
                         required
                         id="component-key"
                         label="Key"
+                        size="small"
                         value={key}
                         onChange={(event) => {
                             setKey(event.target.value)
@@ -62,7 +61,14 @@ export const AddWMLComponent: FunctionComponent<WMLComponentHeaderProps> = ({ ty
                     <Button variant="outlined" onClick={() => { setEnteringKey(false) }}>Cancel</Button>
                 </React.Fragment>
         }
-    </ListItemButton>
+    </React.Fragment>
+    return enteringKey
+        ? <ListItem>
+            { contents }
+        </ListItem>
+        : <ListItemButton onClick={enteringKey ? () => {} : () => { setEnteringKey(true) }}>
+            { contents }
+        </ListItemButton>
 }
 
 export default AddWMLComponent
