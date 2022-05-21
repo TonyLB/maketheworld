d5ed2aa9fa92ccf878ce437c90370c499257cfba
TonyLB <anthony.lowerbasch@gmail.com>
2022-05-21 09:50:24 -0400

    ISS951: Added debounced WML updater for exit name text field

--- charcoal-client/src/components/Library/Edit/RoomExitHeader.tsx
+++ charcoal-client/src/components/Library/Edit/RoomExitHeader.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback, useState } from 'react'
+import React, { FunctionComponent, useCallback, useEffect, useState } from 'react'
 
 import {
     Box,
@@ -8,21 +8,30 @@ import ExitIcon from '@mui/icons-material/CallMade'
 
 import { isNormalExit } from '../../../wml/normalize'
 import AssetDataHeader, { AssetDataHeaderRenderFunction} from './AssetDataHeader'
+import { useLibraryAsset } from './LibraryAsset'
+import useDebounce from '../../../hooks/useDebounce'
 import { noConditionContext } from './utilities'
 
 interface RoomExitHeaderBaseProps {
     defaultName: string;
     toTarget: boolean;
     targetName: string;
+    onChanged?: (value: string) => void;
 }
 
-const RoomExitHeaderBase: FunctionComponent<RoomExitHeaderBaseProps> = ({ defaultName, toTarget, targetName }) => {
+const RoomExitHeaderBase: FunctionComponent<RoomExitHeaderBaseProps> = ({ defaultName, toTarget, targetName, onChanged=() => {} }) => {
     const [name, setName] = useState<string>(defaultName)
+    const debouncedName = useDebounce(name, 1000)
+    useEffect(() => {
+        if (debouncedName !== defaultName) {
+            onChanged(debouncedName)
+        }
+    }, [debouncedName, defaultName])
     const onChange = useCallback((event) => {
         setName(event.target.value)
-    }, [setName])
+    }, [setName, onChanged])
     return <Box sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
-        <Box sx={{ maxWidth: "20em", flexGrow: 1, display: "flex", marginRight: "1em" }}>
+        <Box sx={{ maxWidth: "12em", flexGrow: 1, display: "flex", marginRight: "1em" }}>
             <TextField
                 size="small"
                 sx={{ width: "100%" }}
@@ -45,9 +54,18 @@ interface RoomExitHeaderProps {
 }
 
 export const RoomExitHeader: FunctionComponent<RoomExitHeaderProps> = ({ ItemId, RoomId, onClick }) => {
+    const { wmlQuery, updateWML } = useLibraryAsset()
+    const saveName = useCallback(({ location }: { location: number[] }) => (name: string) => {
+        if (location.length) {
+            const exitQuery = wmlQuery.search(['Asset', ...location.slice(1).map((index) => (`:nthChild(${index})`))].join(''))
+            exitQuery.contents(name)
+            updateWML(exitQuery.source)    
+        }
+    }, [ItemId, RoomId, wmlQuery, updateWML])
     const primaryBase: AssetDataHeaderRenderFunction = ({ item, defaultItem, rooms }) => {
         if (isNormalExit(item)) {
             const toTarget = Boolean(item.to === RoomId)
+            const location = (item.appearances?.filter(noConditionContext)?.[0]?.location) || []
             return <RoomExitHeaderBase
                 targetName={
                     toTarget
@@ -56,13 +74,12 @@ export const RoomExitHeader: FunctionComponent<RoomExitHeaderProps> = ({ ItemId,
                 }
                 toTarget={toTarget}
                 defaultName={item.name || defaultItem.name || ''}
+                onChanged={saveName({ location })}
             />
         }
         return ''
     }
     const primary = useCallback(primaryBase, [])
-    const secondaryBase: AssetDataHeaderRenderFunction = ({ item }) => (item.key)
-    const secondary = useCallback(secondaryBase, [])
     return <AssetDataHeader
         ItemId={ItemId}
         icon={<ExitIcon />}
