4d595a0e04b82ef8ba8eb5b915f22367bfa59747
TonyLB <anthony.lowerbasch@gmail.com>
2022-05-21 14:00:56 -0400

    ISS947: Converted AddWMLComponent to use AssetDataAddHeader

--- charcoal-client/src/components/Library/Edit/AddWMLComponent.tsx
+++ charcoal-client/src/components/Library/Edit/AddWMLComponent.tsx
@@ -1,74 +1,54 @@
-import React, { FunctionComponent, useState, useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import React, { FunctionComponent, useCallback } from 'react'
 
 import {
-    ListItem,
-    ListItemButton,
-    ListItemText,
-    ListItemIcon,
-    Button,
     TextField
 } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add'
 
 import { useLibraryAsset } from './LibraryAsset'
+import AssetDataAddHeader from './AssetDataAddHeader'
 
-interface WMLComponentHeaderProps {
+interface AddWMLComponentProps {
     type: 'Room' | 'Feature';
     onAdd?: (key: string) => void;
 }
 
-export const AddWMLComponent: FunctionComponent<WMLComponentHeaderProps> = ({ type, onAdd = () => { }}) => {
+const addWMLRenderFieldsGenerator: (validate: (props: { key: string }) => string) => FunctionComponent<{ key: string; onChange: (props: { key: string }) => void }> = (validate) => ({ key, onChange }) => {
+    const errorMessage = validate({ key })
+    return <TextField
+        required
+        id="component-key"
+        label="Key"
+        size="small"
+        value={key}
+        onChange={(event) => {
+            onChange({ key: event.target.value })
+        }}
+        error={Boolean(errorMessage)}
+    />
+}
+
+export const AddWMLComponent: FunctionComponent<AddWMLComponentProps> = ({ type, onAdd = () => { }}) => {
+    const onAddWrapper = useCallback(({ key }: { key: string }) => { onAdd(key) }, [onAdd])
     const { normalForm } = useLibraryAsset()
-    const [key, setKey] = useState<string>('')
-    const [enteringKey, setEnteringKey] = useState<boolean>(false)
-    const errorMessage = useMemo<string>(() => {
+    const validate = useCallback(({ key }: { key: string }) => {
         if (key in normalForm) {
             return `"${key}" is already used in this asset`
         }
-        if (key.length && (key.search(/^\w[\w\d\_]*$/) === -1)) {
+        if (key.length && (key.search(/^[A-Za-z][\w\_]*$/) === -1)) {
             return `Keys must start with a letter and be made of up letters, digits, and the "_" character`
         }
+        if (!key.length) {
+            return 'You must specify a key'
+        }
         return ''
-    }, [key, normalForm])
-    const contents = <React.Fragment>
-        <ListItemIcon>
-            <AddIcon />
-        </ListItemIcon>
-        {
-            (enteringKey === false) && <ListItemText primary={`Add ${type}`} />
-        }
-        {
-            (enteringKey === true) &&
-                <React.Fragment>
-                    <TextField
-                        required
-                        id="component-key"
-                        label="Key"
-                        size="small"
-                        value={key}
-                        onChange={(event) => {
-                            setKey(event.target.value)
-                        }}
-                        error={Boolean(errorMessage)}
-                        helperText={errorMessage}
-                    />
-                    <Button disabled={Boolean(errorMessage) || (key.length === 0)} variant="contained" onClick={() => {
-                        onAdd(key)
-                        setEnteringKey(false)
-                        setKey('')
-                    } }>Add</Button>
-                    <Button variant="outlined" onClick={() => { setEnteringKey(false) }}>Cancel</Button>
-                </React.Fragment>
-        }
-    </React.Fragment>
-    return enteringKey
-        ? <ListItem>
-            { contents }
-        </ListItem>
-        : <ListItemButton onClick={enteringKey ? () => {} : () => { setEnteringKey(true) }}>
-            { contents }
-        </ListItemButton>
+    }, [normalForm])
+    return <AssetDataAddHeader
+        defaultFields={{ key: '' }}
+        label={`Add ${type}`}
+        renderFields={addWMLRenderFieldsGenerator(validate)}
+        validate={validate}
+        onAdd={onAddWrapper}
+    />
 }
 
 export default AddWMLComponent
