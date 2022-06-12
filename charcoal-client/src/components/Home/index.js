64a95d33b0482112704460fcfbf3531944aa00ef
TonyLB <anthony.lowerbasch@gmail.com>
2022-06-12 15:21:33 -0400

    ISS1056: Added new character shortcut to Home page

--- charcoal-client/src/components/Home/index.js
+++ charcoal-client/src/components/Home/index.js
@@ -11,6 +11,7 @@ import Divider from '@mui/material/Divider'
 import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import AddIcon from '@mui/icons-material/Add'
 
 import LibraryIcon from '@mui/icons-material/ArtTrack'
 import MapIcon from '@mui/icons-material/Explore'
@@ -100,17 +101,51 @@ export const Home = ({
                         alignItems="center"
                         spacing={2}
                     >
-                            <Avatar
-                                sx={{ width: `${iconSize}px`, height: `${iconSize}px` }}
-                                alt={Name || '???'}
-                                src={fileURL}
-                            >
-                                {Name[0] ? Name[0].toUpperCase() : '?'}
-                            </Avatar>
-                            <React.Fragment>{ Name }</React.Fragment>
+                        <Avatar
+                            sx={{ width: `${iconSize}px`, height: `${iconSize}px` }}
+                            alt={Name || '???'}
+                            src={fileURL}
+                        >
+                            {Name[0] ? Name[0].toUpperCase() : '?'}
+                        </Avatar>
+                        <React.Fragment>{ Name }</React.Fragment>
                     </Stack>
                 </Grid>))
             }
+            <Grid key='NewCharacter'
+                    container
+                    item
+                    sm={3}
+                    sx={{
+                        justifyContent: 'center',
+                        alignContent: 'center',
+                        cursor: 'pointer'
+                    }}
+                    onClick={() => {
+                        navigate(`/Library`)
+                    }}
+            >
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Avatar
+                        sx={{
+                            width: `${iconSize}px`,
+                            height: `${iconSize}px`
+                        }}
+                        alt='New Character'
+                    >
+                        <AddIcon sx={{
+                            fontSize: iconSize,
+                            color: 'grey'
+                        }} />
+                    </Avatar>
+                    <React.Fragment>New Character</React.Fragment>
+                </Stack>
+            </Grid>
             <Grid item xs={12} sx={{ textAlign: "center" }}>
                 <Divider />
                     <h2>Administer</h2>
