0be4675e6d056065f2ec320b8939a0042dcaca00
TonyLB <anthony.lowerbasch@gmail.com>
2022-07-23 11:05:22 -0400

    ISS1263: Added typescript types for parsing tokens

---
+++ packages/mtw-wml/ts/parser/tokenizer/baseClasses.ts
@@ -0,0 +1,56 @@
+export type TokenBase = {
+    source: string;
+    startIdx: number;
+    endIdx: number;
+}
+
+export type TokenComment = {
+    type: 'Comment'
+} & TokenBase
+
+export type TokenWhitespace = {
+    type: 'Whitespace'
+} & TokenBase
+
+export type TokenExpressionValue = {
+    type: 'ExpressionValue'
+} & TokenBase
+
+export type TokenKeyValue = {
+    type: 'KeyValue'
+} & TokenBase
+
+export type TokenLiteralValue = {
+    type: 'LiteralValue'
+}
+
+export type TokenProperty = {
+    type: 'Property'
+} & TokenBase
+
+export type TokenBeginTagOpen = {
+    type: 'BeginTagOpen'
+} & TokenBase
+
+export type TokenBeginTagClose = {
+    type: 'BeginTagClose';
+} & TokenBase
+
+export type TokenEndTag = {
+    type: 'EndTag';
+} & TokenBase
+
+export type TokenDescription = {
+    type: 'Description';
+} & TokenBase
+
+export type Token = TokenComment |
+    TokenWhitespace |
+    TokenExpressionValue |
+    TokenKeyValue |
+    TokenLiteralValue |
+    TokenProperty |
+    TokenBeginTagOpen |
+    TokenBeginTagClose |
+    TokenEndTag |
+    TokenDescription
