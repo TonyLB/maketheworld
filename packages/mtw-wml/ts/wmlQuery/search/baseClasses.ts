667f308a013ead5690e7103ab76a96dc5bf62494
TonyLB <anthony.lowerbasch@gmail.com>
2022-08-01 21:11:10 -0400

    ISS1326: Created WMLQuery Selector typescript

---
+++ packages/mtw-wml/ts/wmlQuery/search/baseClasses.ts
@@ -0,0 +1,97 @@
+export type SearchTokenWhitespace = {
+    type: 'Whitespace';
+}
+
+type SearchTokenTagLegal = "Asset" |
+    "Story" |
+    "Character" |
+    "Name" |
+    "Pronouns" |
+    "OneCoolThing" |
+    "Outfit" |
+    "FirstImpression" |
+    "Condition" |
+    "Layer" |
+    "Exit" |
+    "Description" |
+    "Room" |
+    "Feature" |
+    "Map"
+
+export type SearchTokenTag = {
+    type: 'Tag';
+    tag:  SearchTokenTagLegal;
+}
+
+export type SearchTokenString = {
+    type: 'String';
+    value: string;
+}
+
+type SearchTokenPropertyLegal = 'key' | 'to' | 'from'
+export type SearchTokenProperty = {
+    type: 'Property';
+    value: SearchTokenPropertyLegal
+}
+
+export type SearchTokenFirst = {
+    type: 'First';
+}
+
+export type SearchTokenNthChild = {
+    type: 'NthChild';
+    n: number;
+}
+
+export type SearchTokenGroupOpen = {
+    type: 'GroupOpen';
+}
+
+export type SearchTokenGroupClose = {
+    type: 'GroupClose';
+}
+
+export type SearchTokenComma = {
+    type: 'Comma';
+}
+
+export type SearchToken = SearchTokenTag |
+    SearchTokenString |
+    SearchTokenProperty |
+    SearchTokenFirst |
+    SearchTokenNthChild |
+    SearchTokenGroupOpen |
+    SearchTokenGroupClose |
+    SearchTokenComma |
+    SearchTokenWhitespace
+
+export type SearchParseTag = {
+    type: 'Tag';
+    tag: SearchTokenTagLegal;
+}
+
+export type SearchParseProperty = {
+    type: 'Property';
+    key: SearchTokenPropertyLegal;
+    value: string;
+}
+
+export type SearchParseFirst = {
+    type: 'First';
+}
+
+export type SearchParseNthChild = {
+    type: 'NthChild';
+    n: number;
+}
+
+export type SearchParseGroup = {
+    type: 'Group';
+    items: SearchParse[];
+}
+
+export type SearchParse = SearchParseTag |
+    SearchParseProperty |
+    SearchParseFirst |
+    SearchParseNthChild |
+    SearchParseGroup
