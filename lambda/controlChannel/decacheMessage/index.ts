5da53fa182324e5a10b2f317768cc5c01693d6bc
TonyLB <anthony.lowerbasch@gmail.com>
2022-09-10 12:40:00 -0400

    ISS1408: Refactored Decache Asset to use per-asset merge function

--- lambda/controlChannel/decacheMessage/index.ts
+++ lambda/controlChannel/decacheMessage/index.ts
@@ -1,31 +1,21 @@
-import { marshall } from "@aws-sdk/util-dynamodb";
-import {
-    ephemeraDB,
-    batchWriteDispatcher
-} from "@tonylb/mtw-utilities/dist/dynamoDB"
+import { ephemeraDB } from "@tonylb/mtw-utilities/dist/dynamoDB"
 import { AssetKey } from "@tonylb/mtw-utilities/dist/types"
 import { DecacheAssetMessage, MessageBus } from "../messageBus/baseClasses"
-
-const { TABLE_PREFIX } = process.env;
-const ephemeraTable = `${TABLE_PREFIX}_ephemera`
+import { mergeIntoEphemera } from "../cacheAsset/perAsset";
 
 export const decacheAssetMessage = async ({ payloads, messageBus }: { payloads: DecacheAssetMessage[], messageBus: MessageBus }): Promise<void> => {
     await Promise.all(payloads.map(async ({ assetId }) => {
-        const cachedItems = await ephemeraDB.query<{ EphemeraId: string }[]>({
-            IndexName: 'DataCategoryIndex',
-            DataCategory: AssetKey(assetId),
-        })
-        await batchWriteDispatcher({
-            table: ephemeraTable,
-            items: cachedItems.map(({ EphemeraId }) => ({
-                DeleteRequest: {
-                    Key: marshall({
-                        EphemeraId,
-                        DataCategory: AssetKey(assetId)
-                    })
-                }
-            }))
-        })
+        await Promise.all([
+            mergeIntoEphemera(assetId, []),
+            ephemeraDB.deleteItem({
+                EphemeraId: AssetKey(assetId),
+                DataCategory: 'Meta::Asset'
+            }),
+            ephemeraDB.deleteItem({
+                EphemeraId: AssetKey(assetId),
+                DataCategory: 'Meta::AssetNormalized'
+            })
+        ])
     }))
     messageBus.send({
         type: 'ReturnValue',
