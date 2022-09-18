933a484dbb0071f95fd82b9506e6196cb45cae1d
TonyLB <anthony.lowerbasch@gmail.com>
2022-09-18 17:29:05 -0400

    ISS1464: Drafted change plan in disconnectMessage

--- lambda/controlChannel/disconnectMessage/index.ts
+++ lambda/controlChannel/disconnectMessage/index.ts
@@ -2,6 +2,22 @@ import { DisconnectMessage, MessageBus } from "../messageBus/baseClasses"
 
 import { forceDisconnect } from '@tonylb/mtw-utilities/dist/apiManagement/forceDisconnect'
 
+//
+// TODO:
+//    - Remove Meta::Connection record
+//    - Remove all Character adjacency records using transactions to atomically update Meta::Character item
+//    - Update Library subscriptions
+//    - Update Global connections
+//    - Update activeCharacters in current room for Meta::Character items that were removed
+//
+
+const atomicallyRemoveCharacterAdjacency = async (connectionId, characterId) => {
+    //
+    // TODO: Abstract repeated attempts (with exponential backoff) at a try clause, and use
+    // that abstraction here
+    //
+}
+
 export const disconnectMessage = async ({ payloads }: { payloads: DisconnectMessage[], messageBus?: MessageBus }): Promise<void> => {
     //
     // TODO: Figure out whether a forced disconnet invalidates any cached values
