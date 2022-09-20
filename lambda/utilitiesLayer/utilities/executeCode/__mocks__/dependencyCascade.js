3a61b96fd0e70dc5d84afec076ebaaab09a88968
TonyLB <anthony.lowerbasch@gmail.com>
2022-09-20 19:29:53 -0400

    ISS1493: Deprecated utilities lambda layer

--- lambda/utilitiesLayer/utilities/executeCode/__mocks__/dependencyCascade.js
+++
@@ -1,5 +0,0 @@
-import { jest } from '@jest/globals'
-
-export const dependencyCascade = jest.fn()
-
-export default dependencyCascade
--- lambda/utilitiesLayer/utilities/objects.js
+++
@@ -1,8 +0,0 @@
-export const objectMap = (object, callback) => {
-    return Object.entries(object)
-        .map(([key, value]) => ([key, callback(value)]))
-        .reduce((previous, [key, value]) => ({
-            ...previous,
-            [key]: value
-        }), {})
-}
