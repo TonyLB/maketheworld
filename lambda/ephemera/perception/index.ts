10db1115361d7f5a0465b77ea49d9fa660b318de
TonyLB <anthony.lowerbasch@gmail.com>
2022-10-01 16:09:34 -0400

    ISS1581: Created stub breakout of components in perception message handler

--- lambda/ephemera/perception/index.ts
+++ lambda/ephemera/perception/index.ts
@@ -1,13 +1,16 @@
 import { PerceptionMessage, MessageBus } from "../messageBus/baseClasses"
 import { render } from "@tonylb/mtw-utilities/dist/perception"
 import { deliverRenders } from "@tonylb/mtw-utilities/dist/perception/deliverRenders"
+import { tagFromEphemeraId } from "../internalCache/dependencyGraph"
 
 export const perceptionMessage = async ({ payloads, messageBus }: { payloads: PerceptionMessage[], messageBus: MessageBus }): Promise<void> => {
     const renderOutputs = await render({
-        renderList: payloads.map(({ characterId, ephemeraId }) => ({
-            CharacterId: characterId,
-            EphemeraId: ephemeraId
-        }))
+        renderList: payloads
+            // .filter(({ ephemeraId }) => (!(['Room', 'Feature'].includes(tagFromEphemeraId(ephemeraId)))))
+            .map(({ characterId, ephemeraId }) => ({
+                CharacterId: characterId,
+                EphemeraId: ephemeraId
+            }))
     })
     //
     // TODO: Replace deliverRenders with a messageBus handler
@@ -15,7 +18,16 @@ export const perceptionMessage = async ({ payloads, messageBus }: { payloads: Pe
     await deliverRenders({
         renderOutputs
     })
-    
+
+    //
+    // TODO: Translate other types, above, into directly-handled format like below
+    //
+    await Promise.all(payloads.filter(({ ephemeraId }) => (['Room', 'Feature'].includes(tagFromEphemeraId(ephemeraId))))
+        .map(async ({ characterId, ephemeraId }) => {
+
+        })
+    )
+
     messageBus.send({
         type: 'ReturnValue',
         body: {
