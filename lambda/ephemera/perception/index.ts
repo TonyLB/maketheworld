88312ba99ac393e1b290c2218e2a2cb9a3f61e4a
TonyLB <anthony.lowerbasch@gmail.com>
2022-10-01 21:05:50 -0400

    ISS1581: Created first-draft function to aggregate visible appearances for render

--- lambda/ephemera/perception/index.ts
+++ lambda/ephemera/perception/index.ts
@@ -2,6 +2,10 @@ import { PerceptionMessage, MessageBus } from "../messageBus/baseClasses"
 import { render } from "@tonylb/mtw-utilities/dist/perception"
 import { deliverRenders } from "@tonylb/mtw-utilities/dist/perception/deliverRenders"
 import { tagFromEphemeraId } from "../internalCache/dependencyGraph"
+import internalCache from "../internalCache"
+import { unique } from "@tonylb/mtw-utilities/dist/lists"
+import { EphemeraRoomAppearance } from "../cacheAsset/baseClasses"
+import { componentAppearanceReduce } from "./components"
 
 export const perceptionMessage = async ({ payloads, messageBus }: { payloads: PerceptionMessage[], messageBus: MessageBus }): Promise<void> => {
     const renderOutputs = await render({
@@ -24,6 +28,45 @@ export const perceptionMessage = async ({ payloads, messageBus }: { payloads: Pe
     //
     await Promise.all(payloads.filter(({ ephemeraId }) => (['Room', 'Feature'].includes(tagFromEphemeraId(ephemeraId))))
         .map(async ({ characterId, ephemeraId }) => {
+            const [globalAssets, { assets: characterAssets }] = await Promise.all([
+                internalCache.Global.get('assets'),
+                internalCache.CharacterMeta.get(characterId)
+            ])
+            const appearancesByAsset = await internalCache.ComponentMeta.getAcrossAssets(ephemeraId, unique(globalAssets, characterAssets) as string[])
+
+            const tag = tagFromEphemeraId(ephemeraId) as 'Room' | 'Feature'
+            switch(tag) {
+                case 'Room':
+                    const possibleAppearances = [...(globalAssets || []), ...characterAssets]
+                        .map((assetId) => ((appearancesByAsset[assetId]?.appearances || []) as EphemeraRoomAppearance[]))
+                        .reduce<EphemeraRoomAppearance[]>((previous, appearances) => ([ ...previous, ...appearances ]), [])
+                    const renderAppearances = (await Promise.all(
+                        possibleAppearances.map(async (appearance) => {
+                            const conditionsPassList = await Promise.all(appearance.conditions.map(async ({ if: source, dependencies }) => (
+                                internalCache.EvaluateCode.get({
+                                    source,
+                                    mapping: dependencies.reduce<Record<string, string>>((previous, { EphemeraId, key }) => ({ ...previous, [key]: EphemeraId }), {})
+                                })
+                            )))
+                            const allConditionsPass = conditionsPassList.reduce<boolean>((previous, value) => (previous && Boolean(value)), true)
+                            if (allConditionsPass) {
+                                return appearance
+                            }
+                            else {
+                                return undefined
+                            }
+                        })
+                    )).filter((value: EphemeraRoomAppearance | undefined): value is EphemeraRoomAppearance => (Boolean(value)))
+                    const render = componentAppearanceReduce(...renderAppearances)
+                    break
+                    //
+                    // TODO: Send the render message to the messageBus
+                    //
+
+                    //
+                    // TODO: Create analogous render pipeline for Features
+                    //
+            }
 
         })
     )
