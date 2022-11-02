0ffa34bdd1ce7186c2886e5b9ba98f7c2d92c9c0
TonyLB <anthony.lowerbasch@gmail.com>
2022-11-01 20:25:03 -0400

    ISS1750: Created extractDependencyFromJS POC using Acorn

---
+++ packages/mtw-wml/ts/parser/condition.test.ts
@@ -0,0 +1,22 @@
+import { extractDependenciesFromJS } from './condition'
+
+describe('condition parsing', () => {
+    describe('extractDependenciesFromJS', () => {
+        it('should extract top-level globals', () => {
+            expect(extractDependenciesFromJS('testVariableOne && !testVariableTwo')).toEqual(['testVariableOne', 'testVariableTwo'])
+        })
+
+        it('should ignore function arguments', () => {
+            expect(extractDependenciesFromJS('(localVariable) => (localVariable)(globalVariable)')).toEqual(['globalVariable'])
+        })
+
+        it('should ignore local variables', () => {
+            expect(extractDependenciesFromJS(`
+                (() => {
+                    const localVariable = globalVariable * 2
+                    return localVariable
+                })()
+            `)).toEqual(['globalVariable'])
+        })
+    })
+})
--- packages/mtw-wml/ts/parser/condition.ts
+++ packages/mtw-wml/ts/parser/condition.ts
@@ -1,10 +1,36 @@
 import { ArrayContents } from "../types"
 import { ParseTagFactory, ParseConditionTag, isParseTagDependency, ParseConditionTypeFromContextTag, ParseStackTagEntry, parseDifferentiatingTags } from "./baseClasses"
 import { validateProperties, ExtractProperties, validateContents } from "./utils"
+import { parse as acornParse} from "acorn"
+import { simple as simpleWalk } from "acorn-walk"
 
 //
-// TODO: Figure out how to sub-type different conditions to have different legal contents
+// extractDependenciesFromJS is a painfully naive dependency extractor using only the barest fraction of the recursive
+// scoping functionality of acorn-walk ... and will still probably be good enough for 99+% of cases
 //
+export const extractDependenciesFromJS = (src: string) => {
+    const parsedJS = acornParse(src.trim(), { ecmaVersion: 'latest' })
+    let identifiedGlobals: string[] = []
+    let definedLocals: string[] = []
+    simpleWalk(parsedJS, {
+        Identifier(node) {
+            const identifier = (node as any).name
+            if (!(definedLocals.includes(identifier))) {
+                identifiedGlobals.push((node as any).name)
+            }
+        },
+        ArrowFunctionExpression(node) {
+            ((node as any).params || []).forEach(({ name }) => {
+                definedLocals.push(name)
+            })
+        },
+        VariableDeclarator(node) {
+            definedLocals.push((node as any).id.name)
+        }
+    })
+    return [...(new Set(identifiedGlobals.filter((item) => (!definedLocals.includes(item)))))]
+}
+
 export const parseConditionFactory = <T extends ParseConditionTag["contextTag"]>(contextTag: T): ParseTagFactory<ParseConditionTypeFromContextTag<T>> => ({ open, contents, endTagToken }) => {
     const validate = validateProperties<ExtractProperties<ParseConditionTag, 'dependencies'>>({
         open,
