9dabf24b90b1c9342a9f9c0aaf75233d9299aa2d
TonyLB <anthony.lowerbasch@gmail.com>
2022-11-09 08:20:10 -0500

    ISS1792: Added UnionToIntersection voodoo to functionMixins

--- packages/mtw-wml/ts/functionMixins.ts
+++ packages/mtw-wml/ts/functionMixins.ts
@@ -3,23 +3,37 @@
 // of (particularly) translation functions in WML parsing and schema creation.
 //
 
-type ArgumentTypes<F extends (...args: any) => any> = F extends (...args: infer A) => any ? A : never
-
-const stringIdentity = (value: string): string => (value)
-
-const numberIdentity = <F extends (...args: any) => any>(wrappedFunc: F): (((...args: ArgumentTypes<F>) => ReturnType<F>) & ((value: number) => number)) => (value) => {
-    if (typeof value === 'number') {
-        return value
-    }
-    else {
-        return wrappedFunc(value)
-    }
+type ConverterArgument<A extends any, T extends { tag: string; }> = {
+    typeGuard: (value: any) => value is A;
+    convert: (value: A) => T;
 }
 
-const jointIdentity = numberIdentity(stringIdentity)
+type AnyConverterArgument = ConverterArgument<any, { tag: string }>
 
-const stringTest = jointIdentity('Test')
-const numberTest = jointIdentity(5)
+//
+// The following dark art adopted from: https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type/50375286#50375286
+//
+type UnionToIntersection<U> = 
+  (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never
+
+const composeConvertersHelper = <F extends (...args: any) => any, A extends AnyConverterArgument>(fallback: F, ...args: A[]): UnionToIntersection<A["convert"]> & F => {
+    const returnValue = args.reduce((previous, { typeGuard, convert }) => {
+        const wrapperFunc = (wrappedFunc: (...args: any) => any) => (value: Parameters<typeof convert>[0]) => {
+            if (typeGuard(value)) {
+                return convert(value)
+            }
+            else {
+                return wrappedFunc(value)
+            }
+        }
+        return wrapperFunc(previous)
+    }, fallback)
+    return returnValue as UnionToIntersection<A["convert"]> & F
+}
+
+const composeConverters = <A extends AnyConverterArgument>(...args: A[]): UnionToIntersection<A["convert"]> & (() => never) => (
+    composeConvertersHelper(() => { throw new Error('Functon composition failure') }, ...args)
+)
 
 type TagString = {
     tag: 'String';
@@ -45,34 +59,14 @@ const convertToTagNumber = (value: number): TagNumber => ({
     value
 })
 
-type ConverterArgument<A extends any, T extends TagString | TagNumber> = {
-    typeGuard: (value: any) => value is A;
-    convert: (value: A) => T;
-}
-
-const composeConverters = <A extends ConverterArgument<any, TagString | TagNumber>>(...args: A[]): ((value: ArgumentTypes<A["convert"]>[0]) => ReturnType<A["convert"]>) => {
-    return args.reduce((previous, { typeGuard, convert }) => {
-        const wrapperFunc = <F extends (...args: any) => any>(wrappedFunc: F): (((...args: ArgumentTypes<F>) => ReturnType<F>) & ((value: ArgumentTypes<typeof convert>[0]) => ReturnType<typeof convert>)) => (value) => {
-            if (typeGuard(value)) {
-                return value
-            }
-            else {
-                return wrappedFunc(value)
-            }
-        }
-        return wrapperFunc(previous)
-    }, () => { throw new Error('Composition failed') })
-}
-
 const composedConverters = composeConverters(
     { typeGuard: isString, convert: convertToTagString },
     { typeGuard: isNumber, convert: convertToTagNumber }
 )
 
 //
-// TODO: Figure out how to write composeConverters so that convertStringTest is typed
-// to TagString, etc.
+// Note that each converter is correctly typecast based on incoming arguments
 //
 const convertStringTest = composedConverters('Test')
 const convertNumberTest = composedConverters(5)
-
+//const convertBooleanTest = composedConverters(false)
