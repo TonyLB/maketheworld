8d9911f8bcf253fa55a7c8f553ada0926877da45
TonyLB <anthony.lowerbasch@gmail.com>
2022-12-04 16:10:22 -0500

    ISS1099: Extended Jimp solution to resize image and write to bucket

--- lambda/images/app.ts
+++ lambda/images/app.ts
@@ -1,5 +1,5 @@
 // Import required AWS SDK clients and commands for Node.js
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3"
+import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
 import jimp from "jimp"
 
 const params = { region: process.env.AWS_REGION }
@@ -19,25 +19,32 @@ export const handler = async (event, context) => {
     // Handle EventBridge messages
     if (['mtw.coordination'].includes(event?.source || '')) {
         if (event["detail-type"] === 'Format Image') {
-            if (event.detail?.fromFileName) {
+            const { fromFileName, width, height, toFileName } = event.detail
+            if (fromFileName && toFileName && width && height) {
                 const { Body: contentStream } = await s3Client.send(new GetObjectCommand({
                     Bucket: process.env.UPLOAD_BUCKET,
-                    Key: event.detail?.fromFileName
+                    Key: fromFileName
                 }))
                 const contents = await streamToBuffer(contentStream)
             
                 console.log(`Request to reformat: ${event.detail?.fromFileName}`)
                 try {
-                    const imageBuffer = await jimp.read(contents)
-                    console.log(`Image read: width: ${imageBuffer.bitmap.width}, height: ${imageBuffer.bitmap.height}`)
-                    
+                    const beforeBuffer = await jimp.read(contents)
+                    console.log(`Image read: width: ${beforeBuffer.bitmap.width}, height: ${beforeBuffer.bitmap.height}`)
+                    const afterBuffer = await beforeBuffer.resize(width, height, jimp.RESIZE_BEZIER).deflateLevel(5).getBufferAsync(jimp.MIME_PNG)
+                    await s3Client.send(new PutObjectCommand({
+                        Bucket: process.env.IMAGES_BUCKET,
+                        Key: `${toFileName}.png`,
+                        Body: afterBuffer
+                    }))
+
                 }
                 catch {
                     console.log(`ERROR`)
                 }
                 return JSON.stringify('Success', null, 4)
             }
-            return JSON.stringify(`No fileName specified for Format Image event`)
+            return JSON.stringify(`Invalid arguments specified for Format Image event`)
         }
     }
 }
