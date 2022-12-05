473f55baed3d67e2b6a551113aed78ae189508bc
TonyLB <anthony.lowerbasch@gmail.com>
2022-12-05 08:33:49 -0500

    ISS1089: Added lambda layer giving access to charcoal-client build directory to initialize lambda

--- lambda/initialize/app.ts
+++ lambda/initialize/app.ts
@@ -1,6 +1,7 @@
 // Import required AWS SDK clients and commands for Node.js
 import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
 import type { Readable } from "stream"
+import { readdir } from 'node:fs/promises'
 
 const params = { region: process.env.AWS_REGION }
 const s3Client = new S3Client(params)
@@ -12,6 +13,13 @@ export const handler = async (event, context) => {
     if (event?.source === 'mtw.diagnostics') {
         if (event["detail-type"] === 'Initialize') {
             console.log(`Initializer called`)
+            const dirContents = await readdir('/opt')
+            console.log(`Directory contents: ${JSON.stringify(dirContents, null, 4)}`)
+            // await s3Client.send(new PutObjectCommand({
+            //     Bucket: process.env.CLIENT_BUCKET,
+            //     Key: `Test.txt`,
+            //     Body: 'test'
+            // }))
             return JSON.stringify(`Success`)
         }
     }
