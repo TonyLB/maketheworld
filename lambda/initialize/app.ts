16480d220564a272a0d6ed757208e3659cf090a2
TonyLB <anthony.lowerbasch@gmail.com>
2022-12-05 18:53:31 -0500

    ISS1089: Extended initialize lambda to populate client S3 bucket

--- lambda/initialize/app.ts
+++ lambda/initialize/app.ts
@@ -1,11 +1,43 @@
 // Import required AWS SDK clients and commands for Node.js
-import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
-import type { Readable } from "stream"
-import { readdir } from 'node:fs/promises'
+import { S3Client, ListObjectsCommand, DeleteObjectsCommand, PutObjectCommand } from "@aws-sdk/client-s3"
+import { readdir, stat, readFile } from 'node:fs/promises'
 
 const params = { region: process.env.AWS_REGION }
 const s3Client = new S3Client(params)
 
+const clearClientBucket = async (): Promise<void> => {
+    const objectList = await s3Client.send(new ListObjectsCommand({
+        Bucket: process.env.CLIENT_BUCKET
+    }))
+    if (objectList.Contents) {
+        await s3Client.send(new DeleteObjectsCommand({
+            Bucket: process.env.CLIENT_BUCKET,
+            Delete: {
+                Objects: objectList.Contents.map(({ Key }) => ({ Key }))
+            }
+        }))
+    }
+}
+
+const initializeClientData = async (subDir: string = ''): Promise<void> => {
+    const baseDir = `/opt${subDir}`
+    const dirContents = await readdir(baseDir)
+    await Promise.all(
+        dirContents.map(async (item) => {
+            if ((await stat(`${baseDir}/${item}`)).isDirectory()) {
+                await initializeClientData(`${subDir}/${item}`)
+            }
+            else {
+                const data = await readFile(`${baseDir}/${item}`)
+                await s3Client.send(new PutObjectCommand({
+                    Bucket: process.env.CLIENT_BUCKET,
+                    Key: subDir ? `${subDir.slice(1)}/${item}` : item,
+                    Body: data
+                }))
+            }
+        })
+    )
+}
 
 export const handler = async (event, context) => {
 
@@ -13,13 +45,8 @@ export const handler = async (event, context) => {
     if (event?.source === 'mtw.diagnostics') {
         if (event["detail-type"] === 'Initialize') {
             console.log(`Initializer called`)
-            const dirContents = await readdir('/opt')
-            console.log(`Directory contents: ${JSON.stringify(dirContents, null, 4)}`)
-            // await s3Client.send(new PutObjectCommand({
-            //     Bucket: process.env.CLIENT_BUCKET,
-            //     Key: `Test.txt`,
-            //     Body: 'test'
-            // }))
+            await clearClientBucket()
+            await initializeClientData()
             return JSON.stringify(`Success`)
         }
     }
