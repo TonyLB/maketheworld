a115216a26f0151fd6638fab967093acda01eeff
TonyLB <anthony.lowerbasch@gmail.com>
2022-12-27 21:45:33 -0500

    ISS1936: Added indexFlattenedConditionalNodes to orderedConditionalTree

--- packages/mtw-wml/ts/convert/utils/shortestCommonSupersequence.test.ts
+++ packages/mtw-wml/ts/convert/utils/shortestCommonSupersequence.test.ts
@@ -12,4 +12,7 @@ describe('shortestCommonSupersequence', () => {
     it('should handle repeated entries in the same list', () => {
         expect(shortestCommonSuperSequence([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5], [4, 4, 3, 2])).toEqual([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 3, 2])
     })
+    it('should swap direction back and forth between lists as needed', () => {
+        expect(shortestCommonSuperSequence([1, 2, 3, 4, 5], [3, 2, 1, 4, 3, 2])).toEqual([1, 2, 3, 2, 1, 4, 5, 3, 2])
+    })
 })
