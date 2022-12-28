21735dd1e309b908254f9573104fd23fe694b81b
TonyLB <anthony.lowerbasch@gmail.com>
2022-12-27 21:38:49 -0500

    ISS1936: Added shortestCommonSupersequence utility function

---
+++ packages/mtw-wml/ts/convert/utils/shortestCommonSupersequence.test.ts
@@ -0,0 +1,15 @@
+import shortestCommonSuperSequence from './shortestCommonSupersequence'
+
+describe('shortestCommonSupersequence', () => {
+    it('should return unchanged list when merged with empty list', () => {
+        expect(shortestCommonSuperSequence([], [3, 1, 4, 1, 5, 9])).toEqual([3, 1, 4, 1, 5, 9])
+        expect(shortestCommonSuperSequence([3, 1, 4, 1, 5, 9], [])).toEqual([3, 1, 4, 1, 5, 9])
+    })
+
+    it('should return merger of two lists in common order', () => {
+        expect(shortestCommonSuperSequence([1, 2, 3, 4, 5], [4, 3, 2])).toEqual([1, 2, 3, 4, 5, 3, 2])
+    })
+    it('should handle repeated entries in the same list', () => {
+        expect(shortestCommonSuperSequence([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5], [4, 4, 3, 2])).toEqual([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 3, 2])
+    })
+})
