643c6778fc96d4cdd19dce978d441e3b46592cfd
TonyLB <anthony.lowerbasch@gmail.com>
2023-01-15 17:05:51 -0500

    ISS1995: Added first draft of normalizer put onStabilize

--- charcoal-client/src/components/Maps/Edit/index.tsx
+++ charcoal-client/src/components/Maps/Edit/index.tsx
@@ -15,16 +15,19 @@ import { MapReducer } from './reducer.d'
 import mapReducer from './reducer'
 import { useLibraryAsset } from '../../Library/Edit/LibraryAsset'
 import normalToTree from './normalToTree'
-import { objectEntryMap, objectMap } from '../../../lib/objects'
-import { MapAppearance, isNormalImage } from '@tonylb/mtw-wml/dist/normalize/baseClasses'
+import { deepEqual, objectEntryMap, objectMap } from '../../../lib/objects'
+import { MapAppearance, isNormalImage, isNormalMap, NormalReference } from '@tonylb/mtw-wml/dist/normalize/baseClasses'
 import { InheritedExit } from '../../../slices/personalAssets/inheritedData'
+import Normalizer from '@tonylb/mtw-wml/dist/normalize'
+import { isSchemaMap, isSchemaRoom, SchemaMapLegalContents, SchemaMapRoom, SchemaRoomTag } from '@tonylb/mtw-wml/dist/schema/baseClasses'
+import { extractConditionedItemFromContents } from '@tonylb/mtw-wml/dist/schema/utils'
 
 type MapEditProps = {
 }
 
 export const MapEdit: FunctionComponent<MapEditProps>= () => {
     const localClasses = useMapStyles()
-    const { normalForm, rooms, wmlQuery, updateWML } = useLibraryAsset()
+    const { normalForm, rooms, wmlQuery, updateWML, updateNormal } = useLibraryAsset()
     const { MapId: mapId } = useParams<{ MapId: string }>()
 
     const [toolSelected, setToolSelected] = useState<ToolSelected>('Select')
@@ -33,10 +36,9 @@ export const MapEdit: FunctionComponent<MapEditProps>= () => {
     // put back the inheritedExits and inheritedAppearances functionality in normalToTree that shows
     // locked prior layers of map functionality.
     //
-    const inheritedExits: InheritedExit[] = []
-    const defaultTree = useMemo(() => (
-        normalToTree({ MapId: mapId || '', normalForm, rooms, inheritedExits, inheritedAppearances: [] })
-    ), [normalToTree, mapId, normalForm, rooms, inheritedExits])
+    const defaultTree = useMemo(() => {
+        return normalToTree({ MapId: mapId || '', normalForm, rooms, inheritedExits: [], inheritedAppearances: [] })
+    }, [mapId, normalForm, rooms])
     const [{ tree }, dispatch] = useReducer<MapReducer, MapTree>(
         mapReducer,
         defaultTree,
@@ -68,41 +70,86 @@ export const MapEdit: FunctionComponent<MapEditProps>= () => {
         return images
     }, [normalForm, mapId])
     const onStabilize = useCallback((values) => {
+        console.log(`Stabilize!`)
         const normalMap = normalForm[mapId || '']
-        if (normalMap?.tag === 'Map') {
+        if (mapId && isNormalMap(normalMap)) {
+            let nameSet = false
             const newPositions: Record<string, { x: number; y: number; }> = values.reduce((previous: Record<string, { x: number; y: number }>, { roomId, x, y }: { roomId: string; x: number; y: number }) => ({
                 ...previous,
                 [roomId]: { x: Math.round(x), y: Math.round(y) }
             }), {} as Record<string, { x: number; y: number; }>)
+
             //
-            // TODO: Right now this update is very naive about the possibility of multiple default layers.  Make it smarter as the layer-handling
+            // TODO: Right now this update is very naive about the possibility of multiple layers.  Make it smarter as the layer-handling
             // functionality of MapEdit becomes more sophisticated
             //
-            const locationsToUpdate: Record<string, number[]> = (normalMap.appearances)
-                .filter(({ contextStack = [] }) => (!contextStack.find(({ tag }) => (tag === 'If'))))
-                .reduce((previous, { rooms = [] }) => ({
-                    ...previous,
-                    ...rooms.map(({ location }) => (location))
-                }), {})
-            const updatesNeeded = Object.entries(newPositions)
-                .filter(([roomId]) => (locationsToUpdate[roomId]))
-                .map(([roomId, { x, y }]) => ({
-                    x,
-                    y,
-                    searchString:[
-                        'Asset',
-                        ...(locationsToUpdate[roomId].slice(1).map((index) => (`:nthChild(${index})`)))
-                    ].join('')
-                }))
-            if (updatesNeeded.length) {
-                updatesNeeded.forEach(({ searchString, x, y }) => {
-                    wmlQuery.search(searchString).prop('x', `${x}`).prop('y', `${y}`)
-                })
-                updateWML(wmlQuery.source)
-            }
+            const { tag, appearances = [] } = normalMap
+            const normalizer = new Normalizer()
+            normalizer._normalForm = normalForm
+            appearances.forEach((appearance, index) => {
+                const { contextStack } = appearance
+                const reference: NormalReference = { tag, key: mapId, index }
+                if (!contextStack.find(({ tag }) => (tag === 'If'))) {
+                    console.log(`Reference: ${JSON.stringify(reference, null, 4)}`)
+                    const baseSchema = normalizer.referenceToSchema(reference)
+                    if (isSchemaMap(baseSchema)) {
+                        const roomContents = baseSchema.contents
+                            .map((item) => (isSchemaRoom(item)
+                                ? {
+                                    ...item,
+                                    ...(newPositions[item.key] || {})
+                                } as SchemaRoomTag
+                                : item
+                            ))
+
+                        if (!deepEqual(baseSchema.contents, nameSet ? [] : roomContents)) {
+                            const position = normalizer._referenceToInsertPosition(reference)
+                            console.log(`Map Put position: ${JSON.stringify(position, null, 4)}`)
+                            updateNormal({
+                                type: 'put',
+                                item: {
+                                    ...baseSchema,
+                                    contents: nameSet ? [] : roomContents,
+                                    rooms: nameSet ? [] : extractConditionedItemFromContents({
+                                        contents: roomContents as SchemaMapLegalContents[],
+                                        typeGuard: isSchemaRoom,
+                                        transform: ({ key, x, y }, index) => ({ conditions: [], key, x: x ?? 0, y: y ?? 0, index })
+                                    }),
+                
+                                },
+                                position: { ...position, replace: true },
+                            })
+                        }
+                        nameSet = true
+                    }
+                }
+            })
+    
+            // const locationsToUpdate: Record<string, number[]> = (normalMap.appearances)
+            //     .filter(({ contextStack = [] }) => (!contextStack.find(({ tag }) => (tag === 'If'))))
+            //     .reduce((previous, { rooms = [] }) => ({
+            //         ...previous,
+            //         ...rooms.map(({ location }) => (location))
+            //     }), {})
+            // const updatesNeeded = Object.entries(newPositions)
+            //     .filter(([roomId]) => (locationsToUpdate[roomId]))
+            //     .map(([roomId, { x, y }]) => ({
+            //         x,
+            //         y,
+            //         searchString:[
+            //             'Asset',
+            //             ...(locationsToUpdate[roomId].slice(1).map((index) => (`:nthChild(${index})`)))
+            //         ].join('')
+            //     }))
+            // if (updatesNeeded.length) {
+            //     updatesNeeded.forEach(({ searchString, x, y }) => {
+            //         wmlQuery.search(searchString).prop('x', `${x}`).prop('y', `${y}`)
+            //     })
+            //     updateWML(wmlQuery.source)
+            // }
         }
         
-    }, [normalForm, mapId, wmlQuery, updateWML])
+    }, [normalForm, mapId, updateNormal])
 
     const onAddExit = useCallback(({ to, from }: { to: string; from: string }) => {
         const outgoingQuery = wmlQuery.search(`Room[key="${from}"] Exit[to="${to}"]`).not("If Exit")
