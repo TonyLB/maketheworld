2b213eb47725102fc66f0ca87c810c9e6aaead01
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-04 11:24:13 -0500

    ISS2133: Abstracted Element and Leaf renders into separate component file

--- charcoal-client/src/components/Library/Edit/DescriptionEditor/index.tsx
+++ charcoal-client/src/components/Library/Edit/DescriptionEditor/index.tsx
@@ -1,10 +1,9 @@
-import React, { FunctionComponent, useMemo, useState, useCallback, useEffect, ReactNode, ForwardedRef } from 'react'
+import React, { FunctionComponent, useMemo, useState, useCallback, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import {
     useParams
 } from "react-router-dom"
 import { isKeyHotkey } from 'is-hotkey'
-import { pink, green, blue } from '@mui/material/colors'
 
 import { useSlateStatic, useSlate } from 'slate-react'
 import {
@@ -17,7 +16,7 @@ import {
     Node
 } from 'slate'
 import { withHistory } from 'slate-history'
-import { Slate, Editable, withReact, ReactEditor, RenderElementProps, RenderLeafProps } from 'slate-react'
+import { Slate, Editable, withReact, ReactEditor, RenderElementProps } from 'slate-react'
 
 import {
     Box,
@@ -37,8 +36,6 @@ import LinkIcon from '@mui/icons-material/Link'
 import LinkOffIcon from '@mui/icons-material/LinkOff'
 import BeforeIcon from '@mui/icons-material/Reply'
 import ReplaceIcon from '@mui/icons-material/Backspace'
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn'
-import MoreIcon from '@mui/icons-material/More'
 
 import {
     CustomActionLinkElement,
@@ -54,11 +51,10 @@ import { DescriptionLinkActionChip, DescriptionLinkFeatureChip } from '../../../
 import { getNormalized } from '../../../../slices/personalAssets'
 import useDebounce from '../../../../hooks/useDebounce'
 import { deepEqual } from '../../../../lib/objects'
-import { LabelledIndentBox } from '../LabelledIndentBox'
 import descendantsToRender from './descendantToRender'
-import InlineChromiumBugfix from './InlineChromiumBugfix'
 import descendantsFromRender from './descendantsFromRender'
 import withConditionals from './conditionals'
+import { Element, Leaf } from './components'
 
 interface DescriptionEditorProps {
     inheritedRender?: ComponentRenderItem[];
@@ -112,122 +108,6 @@ const InheritedDescription: FunctionComponent<{ inheritedRender?: ComponentRende
                 }
             })
         }
-    </span>
-}
-
-type SlateIndentBoxProps = {
-    color: Record<number | string, string>;
-    children: any;
-    label: ReactNode;
-}
-
-const SlateIndentBox = React.forwardRef(<T extends SlateIndentBoxProps>({ color, children, label, ...attributes }: T, ref: ForwardedRef<any>) => {
-    return <LabelledIndentBox
-        color={color}
-        label={label}
-        slate
-        { ...attributes }
-        ref={ref}
-    >
-        <InlineChromiumBugfix />
-        {children}
-        <InlineChromiumBugfix />
-    </LabelledIndentBox>
-})
-
-const Element: FunctionComponent<RenderElementProps & { inheritedRender?: ComponentRenderItem[] }> = ({ inheritedRender, ...props }) => {
-    const { attributes, children, element } = props
-    switch(element.type) {
-        case 'featureLink':
-            return <span {...attributes}>
-                <DescriptionLinkFeatureChip tooltipTitle={`Feature: ${element.to}`}>
-                    <InlineChromiumBugfix />
-                    {children}
-                    <InlineChromiumBugfix />
-                </DescriptionLinkFeatureChip>
-            </span>
-        case 'actionLink':
-            return <span {...attributes}>
-                <DescriptionLinkActionChip tooltipTitle={`Action: ${element.to}`}>
-                    <InlineChromiumBugfix />
-                    {children}
-                    <InlineChromiumBugfix />
-                </DescriptionLinkActionChip>
-            </span>
-        case 'before':
-        case 'replace':
-            const highlight = element.type === 'before' ? green : pink
-            return <SlateIndentBox
-                    { ...attributes }
-                    color={highlight}
-                    label={element.type === 'before'
-                        ? <React.Fragment><BeforeIcon sx={{ verticalAlign: "middle", paddingBottom: '0.2em' }} />Before</React.Fragment>
-                        : <React.Fragment><ReplaceIcon sx={{ verticalAlign: "middle", paddingBottom: '0.2em' }} />Replace</React.Fragment>
-                    }
-                >
-                    { children }
-            </SlateIndentBox>
-        case 'ifBase':
-        case 'elseif':
-            return <SlateIndentBox
-                    { ...attributes }
-                    color={blue}
-                    label={<React.Fragment>{element.type === 'ifBase' ? 'If' : 'Else If'} [{element.source}]</React.Fragment>}
-                >
-                    { children }
-            </SlateIndentBox>
-        case 'else':
-            return <SlateIndentBox
-                    {...attributes}
-                    color={blue}
-                    label={<React.Fragment>Else</React.Fragment>}
-                >
-                    { children }
-            </SlateIndentBox>
-        //
-        // TODO: Build InheritedDescription into render base rather than into a Slate Element
-        //
-        // case 'description':
-        //     const interspersedChildren = children.reduce((previous: any, item: any, index: number) => ([
-        //         ...previous,
-        //         ...((index > 0) ? [<br key={`line-break-${index}`} />] : []),
-        //         item
-        //     ]), [] as any[])
-        //     return <span {...attributes}><span contentEditable={false}><InheritedDescription inheritedRender={inheritedRender} /></span>{interspersedChildren}</span>
-        case 'paragraph':
-            //
-            // TODO: Add a decorator that tags paragraph items with "softBR" if they have any text and "explicitBR" if they are followed by
-            // another paragraph ... and then adds a carriage-return icon in the case of "explicitBR" and a <br /> in the case of softBR
-            //
-            return <span {...attributes} >{ children }</span>
-        default: return (
-            <p {...attributes}>
-                {children}
-            </p>
-        )
-    }
-}
-
-const Leaf: FunctionComponent<RenderLeafProps> = ({ attributes, children, leaf }) => {
-    return <span {...attributes}>
-        {children}
-        { (leaf.explicitBR || leaf.softBR) && <span contentEditable={false}>
-            {
-                leaf.explicitBR && <KeyboardReturnIcon
-                    color="primary"
-                    fontSize="small"
-                    sx={{ verticalAlign: 'middle' }}
-                />
-            }
-            {
-                (leaf.softBR && !leaf.explicitBR) && <MoreIcon
-                    color="primary"
-                    fontSize="small"
-                    sx={{ verticalAlign: 'middle', transform: "rotate(-0.25turn)" }}
-                />
-            }
-            <br />
-        </span> }
     </span>
 }
 
