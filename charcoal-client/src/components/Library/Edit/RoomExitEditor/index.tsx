e02a5b5a53a93000662df8632323e2182ecda7da
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-08 18:37:43 -0500

    ISS2128: Added delete and flip functionality to RoomExitEditor

--- charcoal-client/src/components/Library/Edit/RoomExitEditor/index.tsx
+++ charcoal-client/src/components/Library/Edit/RoomExitEditor/index.tsx
@@ -1,14 +1,19 @@
 import Box from "@mui/material/Box"
+import Chip from "@mui/material/Chip"
+import IconButton from "@mui/material/IconButton"
 import { blue } from "@mui/material/colors"
 import { isNormalExit, NormalExit } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
 import React, { FunctionComponent, useCallback, useMemo } from "react"
-import { createEditor, Descendant } from "slate"
+import { createEditor, Descendant, Node, Transforms } from "slate"
 import { withHistory } from "slate-history"
-import { Editable, RenderElementProps, Slate, withReact } from "slate-react"
+import { Editable, ReactEditor, RenderElementProps, Slate, useSlate, withReact } from "slate-react"
 import { reduceItemsToTree } from "../conditionTree"
 import { useLibraryAsset } from "../LibraryAsset"
 import SlateIfElse from "../SlateIfElse"
 import exitTreeToSlate from "./exitTreeToSlate"
+import ExitIcon from '@mui/icons-material/CallMade'
+import DeleteIcon from '@mui/icons-material/Delete'
+import FlipIcon from '@mui/icons-material/Loop'
 
 type RoomExitEditorProps = {
     RoomId: string;
@@ -27,17 +32,54 @@ const Leaf = ({ attributes, children, leaf }: { attributes: any, children: any, 
 }
 
 const Element: FunctionComponent<RenderElementProps & { RoomId: string }> = ({ RoomId, ...props }) => {
+    const editor = useSlate()
     const { attributes, children, element } = props
+    const path = useMemo(() => (ReactEditor.findPath(editor, element)), [editor, element])
+    const onDeleteHandler = useCallback(() => {
+        Transforms.removeNodes(editor, { at: path })
+    }, [editor, path])
+    const onFlipHandler = useCallback(() => {
+        if (element.type === 'exit') {
+            Transforms.setNodes(editor, { to: element.from, from: element.to }, { at: path })
+        }
+    }, [element, editor, path])
     switch(element.type) {
         case 'ifBase':
         case 'elseif':
         case 'else':
             return <SlateIfElse { ...props } />
         case 'exit':
-            return <Box sx={{ width: "100%", display: "inline-flex", flexDirection: "row" }} { ...attributes }>
-                <Box contentEditable={false} sx={{ display: 'inline-block' }}>Exit [</Box>
-                {children}
-                <Box contentEditable={false} sx={{ display: 'inline-block' }}>] from { element.from } to { element.to }</Box>
+            const hereChip = <Chip icon={<FlipIcon />} label="here" onClick={onFlipHandler} />
+            const fromElement = (element.from === RoomId)
+                ? hereChip
+                : element.from
+            const toElement = (element.to === RoomId)
+                ? hereChip
+                : element.to
+            return <Box sx={{
+                width: "calc(100% - 0.5em)",
+                display: "inline-flex",
+                flexDirection: "row",
+                borderRadius: '0.5em',
+                padding: '0.1em',
+                margin: '0.25em',
+                alignItems: "center"
+            }}>
+                <Box contentEditable={false} sx={{ display: 'flex', marginRight: '0.5em' }} ><ExitIcon sx={{ fill: "grey" }} /></Box>
+                <Box sx={{
+                    display: 'flex',
+                    minWidth: '12em',
+                    borderRadius: '0.25em',
+                    borderStyle: "solid",
+                    borderWidth: '0.5px',
+                    borderColor: 'grey',
+                    backgroundColor: "white",
+                    padding: '0.1em',
+                    paddingLeft: '0.25em',
+                    paddingRight: '0.25em'
+                }} { ...attributes }>{children}</Box>
+                <Box contentEditable={false} sx={{ display: 'flex', flexGrow: 1 }} > from { fromElement } to { toElement }</Box>
+                <Box contentEditable={false} sx={{ display: 'flex' }} ><IconButton onClick={onDeleteHandler}><DeleteIcon /></IconButton></Box>
             </Box>
         default: return (
             <p {...attributes}>
