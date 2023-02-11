622405b07a53d6fe18629d8e80629bde1d9658d9
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-11 15:44:16 -0500

    ISS2178: Created different render modes for CurrentDraftSelector

--- charcoal-client/src/components/LineEntry/CurrentDraftSelector.tsx
+++ charcoal-client/src/components/LineEntry/CurrentDraftSelector.tsx
@@ -6,15 +6,18 @@ import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
 import InputLabel from "@mui/material/InputLabel"
 import CreateIcon from '@mui/icons-material/Create'
+import CancelIcon from '@mui/icons-material/HighlightOff'
 import { useDispatch, useSelector } from "react-redux"
 import { getPlayer, setCurrentDraft } from "../../slices/player"
+import { getStatus, setIntent } from "../../slices/personalAssets"
 
-export const CurrentDraftSelector: FunctionComponent<{}> = () => {
-    const { currentDraft, Assets } = useSelector(getPlayer)
-    const dispatch = useDispatch()
+const InactiveDraftWrapper: FunctionComponent<{}> = ({ children }) => {
     return <Box sx={{
         height: 50,
-        bgcolor: blue[500],
+        bgcolor: blue[100],
+        borderColor: blue[500],
+        borderStyle: 'solid',
+        borderWidth: '1px',
         borderRadius: 25,
         alignItems: 'center',
         display: 'flex',
@@ -23,6 +26,70 @@ export const CurrentDraftSelector: FunctionComponent<{}> = () => {
         paddingLeft: '0.5em',
         paddingRight: '0.5em'
     }}>
+        { children}
+    </Box>
+}
+
+const ActiveDraftWrapper: FunctionComponent<{ currentDraft: string }> = ({ children, currentDraft }) => {
+    const status = useSelector(getStatus(currentDraft))
+    const dispatch = useDispatch()
+    if (['FRESH', 'WMLDIRTY', 'NORMALDIRTY'].includes(status || '')) {
+        return <Box sx={{
+            height: 50,
+            bgcolor: blue[500],
+            borderRadius: 25,
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'row',
+            columnGap: '0.5em',
+            paddingLeft: '0.5em',
+            paddingRight: '0.5em'
+        }}>
+            { children}
+        </Box>
+    }
+    else {
+        return <Box sx={{
+            height: 50,
+            background: (theme: any) => (theme.palette.extras.stripedGradient),
+            backgroundBlendMode: 'multiply',
+            borderColor: blue[500],
+            borderStyle: 'solid',
+            borderWidth: '1px',
+            borderRadius: 25,
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'row',
+            columnGap: '0.5em',
+            paddingLeft: '0.5em',
+            paddingRight: '0.5em'
+        }}>
+            <CreateIcon
+                sx={{ width:40, height: 50, fill: blue[200] }}
+            />
+            Loading
+            <CancelIcon
+                fontSize="small"
+                onClick={() => { dispatch(setCurrentDraft(undefined)) }}
+                sx={{ fill: blue[500] }}
+            />
+        </Box>
+    }
+}
+
+const CurrentDraftWrapper: FunctionComponent<{ currentDraft: string | undefined }> = ({ children, currentDraft }) => {
+    if (currentDraft) {
+        return <ActiveDraftWrapper currentDraft={`ASSET#${currentDraft}`}>{ children }</ActiveDraftWrapper>
+    }
+    else {
+        return <InactiveDraftWrapper>{ children }</InactiveDraftWrapper>
+    }
+}
+
+export const CurrentDraftSelector: FunctionComponent<{}> = () => {
+    const { currentDraft, Assets } = useSelector(getPlayer)
+    const dispatch = useDispatch()
+    return <CurrentDraftWrapper currentDraft={currentDraft}>
         <CreateIcon
             sx={{ width:40, height: 50, fill: 'white' }}
         />
@@ -33,7 +100,9 @@ export const CurrentDraftSelector: FunctionComponent<{}> = () => {
                 id="select-small"
                 value={currentDraft || ''}
                 label="Edit Target"
-                onChange={(event) => { dispatch(setCurrentDraft(event.target.value)) }}
+                onChange={(event) => {
+                    dispatch(setCurrentDraft(event.target.value))
+                }}
             >
                 <MenuItem value={""}><em>Not editing</em></MenuItem>
                 {
@@ -43,7 +112,7 @@ export const CurrentDraftSelector: FunctionComponent<{}> = () => {
                 }
             </Select>
         </FormControl>
-    </Box>
+    </CurrentDraftWrapper>
 }
 
 export default CurrentDraftSelector
