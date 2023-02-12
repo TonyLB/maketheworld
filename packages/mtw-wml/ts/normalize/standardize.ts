51871e3a82c47a36b86eb4d5120ad3a8677fd78a
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-12 15:49:37 -0500

    ISS2191: Added README.standardize.md to mtw-wml to document standard normal form

---
+++ packages/mtw-wml/ts/normalize/standardize.ts
@@ -0,0 +1,12 @@
+//
+// standardizeNormal creates a new NormalForm with the exact same semantic content as the input,
+// but with everything organized in a standard structure (see README.standardize.md)
+//
+
+import { NormalForm } from "./baseClasses";
+
+export const standardizeNormal = (normal: NormalForm): NormalForm => {
+    return {}
+}
+
+export default standardizeNormal
