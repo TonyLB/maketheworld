40712bd10aab313e2fafbfa2feb666aac57b34f6
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-14 18:22:15 -0500

    ISS2191: Added normalSubset to extract relevant data from fetched normal

--- lambda/assets/fetchImportDefaults/index.ts
+++ lambda/assets/fetchImportDefaults/index.ts
@@ -3,7 +3,7 @@ import { FetchImportDefaultsMessage, FetchImportsMessage, MessageBus } from "../
 import internalCache from '../internalCache'
 import { AssetClientImportDefaults, AssetClientImportDefaultsFeature, AssetClientImportDefaultsRoom } from "@tonylb/mtw-interfaces/dist/asset"
 import { apiClient } from "@tonylb/mtw-utilities/dist/apiManagement/apiManagementClient"
-import { ComponentRenderItem, isNormalExit, isNormalFeature, isNormalImport, isNormalRoom, NormalComponent, NormalItem } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
+import { ComponentRenderItem, isNormalExit, isNormalFeature, isNormalImport, isNormalRoom, NormalComponent, NormalForm, NormalItem } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
 import { unique } from "@tonylb/mtw-utilities/dist/lists"
 import { RoomExit } from "@tonylb/mtw-interfaces/dist/messages"
 import { isEphemeraRoomId } from "@tonylb/mtw-interfaces/dist/baseClasses"
@@ -14,6 +14,17 @@ import AssetWorkspace from "@tonylb/mtw-asset-workspace"
 import { SchemaTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
 import { splitType } from "@tonylb/mtw-utilities/dist/types"
 import { componentRenderToSchemaTaggedMessage } from "@tonylb/mtw-wml/dist/normalize"
+import Normalizer from "@tonylb/mtw-wml/dist/normalize"
+import { isSchemaFeature } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaRoom } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaFeatureTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaRoomTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaRoomLegalContents } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaExit } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaCondition } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaConditionTagRoomContext } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaConditionTagRoomContext } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import normalSubset from "./normalSubset"
 
 const { S3_BUCKET } = process.env
 
@@ -188,156 +199,26 @@ type RecursiveFetchImportArgument = {
     stubKeys: string[];
 }
 
-type RecursiveFetchSelectReturnKeyItem = {
-    key: string; tag: 'Room' | 'Feature';
-    name: ComponentRenderItem[];
-    render: ComponentRenderItem[];
-}
-
-type RecursiveFetchSelectReturnStubItem = {
-    key: string; tag: 'Room' | 'Feature';
-    name: ComponentRenderItem[];
-    render: null;
-}
-
-type RecursiveFetchSelectReturnExitItem = {
-    key: string;
-    tag: 'Exit';
-    exitName: string;
-}
-
-type RecursiveFetchSelectReturn = RecursiveFetchSelectReturnKeyItem | RecursiveFetchSelectReturnStubItem | RecursiveFetchSelectReturnExitItem
-
-const isRecursiveFetchSelectReturnKeyItem = (value: RecursiveFetchSelectReturn): value is RecursiveFetchSelectReturnKeyItem => {
-    if (value.tag === 'Room' || value.tag === 'Feature') {
-        if (Array.isArray(value.render)) {
-            return true
-        }
-    }
-    return false
-}
-
-const isRecursiveFetchSelectReturnStubItem = (value: RecursiveFetchSelectReturn): value is RecursiveFetchSelectReturnStubItem => {
-    if (value.tag === 'Room' || value.tag === 'Feature') {
-        if (!Array.isArray(value.render)) {
-            return true
-        }
-    }
-    return false
-}
-
-const isRecursiveFetchSelectReturnExitItem = (value: RecursiveFetchSelectReturn): value is RecursiveFetchSelectReturnExitItem => (value.tag === 'Exit')
-
 export const recursiveFetchImports = async ({ payload, messageBus }: { payload: Record<AssetKey, RecursiveFetchImportArgument>, messageBus: MessageBus }): Promise<void> => {
-
-    //
-    // TODO:  ABORT!  ABORT!  Using S3Select for data as intricately interwoven as the NormalForm files is an exercise in frustration
-    // and inefficiency.  Until and unless I can think of a cleaner way to represent the data that would allow for easy
-    // disentangling of a specific subsection of the NormalForm to deliver, return to the original plan of pulling the JSON file
-    // from the internalCache and applying `normalSubset` to the (now guaranteed standardized) file format to pull out the
-    // relevant data.
-    //
-
-    const s3Client = await internalCache.Connection.get('s3Client')
-    if (!s3Client) {
-        return
-    }
-    const fetches = Object.assign({}, ...Object.values(payload).map(async ({ assetId, keys, stubKeys }): Promise<Record<string, SchemaTag[]>> => {
-        const assetWorkspace = await assetWorkspaceFromAssetId(assetId)
-        if (!assetWorkspace) {
-            return { [assetId]: [] }
-        }
-        const keysExpression = `(${keys.map((key) => (`'${key}'`)).join(',')})`
-        const selectExpression = `SELECT
-                _1."key", _1.tag, _1.name as exitName, _1.appearances[0].name,
-                CASE _1."key" IN ${keysExpression} WHEN true THEN _1.appearances[0].render END as render,
-                CASE (_1."key" IN ${keysExpression}) AND (_1.tag = 'Room') WHEN true THEN _1.appearances[0].contents END as contents
-            FROM s3Object[*].normal.*
-            WHERE
-                (_1."key" IN ${keysExpression}) OR
-                (_1.tag  = 'Room') OR
-                ((_1.tag = 'Exit') AND (_1."from" IN ${keysExpression} OR _1."to" in ${keysExpression}))`
-        const params = {
-            Bucket: S3_BUCKET,
-            Key: `${assetWorkspace.fileNameBase}.json`,
-            ExpressionType: 'SQL',
-            Expression: selectExpression,
-            InputSerialization: {
-                JSON: { Type: 'Document' }
-            },
-            OutputSerialization: {
-                JSON: { RecordDelimiter: ',' }
-            }
-        }
-        const s3SelectResponse = await s3Client.send(new SelectObjectContentCommand(params))
+    const fetches = await Promise.all(Object.values(payload).map(async ({ assetId, keys, stubKeys }) => {
+        const { normal } = await internalCache.JSONFile.get(assetId)
         //
-        // TODO: Better error handling
+        // Coming straight from the datalake, this normal should already be in standardized form,
+        // and can be fed directly to normalSubset
         //
-        if (s3SelectResponse.$metadata.httpStatusCode === 200 && s3SelectResponse.Payload) {
-            const convertedData = (await convertSelectDataToJson(s3SelectResponse.Payload)) as RecursiveFetchSelectReturn[]
-            const importItemByKey = convertedData.reduce<Record<string, RecursiveFetchSelectReturn>>((previous, item) => ({
-                ...previous,
-                [item.key]: item
-            }), {})
-            const keyImportItems = keys.map((key) => (importItemByKey[key])).filter(isRecursiveFetchSelectReturnKeyItem)
-            //
-            // TODO: Create a recursive tree-walker to pull out all conditional keys from the key item contents.
-            //
-            const exitItems = Object.values(importItemByKey)
-                .filter(isRecursiveFetchSelectReturnExitItem)
-                .map(({ key, exitName }) => {
-                    const [from, to] = splitType(key)
-                    return {
-                        key,
-                        from,
-                        to,
-                        name: exitName
-                    }
-                })
-            const exitStubs = [
-                    ...exitItems.map(({ to }) => (to)),
-                    ...exitItems.map(({ from }) => (from)),
-                ]
-            const allStubs = (unique(stubKeys, exitStubs) as string[]).filter((value) => (!keys.includes(value)))
-            //
-            // TODO: Use the list of allStubs and allIfs to run a second Select through the S3 file to get
-            // information needed to support the key elements.
-            //
-            const schemaTags: SchemaTag[] = [
-                ...keyImportItems.map(({ key, name, render }): SchemaTag => ({
-                    tag: 'Room',
-                    key,
-                    name: name.map(componentRenderToSchemaTaggedMessage),
-                    render: render.map(componentRenderToSchemaTaggedMessage),
-                    global: false,
-                    contents: []
-                })),
-                ...allStubs.map((key): SchemaTag => {
-                    
-                })
-            ]
 
-            return {}
+        //
+        // TODO: Extend normalSubset to return an updated list of stubKeys, in addition to
+        // the SchemaTag main output, and use that to determine what needs to be looked up
+        // as possible imports from a different asset, to fill out yet more of the
+        // layers of inheritance.
+        //
+        return {
+            [assetId]: normalSubset({ normal, keys, stubKeys })
         }
-        else {
-            return { [assetId]: [] }
-        }
-    
-    //
-    // TODO: Parse items to determine whether the keys require any stubs not yet present
-    //
+    }))
 
-    //
-    // TODO: Recurse to fetch stubs that are required but have not yet been fetched, either
-    // those passed as stubKeys, or those added to the necessary stubs by parsing of keys
-    //
-
-    //
-    // TODO: Once stubs are fetched, you know all further imports that are necessary. Recurse
-    // to fetch those as well
-    //
-    })
-
+    return Object.assign({}, ...fetches)
 }
 
 export const fetchImportsMessage = async ({ payloads, messageBus }: { payloads: FetchImportsMessage[], messageBus: MessageBus }): Promise<void> => {
--- lambda/assets/fetchImportDefaults/normalSubset.ts
+++ lambda/assets/fetchImportDefaults/normalSubset.ts
@@ -1,13 +1,115 @@
 //
-// normalSubset takes a NormalForm and a set of keys for Rooms, Features, Bookmarks, and Maps,
-// and returns a *subset* NormalForm that contains a listing of only those things needed in order
-// to render the structure of the object specified by the given keys
+// normalSubset takes a standardized NormalForm and a set of keys for Rooms, Features, Bookmarks,
+// and Maps, and returns a *subset* list of SchemaTags that contains a listing of only those things
+// needed in order to render the structure of the object specified by the given keys
 //
 
-import { NormalForm } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
+import Normalizer from "@tonylb/mtw-wml/dist/normalize"
+import { isNormalExit, NormalForm, NormalItem } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
+import { isSchemaCondition } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaFeatureTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaFeature } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaRoom } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaRoomTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaConditionTagRoomContext } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaConditionTagRoomContext } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { isSchemaExit } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaRoomLegalContents } from "@tonylb/mtw-wml/dist/schema/baseClasses"
+import { SchemaTag } from "@tonylb/mtw-wml/dist/schema/baseClasses"
 
-export const normalSubset = (normal: NormalForm, keys: string[]): NormalForm => {
-    return {}
+//
+// RecursiveRoomContentsFilter filters all contents of a room to include only the exits that are relevant to the
+// slice of the asset being examined. It dives into conditions to do this, and excludes conditions that have
+// no contents
+//
+const recursiveRoomContentsFilter = ({ contents, keys }: { contents: SchemaRoomLegalContents[], keys: string[] }): SchemaRoomLegalContents[] => {
+    return contents.reduce<SchemaRoomLegalContents[]>((previous, item) => {
+        if (isSchemaExit(item) && (keys.includes(item.from) || keys.includes(item.to))) {
+            return [
+                ...previous,
+                item
+            ]
+        }
+        if (isSchemaCondition(item) && isSchemaConditionTagRoomContext(item)) {
+            const conditionContents = recursiveRoomContentsFilter({ contents: item.contents, keys })
+            if (conditionContents.length) {
+                const conditionRecurse: SchemaConditionTagRoomContext = {
+                    tag: 'If',
+                    contextTag: 'Room',
+                    conditions: item.conditions,
+                    contents: conditionContents
+                }
+                return [
+                    ...previous,
+                    conditionRecurse
+                ]
+            }
+        }
+        return previous
+    }, [])
+}
+
+export const normalSubset = ({ normal, keys, stubKeys }: { normal: NormalForm, keys: string[], stubKeys: string[] }): SchemaTag[] => {
+    const normalizer = new Normalizer()
+    normalizer.loadNormal(normal)
+    //
+    // Extend the incoming stubKeys with any that need to be added because of connection to first-class
+    // keys
+    //
+    const newStubKeys = Object.values(normal)
+        .filter(isNormalExit)
+        .filter(({ from, to }) => (keys.includes(from) || keys.includes(to)))
+        .map(({ from, to }) => [from, to])
+        .flat()
+        .filter((key) => (!keys.includes(key)))
+    const allStubKeys = [...stubKeys, ...newStubKeys]
+
+    //
+    // Identify tags for all the keys, to facilitate looking them up by reference
+    //
+    const keyTags: Record<string, NormalItem["tag"]> = [...keys, ...allStubKeys].reduce((previous, key) => {
+        const tag = normal[key]?.tag
+        return tag ? { ...previous, [key]: tag } : previous
+    }, {})
+    //
+    // Generate the full schema items for keys and stubs that match against this asset
+    //
+    const allItems: Record<string, SchemaTag> = Object.assign({}, ...[...keys, ...allStubKeys]
+        .map((key): [string, NormalItem["tag"]] => ([key, keyTags[key]]))
+        .filter(([_, tag]) => (tag))
+        .map(([key, tag]) => ({ [key]: normalizer.referenceToSchema({ key, tag, index: 0 }) }))
+    )
+        
+    //
+    // Redact the schema items for stubs that match against this asset (since we won't need their
+    // renders, or exits to non-key items)
+    //
+    const stubItems = allStubKeys
+        .map((key) => (allItems[key]))
+        .filter((value) => (value))
+        .filter((item: SchemaTag): item is SchemaFeatureTag | SchemaRoomTag => (isSchemaFeature(item) || isSchemaRoom(item)))
+        .map((item) => {
+            if (isSchemaRoom(item)) {
+                return {
+                    ...item,
+                    render: [],
+                    contents: recursiveRoomContentsFilter({ contents: item.contents, keys })
+                }
+            }
+            else {
+                return {
+                    ...item,
+                    render: [],
+                    contents: []
+                }
+            }
+        })
+
+    const keyItems = Object.entries(allItems)
+        .filter(([key]) => (keys.includes(key)))
+        .map(([_, item]) => (item))
+
+    return [...keyItems, ...stubItems]
 }
 
 export default normalSubset
