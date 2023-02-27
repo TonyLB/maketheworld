d729fb8ac6d813fce11a08649b5ce4baa3b1ffd7
TonyLB <anthony.lowerbasch@gmail.com>
2023-02-26 20:12:53 -0500

    ISS2234: Replaced direct connectionDB call with internalCache call

--- lambda/assets/libraryUpdate/index.ts
+++ lambda/assets/libraryUpdate/index.ts
@@ -2,21 +2,15 @@ import { apiClient } from '@tonylb/mtw-utilities/dist/apiManagement/apiManagemen
 import { LibraryUpdateMessage, MessageBus } from "../messageBus/baseClasses"
 
 import internalCache from '../internalCache'
-import { connectionDB } from '@tonylb/mtw-utilities/dist/dynamoDB'
 
 export const libraryUpdateMessage = async ({ payloads, messageBus }: { payloads: LibraryUpdateMessage[], messageBus: MessageBus }): Promise<void> => {
     internalCache.Library.clear()
-    const [Characters, Assets, connectionFetch] = await Promise.all([
+    const [Characters, Assets, ConnectionIds] = await Promise.all([
         internalCache.Library.get('Characters'),
         internalCache.Library.get('Assets'),
-        connectionDB.getItem<{ ConnectionIds: string[] }>({
-            ConnectionId: 'Library',
-            DataCategory: 'Subscriptions',
-            ProjectionFields: ['ConnectionIds']
-        })
+        internalCache.Connection.get('librarySubscriptions')
     ])
-    const { ConnectionIds = [] } = connectionFetch || {}
-    await Promise.all(ConnectionIds.map((ConnectionId) => (
+    await Promise.all((ConnectionIds || []).map((ConnectionId) => (
         apiClient.send({
             ConnectionId,
             Data: JSON.stringify({
