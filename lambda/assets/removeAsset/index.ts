69ed4fdd5ebe3542722b0f444e26fbbb8c3f9cca
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-05 16:13:48 -0400

    ISS2672: Refactored RemoveAsset to update ephemera items derived from player

--- lambda/assets/removeAsset/index.ts
+++ lambda/assets/removeAsset/index.ts
@@ -2,13 +2,19 @@ import { RemoveAssetMessage, MessageBus } from "../messageBus/baseClasses"
 
 import { assetDB } from "@tonylb/mtw-utilities/dist/dynamoDB"
 import { AssetKey } from "@tonylb/mtw-utilities/dist/types"
+import internalCache from "../internalCache"
+import { healPlayer } from "../selfHealing/player"
 
 export const removeAssetMessage = async ({ payloads, messageBus }: { payloads: RemoveAssetMessage[], messageBus: MessageBus }): Promise<void> => {
     await Promise.all(payloads.map(async ({ assetId }) => {
-        await assetDB.deleteItem({
-            AssetId: AssetKey(assetId),
-            DataCategory: 'Meta::Asset'
-        })
+        const { address } = await internalCache.Meta.get(`ASSET#${assetId}`)
+        await Promise.all([
+            assetDB.deleteItem({
+                AssetId: AssetKey(assetId),
+                DataCategory: 'Meta::Asset'
+            }),
+            ...(address && address.zone === 'Personal' ? [healPlayer(address.player)] : [])
+        ])
         console.log(`Received Remove Asset message for ${assetId}`)
     }))
 }
