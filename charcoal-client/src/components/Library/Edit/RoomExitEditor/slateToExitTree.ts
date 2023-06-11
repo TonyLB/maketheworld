0398f6a0ec1393c0aaa258c33a496fb59b9ae3e2
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-11 10:14:23 -0400

    ISS2561: Extracted slate to exit reducer as separate function

--- charcoal-client/src/components/Library/Edit/RoomExitEditor/slateToExitTree.ts
+++ charcoal-client/src/components/Library/Edit/RoomExitEditor/slateToExitTree.ts
@@ -17,70 +17,72 @@ const slateToSourceRooms = (nodes: CustomBlock[]): string[] => {
     }, [])
 }
 
+const slateToExitReducer = (nodes: CustomBlock[], currentElseConditions: NormalConditionStatement[]) => (previous, roomId) => ({
+    ...previous,
+    [roomId]: nodes.reduce<SchemaTag[]>((accumulator, node) => {
+        if (isCustomIfBlock(node) || isCustomElseIfBlock(node)) {
+            const conditionStatement = {
+                if: node.source,
+                dependencies: extractDependenciesFromJS(node.source)
+            }
+            if (isCustomIfBlock(node)) {
+                currentElseConditions = []
+            }
+            currentElseConditions = [...currentElseConditions, { ...conditionStatement, not: true }]
+            const childrenSchema = slateToExitSchema(node.children, { roomId })
+            const contents = (childrenSchema?.[roomId] || []).filter((child: SchemaTag): child is SchemaExitTag | SchemaConditionTagRoomContext => (isSchemaExit(child) || isSchemaCondition(child)))
+            if (contents.length) {
+                return [
+                    ...accumulator,
+                    {
+                        tag: 'If' as 'If',
+                        contextTag: 'Room',
+                        conditions: [...currentElseConditions.slice(0, -1), conditionStatement],
+                        contents
+                    }
+                ]
+            }
+        }
+        if (isCustomElseBlock(node)) {
+            const contents = slateToExitSchema(node.children, { roomId })[roomId].filter((child: SchemaTag): child is SchemaExitTag | SchemaConditionTagRoomContext => (isSchemaExit(child) || isSchemaCondition(child)))
+            if (contents.length) {
+                const returnElse: SchemaConditionTagRoomContext = {
+                    tag: 'If' as 'If',
+                    contextTag: 'Room',
+                    conditions: currentElseConditions,
+                    contents
+                }
+                currentElseConditions = []
+                return [
+                    ...accumulator,
+                    returnElse
+                ]
+            }
+        }
+        if (isCustomExitBlock(node) && node.from === roomId && node.to && Node.string(node)) {
+            return [
+                ...accumulator,
+                {
+                    tag: 'Exit' as 'Exit',
+                    key: `${node.from}#${node.to}`,
+                    from: node.from,
+                    to: node.to,
+                    name: Node.string(node),
+                    contents: []
+                }
+            ]
+        }
+        return accumulator
+    }, [])
+})
+
 export const slateToExitSchema = (nodes: CustomBlock[], options?: { roomId?: string }): Record<string, SchemaTag[]> => {
     //
     // TODO: Reduce a set of exitEditor nodes to a list of SchemaTags
     //
     let currentElseConditions: NormalConditionStatement[] = []
     const allSourceRooms = options?.roomId ? [options?.roomId] : slateToSourceRooms(nodes)
-    return allSourceRooms.reduce<Record<string, SchemaTag[]>>((previous, roomId) => ({
-        ...previous,
-        [roomId]: nodes.reduce<SchemaTag[]>((accumulator, node) => {
-            if (isCustomIfBlock(node) || isCustomElseIfBlock(node)) {
-                const conditionStatement = {
-                    if: node.source,
-                    dependencies: extractDependenciesFromJS(node.source)
-                }
-                if (isCustomIfBlock(node)) {
-                    currentElseConditions = []
-                }
-                currentElseConditions = [...currentElseConditions, { ...conditionStatement, not: true }]
-                const childrenSchema = slateToExitSchema(node.children, { roomId })
-                const contents = (childrenSchema?.[roomId] || []).filter((child: SchemaTag): child is SchemaExitTag | SchemaConditionTagRoomContext => (isSchemaExit(child) || isSchemaCondition(child)))
-                if (contents.length) {
-                    return [
-                        ...accumulator,
-                        {
-                            tag: 'If' as 'If',
-                            contextTag: 'Room',
-                            conditions: [...currentElseConditions.slice(0, -1), conditionStatement],
-                            contents
-                        }
-                    ]
-                }
-            }
-            if (isCustomElseBlock(node)) {
-                const contents = slateToExitSchema(node.children, { roomId })[roomId].filter((child: SchemaTag): child is SchemaExitTag | SchemaConditionTagRoomContext => (isSchemaExit(child) || isSchemaCondition(child)))
-                if (contents.length) {
-                    const returnElse: SchemaConditionTagRoomContext = {
-                        tag: 'If' as 'If',
-                        contextTag: 'Room',
-                        conditions: currentElseConditions,
-                        contents
-                    }
-                    currentElseConditions = []
-                    return [
-                        ...accumulator,
-                        returnElse
-                    ]
-                }
-            }
-            if (isCustomExitBlock(node) && node.from === roomId && node.to && Node.string(node)) {
-                return [
-                    ...accumulator,
-                    {
-                        tag: 'Exit' as 'Exit',
-                        key: `${node.from}#${node.to}`,
-                        from: node.from,
-                        to: node.to,
-                        name: Node.string(node),
-                        contents: []
-                    }
-                ]
-            }
-            return accumulator
-        }, [])
-    }), {})
+    return allSourceRooms.reduce<Record<string, SchemaTag[]>>(slateToExitReducer(nodes, currentElseConditions), {})
 }
 
 export default slateToExitSchema
