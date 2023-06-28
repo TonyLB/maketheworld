b50c2725dfc01b0545da99f1e996116811d7b307
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-28 19:05:59 -0400

    ISS2707: Added directionality and node utilities to graph class

--- packages/mtw-utilities/ts/graphStorage/utils/graph/index.ts
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/index.ts
@@ -1,15 +1,48 @@
 import { GraphEdge } from "./baseClasses";
 
-//
-// TODO: Add directionality (true/false) to the class, and a "edges" getter for nodes that gives
-// the edges connected to that node (also a "backEdges" getter for nodes in a directed graph)
-//
+export class GraphNode <K extends string, T extends { key: K } & Record<string, any>> {
+    node?: T
+    _edges: GraphEdge<K>[]
+    _directional: boolean;
+    _key: K;
+
+    constructor(graph: Graph<K, T>, key: K) {
+        this._key = key
+        this.node = graph.nodes[key]
+        this._edges = [...graph.edges]
+        this._directional = graph.directional
+    }
+
+    get edges(): GraphEdge<K>[] {
+        return this._edges.filter(({ from, to }) => (from === this._key || ((!this._directional) && to === this._key)))
+    }
+
+    get backEdges(): GraphEdge<K>[] {
+        return this._edges.filter(({ from, to }) => (to === this._key || ((!this._directional) && from === this._key)))
+    }
+
+}
+
 export class Graph <K extends string, T extends { key: K } & Record<string, any>> {
     nodes: Record<K, T>
     edges: GraphEdge<K>[]
+    directional: boolean;
 
-    constructor(nodes: Record<K, T>, edges: GraphEdge<K>[]) {
+    constructor(nodes: Record<K, T>, edges: GraphEdge<K>[], directional: boolean = false) {
         this.nodes = { ...nodes }
         this.edges = [...edges]
+        this.directional = directional
+    }
+
+    getNode(key: K): GraphNode<K, T> | undefined {
+        const nodeCheck = new GraphNode<K, T>(this, key)
+        if (nodeCheck.node) {
+            return nodeCheck
+        }
+        return undefined
+    }
+
+    setNode(key: K, node: T): void {
+        this.nodes[key] = node
     }
 }
