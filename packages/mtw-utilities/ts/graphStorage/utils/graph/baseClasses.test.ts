6c8db46e6816dd8a98e3cf9b3d56a791f06c2b2b
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-28 19:09:53 -0400

    ISS2707: Added compareEdges sort function to graph class

---
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/baseClasses.test.ts
@@ -0,0 +1,20 @@
+import { GraphEdge, compareEdges } from './baseClasses'
+
+describe('graph base utilities', () => {
+    describe('compareEdges', () => {
+        it('should sort edges properly', () => {
+            const testEdges: GraphEdge<string>[] = [
+                { from: 'C', to : 'B' },
+                { from: 'A', to: 'B' },
+                { from: 'B', to: 'D' },
+                { from: 'A', to: 'C' }
+            ]
+            expect(testEdges.sort(compareEdges)).toEqual([
+                { from: 'A', to: 'B' },
+                { from: 'A', to: 'C' },
+                { from: 'B', to: 'D' },
+                { from: 'C', to: 'B' }
+            ])
+        })
+    })
+})
--- packages/mtw-utilities/ts/graphStorage/utils/graph/baseClasses.ts
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/baseClasses.ts
@@ -2,3 +2,8 @@ export type GraphEdge <K extends string> = {
     from: K;
     to: K;
 }
+
+export const compareEdges = <K extends string>(A: GraphEdge<K>, B: GraphEdge<K>): number => {
+    const fromCompare = A.from.localeCompare(B.from)
+    return fromCompare ? fromCompare : A.to.localeCompare(B.to)
+}
