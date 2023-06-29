74a277383a7894b10bd9a9a5a5a1728f3928d76c
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-28 20:25:36 -0400

    ISS2706: Added simpleWalk to Graph class

--- packages/mtw-utilities/ts/graphStorage/utils/graph/index.test.ts
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/index.test.ts
@@ -1,4 +1,4 @@
-import { Graph, GraphNode } from './'
+import { Graph } from './'
 import { GraphEdge, compareEdges } from './baseClasses'
 
 describe('Graph class', () => {
@@ -26,7 +26,7 @@ describe('Graph class', () => {
         })
 
         it('should correctly get edges on a directional graph', () => {
-            const testGraph = new Graph<string, { key: string}>(testNodes, testEdges, true)
+            const testGraph = new Graph(testNodes, testEdges, true)
             expect(testGraph.getNode('C')?.edges.sort(compareEdges)).toEqual([
                 { from: 'C', to: 'D' },
                 { from: 'C', to: 'E' }
@@ -34,7 +34,7 @@ describe('Graph class', () => {
         })
 
         it('should correctly get back-edges on a directional graph', () => {
-            const testGraph = new Graph<string, { key: string}>(testNodes, testEdges, true)
+            const testGraph = new Graph(testNodes, testEdges, true)
             expect(testGraph.getNode('C')?.backEdges.sort(compareEdges)).toEqual([
                 { from: 'A', to: 'C' },
                 { from: 'B', to: 'C' }
@@ -42,24 +42,69 @@ describe('Graph class', () => {
         })
 
         it('should correctly get edges on a non-directional graph', () => {
-            const testGraph = new Graph<string, { key: string}>(testNodes, testEdges)
+            const testGraph = new Graph(testNodes, testEdges)
             expect(testGraph.getNode('C')?.edges.sort(compareEdges)).toEqual([
-                { from: 'A', to: 'C' },
-                { from: 'B', to: 'C' },
+                { from: 'C', to: 'A' },
+                { from: 'C', to: 'B' },
                 { from: 'C', to: 'D' },
                 { from: 'C', to: 'E' }
             ])
         })
 
         it('should correctly get back-edges on a non-directional graph', () => {
-            const testGraph = new Graph<string, { key: string}>(testNodes, testEdges)
+            const testGraph = new Graph(testNodes, testEdges)
             expect(testGraph.getNode('C')?.backEdges.sort(compareEdges)).toEqual([
                 { from: 'A', to: 'C' },
                 { from: 'B', to: 'C' },
-                { from: 'C', to: 'D' },
-                { from: 'C', to: 'E' }
+                { from: 'D', to: 'C' },
+                { from: 'E', to: 'C' }
             ])
         })
         
+    })
+
+    describe('simpleWalk', () => {
+        let testNodes: Record<string, { key: string }>
+        let testEdges: GraphEdge<string>[]
+
+        beforeEach(() => {
+            testNodes = {
+                A: { key: 'A' },
+                B: { key: 'B' },
+                C: { key: 'C' },
+                D: { key: 'D' },
+                E: { key: 'E' },
+                F: { key: 'F' },
+            }
+            testEdges = [
+                { from: 'A', to: 'B' },
+                { from: 'B', to: 'C' },
+                { from: 'A', to: 'C' },
+                { from: 'D', to: 'E' },
+                { from: 'E', to: 'F' },
+                { from: 'F', to: 'D' }
+            ]
+        })
+
+        it('should correctly walk an acyclic tree', () => {
+            const callback = jest.fn()
+            const testGraph = new Graph(testNodes, testEdges, true)
+            testGraph.simpleWalk('A', callback)
+            expect(callback).toHaveBeenCalledTimes(3)
+            expect(callback).toHaveBeenCalledWith('A')
+            expect(callback).toHaveBeenCalledWith('B')
+            expect(callback).toHaveBeenCalledWith('C')
+        })
+
+        it('should correctly walk a cyclic tree', () => {
+            const callback = jest.fn()
+            const testGraph = new Graph(testNodes, testEdges, true)
+            testGraph.simpleWalk('D', callback)
+            expect(callback).toHaveBeenCalledTimes(3)
+            expect(callback).toHaveBeenCalledWith('D')
+            expect(callback).toHaveBeenCalledWith('E')
+            expect(callback).toHaveBeenCalledWith('F')
+        })
+
     })
 })
