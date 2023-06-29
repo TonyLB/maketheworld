b291f503fdef97776b2607922eaaef0bafc3673e
TonyLB <anthony.lowerbasch@gmail.com>
2023-06-29 18:48:13 -0400

    ISS2703: Added mergeEdge function to kargerStein algorithm

--- packages/mtw-utilities/ts/graphStorage/utils/graph/kargerStein.test.ts
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/kargerStein.test.ts
@@ -1,6 +1,6 @@
 import { Graph } from '.'
 import { GraphEdge, compareEdges } from './baseClasses'
-import kargerStein from './kargerStein'
+import kargerStein, * as kargerSteinModule from './kargerStein'
 
 describe('Karger-Stein algorithm', () => {
     let testNodes: Record<string, { key: string }>
@@ -39,5 +39,172 @@ describe('Karger-Stein algorithm', () => {
             { from: 'D', to: 'F' }
         ])
         expect(Object.keys(cutSet.nodes).length).toBe(0)
+    })
+
+    describe('countMergeNodeAndEdges', () => {
+        let testNodes: Record<string, { key: string }>
+        let testEdges: GraphEdge<string>[]
+        let testGraph: Graph<string, { key: string }>
+    
+        beforeEach(() => {
+            testNodes = {
+                A: { key: 'A' },
+                B: { key: 'B' },
+                C: { key: 'C' },
+                D: { key: 'D' },
+                E: { key: 'E' },
+                F: { key: 'F' },
+            }
+            testEdges = [
+                { from: 'A', to: 'B' },
+                { from: 'B', to: 'C' },
+                { from: 'A', to: 'C' },
+                { from: 'C', to: 'D' },
+                { from: 'C', to: 'E' },
+                { from: 'D', to: 'F' }
+            ]
+            testGraph = new Graph(testNodes, testEdges)
+        })
+
+        it('should count merger when no labels', () => {
+            expect(kargerSteinModule.countMergedNodesAndEdges(testGraph, {}, { from: 'A', to: 'B' })).toEqual(3)
+        })
+
+        it('should correct sum merging two merged nodes', () => {
+            expect(kargerSteinModule.countMergedNodesAndEdges(
+                testGraph,
+                {
+                    A: { key: 'B', nodesAndEdges: 3 },
+                    B: { key: 'B', nodesAndEdges: 3 },
+                    C: { key: 'C', nodesAndEdges: 3 },
+                    D: { key: 'C', nodesAndEdges: 3 },
+                },
+                { from: 'B', to: 'C' }
+            )).toEqual(8)
+        })
+    })
+
+    describe('selectRandomUnmergedEdge', () => {
+        let testNodes: Record<string, { key: string }>
+        let testEdges: GraphEdge<string>[]
+    
+        beforeEach(() => {
+            testNodes = {
+                A: { key: 'A' },
+                B: { key: 'B' },
+                C: { key: 'C' },
+                D: { key: 'D' },
+                E: { key: 'E' },
+                F: { key: 'F' },
+            }
+            testEdges = [
+                { from: 'A', to: 'B' },
+                { from: 'B', to: 'C' },
+                { from: 'A', to: 'C' },
+                { from: 'C', to: 'D' },
+                { from: 'C', to: 'E' },
+                { from: 'D', to: 'F' }
+            ]
+        })
+    
+        it('should select a random edge from unmerged graph', () => {
+            const testGraph = new Graph(testNodes, testEdges)
+            jest.spyOn(kargerSteinModule, 'randomInRange').mockReturnValue(2)
+            expect(kargerSteinModule.selectRandomUnmergedEdge(testGraph, {}, 10)).toEqual(2)
+        })
+
+        it('should exclude merged self-edges from selection', () => {
+            const testGraph = new Graph(testNodes, testEdges)
+            jest.spyOn(kargerSteinModule, 'randomInRange').mockReturnValue(2)
+            expect(kargerSteinModule.selectRandomUnmergedEdge(testGraph, { B: { key: 'B', nodesAndEdges: 3 }, C: { key: 'B', nodesAndEdges: 3 }}, 10)).toEqual(3)
+        })
+
+        it('should exclude edges that would make a past-threshold merge node from selection', () => {
+            const testGraph = new Graph(testNodes, testEdges)
+            jest.spyOn(kargerSteinModule, 'randomInRange').mockReturnValue(0)
+            expect(kargerSteinModule.selectRandomUnmergedEdge(testGraph, { B: { key: 'B', nodesAndEdges: 3 }, C: { key: 'B', nodesAndEdges: 3 }}, 8)).toEqual(0)
+            expect(kargerSteinModule.selectRandomUnmergedEdge(testGraph, { B: { key: 'B', nodesAndEdges: 3 }, C: { key: 'B', nodesAndEdges: 3 }}, 6)).toEqual(3)
+            expect(kargerSteinModule.selectRandomUnmergedEdge(testGraph, { B: { key: 'B', nodesAndEdges: 3 }, C: { key: 'B', nodesAndEdges: 3 }}, 5)).toEqual(5)
+        })
+
+    })
+
+    describe('mergeEdge', () => {
+        let testNodes: Record<string, { key: string }>
+        let testEdges: GraphEdge<string>[]
+        let testGraph: Graph<string, { key: string }>
+    
+        beforeEach(() => {
+            testNodes = {
+                A: { key: 'A' },
+                B: { key: 'B' },
+                C: { key: 'C' },
+                D: { key: 'D' },
+                E: { key: 'E' },
+                F: { key: 'F' },
+            }
+            testEdges = [
+                { from: 'A', to: 'B' },
+                { from: 'B', to: 'C' },
+                { from: 'A', to: 'C' },
+                { from: 'C', to: 'D' },
+                { from: 'C', to: 'E' },
+                { from: 'D', to: 'F' }
+            ]
+            testGraph = new Graph(testNodes, testEdges)
+        })
+
+        it('should merge edges on unmerged graph', () => {
+            expect(kargerSteinModule.mergeEdge(testGraph, {}, { from : 'A', to: 'B' })).toEqual({ A: { key: 'A', nodesAndEdges: 3 }, B: { key: 'A', nodesAndEdges: 3 } })
+        })
+
+        it('should merge unmerged edges on merged graph', () => {
+            expect(kargerSteinModule.mergeEdge(
+                testGraph,
+                {
+                    C: { key: 'C', nodesAndEdges: 3 },
+                    D: { key: 'C', nodesAndEdges: 3 }
+                },
+                { from : 'A', to: 'B' }
+            )).toEqual({
+                A: { key: 'A', nodesAndEdges: 3 },
+                B: { key: 'A', nodesAndEdges: 3 },
+                C: { key: 'C', nodesAndEdges: 3 },
+                D: { key: 'C', nodesAndEdges: 3 }
+            })
+        })
+
+        it('should merge unmerged node with merged node', () => {
+            expect(kargerSteinModule.mergeEdge(
+                testGraph,
+                {
+                    C: { key: 'C', nodesAndEdges: 3 },
+                    D: { key: 'C', nodesAndEdges: 3 }
+                },
+                { from : 'A', to: 'C' }
+            )).toEqual({
+                A: { key: 'A', nodesAndEdges: 5 },
+                C: { key: 'A', nodesAndEdges: 5 },
+                D: { key: 'A', nodesAndEdges: 5 }
+            })
+        })
+
+        it('should merge already merged nodes', () => {
+            expect(kargerSteinModule.mergeEdge(
+                testGraph,
+                {
+                    A: { key: 'A', nodesAndEdges: 3 },
+                    B: { key: 'A', nodesAndEdges: 3 },
+                    C: { key: 'D', nodesAndEdges: 3 },
+                    D: { key: 'D', nodesAndEdges: 3 }
+                },
+                { from : 'B', to: 'C' }
+            )).toEqual({
+                A: { key: 'A', nodesAndEdges: 8 },
+                B: { key: 'A', nodesAndEdges: 8 },
+                C: { key: 'A', nodesAndEdges: 8 },
+                D: { key: 'A', nodesAndEdges: 8 }
+            })
+        })
     })
 })
--- packages/mtw-utilities/ts/graphStorage/utils/graph/kargerStein.ts
+++ packages/mtw-utilities/ts/graphStorage/utils/graph/kargerStein.ts
@@ -5,11 +5,70 @@
 //
 
 import { Graph } from "."
+import { objectEntryMap, objectMap } from "../../../objects";
 import { GraphEdge } from "./baseClasses";
 
 type KargerSteinReturn<K extends string, T extends { key: K } & Record<string, any>> = {
     subGraphs: Graph<K, T>[];
     cutSet: Graph<K, T>;
+}
+
+type KargerSteinMergeLabel<K extends string> = {
+    key: K;
+    nodesAndEdges: number;
+}
+
+type KargerSteinMergeSet<K extends string> = Partial<Record<K, KargerSteinMergeLabel<K>>>
+
+export const countMergedNodesAndEdges = <K extends string, T extends { key: K } & Record<string, any>>(graph: Graph<K, T>, mergeLabels: KargerSteinMergeSet<K>, edge: GraphEdge<K>): number => {
+    const fromLabel = mergeLabels[edge.from]?.key || edge.from
+    const toLabel = mergeLabels[edge.to]?.key || edge.to
+    const edgeCount = graph.edges.filter(({ from, to }) => ([fromLabel, toLabel].includes(mergeLabels[from]?.key || from) && [fromLabel, toLabel].includes(mergeLabels[to]?.key || to))).length
+    console.log(`edgeCount: ${edgeCount}`)
+    const nodeCount = (Object.keys(graph.nodes) as K[]).filter((key) => ([fromLabel, toLabel].includes(mergeLabels[key]?.key || key))).length
+    console.log(`nodeCount: ${nodeCount}`)
+    return edgeCount + nodeCount
+}
+
+export const randomInRange = (range: number): number => (Math.floor(Math.random() * range))
+
+export const selectRandomUnmergedEdge = <K extends string, T extends { key: K } & Record<string, any>>(graph: Graph<K, T>, mergeLabels: Partial<Record<K, KargerSteinMergeLabel<K>>>, threshold: number): number | undefined => {
+    const unmergedEdgeIndices = graph.edges.reduce<number[]>((previous, { from, to }, index) => {
+        if ((mergeLabels[from]?.key || from) !== (mergeLabels[to]?.key || to)) {
+            if (countMergedNodesAndEdges(graph, mergeLabels, { from, to }) < threshold) {
+                return [
+                    ...previous,
+                    index
+                ]
+            }
+        }
+        return previous
+    }, [])
+    
+    if (!unmergedEdgeIndices.length) {
+        return undefined
+    }
+    return unmergedEdgeIndices[randomInRange(unmergedEdgeIndices.length)]
+}
+
+export const mergeEdge = <K extends string, T extends { key: K } & Record<string, any>>(graph: Graph<K, T>, mergeLabels: KargerSteinMergeSet<K>, edge: GraphEdge<K>): KargerSteinMergeSet<K>  => {
+    const fromLabel = mergeLabels[edge.from]?.key || edge.from
+    const toLabel = mergeLabels[edge.to]?.key || edge.to
+    const nodesAndEdges = countMergedNodesAndEdges(graph, mergeLabels, edge)
+    const newMergeLabel: KargerSteinMergeLabel<K> = {
+        key: mergeLabels[edge.from]?.key || edge.from,
+        nodesAndEdges
+    }
+    return {
+        ...objectEntryMap(mergeLabels as Record<string, KargerSteinMergeLabel<K>>, (originalKey: string, item: KargerSteinMergeLabel<K>) => {
+            if ([fromLabel, toLabel].includes(item.key)) {
+                return newMergeLabel
+            }
+            return item
+        }),
+        [edge.from]: newMergeLabel,
+        [edge.to]: newMergeLabel
+    } as KargerSteinMergeSet<K>
 }
 
 export const kargerStein = <K extends string, T extends { key: K } & Record<string, any>>(graph: Graph<K, T>, threshold: number): KargerSteinReturn<K, T> => {
@@ -19,6 +78,15 @@ export const kargerStein = <K extends string, T extends { key: K } & Record<stri
             cutSet: new Graph<K, T>({}, [], graph.directional)
         }
     }
+    let mergeLabels: KargerSteinMergeSet<K> = {}
+    //
+    // TODO: Select random edges to merge until there are none remaining
+    //
+
+    //
+    // TODO: Test whether the edge-set is below threshold (likely) ... if not, run a second time, then accept the best of the two alternatives
+    //
+
     return {
         subGraphs: [],
         cutSet: new Graph<K, T>({}, [], graph.directional)
