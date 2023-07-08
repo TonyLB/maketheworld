6edf4aa3dd5304e0314d67b82b887273a0fc1bf9
TonyLB <anthony.lowerbasch@gmail.com>
2023-07-08 15:55:06 -0400

    ISS2397: Added withUpdate mixin to DBHandler

---
+++ packages/mtw-utilities/ts/dynamoDB/mixins/update.test.ts
@@ -0,0 +1,199 @@
+import { BatchWriteItemCommand } from '@aws-sdk/client-dynamodb'
+import { DBHandlerBase } from '../baseClasses'
+import withGetOperations from './get'
+import withUpdate from './update'
+import { marshall } from '@aws-sdk/util-dynamodb'
+
+const dbMock = {
+    send: jest.fn()
+}
+
+describe('withUpdate', () => {
+    const dbHandler = new (withUpdate(withGetOperations(DBHandlerBase)))({
+        client: dbMock as any,
+        tableName: 'Ephemera',
+        incomingKeyLabel: 'PrimaryKey',
+        internalKeyLabel: 'EphemeraId',
+        options: { getBatchSize: 3 }
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+    })
+
+    describe('getItem', () => {
+
+        it('should remap incoming primary key', async () => {
+            dbMock.send
+                .mockResolvedValueOnce({ Item: marshall({ testOne: 'Testing', testTwo: 'Also Testing', testFour: 'Unchanged' }) })
+                .mockResolvedValueOnce({ Attributes: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test', testOne: 'Different Test', testThree: 'New test', testFour: 'Unchanged' })})
+
+            const output = await dbHandler.optimisticUpdate({
+                Key: { PrimaryKey: 'TestOne', DataCategory: 'DC1'},
+                updateKeys: ['testOne', 'testTwo', 'testThree', 'testFour'],
+                updateReducer: (draft) => {
+                    draft.testOne = 'Different Test',
+                    draft.testTwo = undefined
+                    draft.testThree = 'New test'
+                }
+            })
+            expect(dbMock.send).toHaveBeenCalledTimes(2)
+            expect(dbMock.send.mock.calls[0][0].input).toEqual({
+                Key: marshall({ EphemeraId: 'TestOne', DataCategory: 'DC1'}),
+                TableName: 'Ephemera',
+                ProjectionExpression: 'testOne, testTwo, testThree, testFour'
+            })
+            expect(dbMock.send.mock.calls[1][0].input).toEqual({
+                ConditionExpression: "testOne = :Old0 AND testTwo = :Old1 AND attribute_not_exists(testThree)",
+                ExpressionAttributeValues: marshall({
+                  ":New0": "Different Test",
+                  ":New2": "New test",
+                  ":Old0": "Testing",
+                  ":Old1": "Also Testing"
+                }),
+                Key: marshall({
+                    EphemeraId: "TestOne",
+                    DataCategory: "DC1",
+                }),
+                TableName: "Ephemera",
+                UpdateExpression: "SET testOne = :New0, testThree = :New2 REMOVE testTwo",
+            })
+            expect(output).toEqual({
+                PrimaryKey: "TEST",
+                DataCategory: "Meta::Test",
+                testOne: "Different Test",
+                testThree: "New test",
+                testFour: "Unchanged"
+            })
+        })
+
+        it('should replace reserved words with ExpressionAttributeName items', async () => {
+            dbMock.send
+                .mockResolvedValueOnce({ Item: marshall({ Name: 'Testing', zone: 'Also Testing', value: 'Unchanged' }) })
+                .mockResolvedValueOnce({ Attributes: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test', Name: 'Different Test', key: 'New test', value: 'Unchanged' })})
+
+            const output = await dbHandler.optimisticUpdate({
+                Key: { PrimaryKey: 'TestOne', DataCategory: 'DC1'},
+                updateKeys: ['Name', 'zone', 'key', 'value'],
+                updateReducer: (draft) => {
+                    draft.Name = 'Different Test',
+                    draft.zone = undefined
+                    draft.key = 'New test'
+                }
+            })
+            expect(dbMock.send).toHaveBeenCalledTimes(2)
+            expect(dbMock.send.mock.calls[0][0].input).toEqual({
+                Key: marshall({ EphemeraId: 'TestOne', DataCategory: 'DC1'}),
+                TableName: 'Ephemera',
+                ProjectionExpression: '#name, #zone, #key, #value',
+                ExpressionAttributeNames: {
+                    '#name': 'Name',
+                    '#zone': 'zone',
+                    '#key': 'key',
+                    '#value': 'value'
+                }
+            })
+            expect(dbMock.send.mock.calls[1][0].input).toEqual({
+                ConditionExpression: "#name = :Old0 AND #zone = :Old1 AND attribute_not_exists(#key)",
+                ExpressionAttributeValues: marshall({
+                  ":New0": "Different Test",
+                  ":New2": "New test",
+                  ":Old0": "Testing",
+                  ":Old1": "Also Testing"
+                }),
+                Key: marshall({
+                    EphemeraId: "TestOne",
+                    DataCategory: "DC1",
+                }),
+                TableName: "Ephemera",
+                UpdateExpression: "SET #name = :New0, #key = :New2 REMOVE #zone",
+                ExpressionAttributeNames: {
+                    '#name': 'Name',
+                    '#zone': 'zone',
+                    '#key': 'key'
+                }
+            })
+            expect(output).toEqual({
+                PrimaryKey: "TEST",
+                DataCategory: "Meta::Test",
+                Name: "Different Test",
+                key: "New test",
+                value: "Unchanged"
+            })
+        })
+
+        it('should update when no item fetched', async () => {
+            dbMock.send
+                .mockResolvedValueOnce({})
+                .mockResolvedValueOnce({ Attributes: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test', testOne: 'Test', testTwo: 'Another test' })})
+            const output = await dbHandler.optimisticUpdate({
+                Key: {
+                    PrimaryKey: 'TEST',
+                    DataCategory: 'Meta::Test'
+                },
+                updateKeys: ['testOne', 'testTwo'],
+                updateReducer: (draft) => {
+                    draft.testOne = 'Test'
+                    draft.testTwo = 'Another test'
+                }
+            })
+            expect(dbMock.send).toHaveBeenCalledTimes(2)
+            expect(dbMock.send.mock.calls[0][0].input).toEqual({
+                Key: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test'}),
+                TableName: 'Ephemera',
+                ProjectionExpression: 'testOne, testTwo'
+            })
+            expect(dbMock.send.mock.calls[1][0].input).toEqual({
+                ConditionExpression: "attribute_not_exists(DataCategory)",
+                ExpressionAttributeValues: marshall({
+                  ":New0": "Test",
+                  ":New1": "Another test"
+                }),
+                Key: marshall({
+                    EphemeraId: "TEST",
+                    DataCategory: "Meta::Test",
+                }),
+                TableName: "Ephemera",
+                UpdateExpression: "SET testOne = :New0, testTwo = :New1",
+            })
+            expect(output).toEqual({ PrimaryKey: 'TEST', DataCategory: 'Meta::Test', testOne: 'Test', testTwo: 'Another test' })
+        })
+    
+        it('should update when field not defined', async () => {
+            dbMock.send
+                .mockResolvedValueOnce({ Item: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test' })})
+                .mockResolvedValueOnce({ Attributes: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test', optionalField: 'Test' })})
+            const output = await dbHandler.optimisticUpdate({
+                Key: {
+                    PrimaryKey: 'TEST',
+                    DataCategory: 'Meta::Test'
+                },
+                updateKeys: ['optionalField'],
+                updateReducer: (draft) => {
+                    draft.optionalField = 'Test'
+                }
+            })
+            expect(dbMock.send).toHaveBeenCalledTimes(2)
+            expect(dbMock.send.mock.calls[0][0].input).toEqual({
+                Key: marshall({ EphemeraId: 'TEST', DataCategory: 'Meta::Test'}),
+                TableName: 'Ephemera',
+                ProjectionExpression: 'optionalField'
+            })
+            expect(dbMock.send.mock.calls[1][0].input).toEqual({
+                ConditionExpression: "attribute_not_exists(optionalField)",
+                ExpressionAttributeValues: marshall({
+                  ":New0": "Test"
+                }),
+                Key: marshall({
+                    EphemeraId: "TEST",
+                    DataCategory: "Meta::Test",
+                }),
+                TableName: "Ephemera",
+                UpdateExpression: "SET optionalField = :New0",
+            })
+            expect(output).toEqual({ PrimaryKey: 'TEST', DataCategory: 'Meta::Test', optionalField: 'Test' })
+        })
+
+    })
+})
---
+++ packages/mtw-utilities/ts/dynamoDB/mixins/update.ts
@@ -0,0 +1,232 @@
+import { BatchGetItemCommand, BatchWriteItemCommand, GetItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb"
+import { Constructor, DBHandlerBase } from "../baseClasses"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
+import { asyncSuppressExceptions } from "../../errors"
+import paginateList from "./utils/paginateList"
+import mapProjectionFields from './utils/mapProjectionFields'
+import produce from "immer"
+import { WritableDraft } from "immer/dist/internal"
+import withGetOperations from "./get"
+import { DEVELOPER_MODE } from '../../constants'
+import delayPromise from "../delayPromise"
+
+type DynamicUpdateOutput = {
+    ExpressionAttributeNames: Record<string, any>;
+    ExpressionAttributeValues: Record<string, any>;
+    setExpressions: string[];
+    removeExpressions: string[];
+    conditionExpressions: string[];
+}
+
+//
+// TODO: Can we replace empty objects with undefined, and do away with the need for this typeguard?
+//
+const isDynamicUpdateOutput = (item: {} | DynamicUpdateOutput): item is DynamicUpdateOutput => (Object.values(item).length > 0)
+
+//
+// TODO: Remove dependency on explicit ExpressionAttributeNames, and replace with automatic remapping of reserved words (utils/mapProjectionFields.ts)
+//
+const updateByReducer = <T extends Record<string, any>>({ updateKeys, reducer }: { updateKeys: string[]; reducer: (draft: WritableDraft<T>) => void }) => (state: T | undefined): DynamicUpdateOutput | {} => {
+    const { ExpressionAttributeNames } = mapProjectionFields(updateKeys)
+    const translateToExpressionAttributeNames = Object.entries(ExpressionAttributeNames).reduce<Record<string, string>>((previous, [key, value]) => ({ ...previous, [value]: key }), {})
+    const newState = produce(state || {}, reducer)
+    if (newState === state) {
+        return {}
+    }
+    if (typeof state === 'object' && typeof newState === 'object') {
+        //
+        // Updating an existing record
+        //
+        const startingDraft: {
+            ExpressionAttributeNames: Record<string, any>;
+            ExpressionAttributeValues: Record<string, any>;
+            setExpressions: string[];
+            removeExpressions: string[];
+            conditionExpressions: string[];
+        } = {
+            ExpressionAttributeNames: {},
+            ExpressionAttributeValues: {},
+            setExpressions: [],
+            removeExpressions: [],
+            conditionExpressions: []
+        }
+        return produce(startingDraft, (draft) => {
+            updateKeys.forEach((key, index) => {
+                const translatedKey = key in translateToExpressionAttributeNames ? translateToExpressionAttributeNames[key] : key
+                if (typeof state === 'object' && key in state && (typeof state[key] !== 'undefined')) {
+                    if (newState?.[key] === undefined) {
+                        //
+                        // Remove existing item
+                        //
+                        draft.removeExpressions.push(`${translatedKey}`)
+                        draft.ExpressionAttributeValues[`:Old${index}`] = state[key]
+                        draft.conditionExpressions.push(`${translatedKey} = :Old${index}`)
+                        if (translatedKey in ExpressionAttributeNames) {
+                            draft.ExpressionAttributeNames[translatedKey] = key
+                        }
+                    }
+                    if (typeof newState === 'object' && key in newState && (typeof newState[key] !== 'undefined') && newState[key] !== state[key]) {
+                        //
+                        // Update existing item to new value
+                        //
+                        draft.ExpressionAttributeValues[`:Old${index}`] = state[key]
+                        draft.ExpressionAttributeValues[`:New${index}`] = newState[key]
+                        draft.setExpressions.push(`${translatedKey} = :New${index}`)
+                        draft.conditionExpressions.push(`${translatedKey} = :Old${index}`)
+                        if (translatedKey in ExpressionAttributeNames) {
+                            draft.ExpressionAttributeNames[translatedKey] = key
+                        }
+                    }
+                }
+                else {
+                    draft.conditionExpressions.push(`attribute_not_exists(${translatedKey})`)
+                    if (translatedKey in ExpressionAttributeNames) {
+                        draft.ExpressionAttributeNames[translatedKey] = key
+                    }
+                    if (typeof newState === 'object' && key in newState && newState[key] !== undefined) {
+                        //
+                        // Add new item
+                        //
+                        draft.ExpressionAttributeValues[`:New${index}`] = newState[key]
+                        draft.setExpressions.push(`${translatedKey} = :New${index}`)
+                    }
+                }
+            })
+        })
+    }
+    else {
+        //
+        // Putting a new record
+        //
+        const startingDraft: {
+            ExpressionAttributeNames: Record<string, any>;
+            ExpressionAttributeValues: Record<string, any>;
+            setExpressions: string[];
+            removeExpressions: string[];
+            conditionExpressions: string[];
+        } = {
+            ExpressionAttributeNames: {},
+            ExpressionAttributeValues: {},
+            setExpressions: [],
+            removeExpressions: [],
+            conditionExpressions: []
+        }
+        return produce(startingDraft, (draft) => {
+            draft.conditionExpressions.push(`attribute_not_exists(DataCategory)`)
+            updateKeys.forEach((key, index) => {
+                const translatedKey = key in translateToExpressionAttributeNames ? translateToExpressionAttributeNames[key] : key
+                if (newState && key in newState && newState[key] !== undefined) {
+                    //
+                    // Add new item
+                    //
+                    if (translatedKey in ExpressionAttributeNames) {
+                        draft.ExpressionAttributeNames[translatedKey] = key
+                    }
+                    draft.ExpressionAttributeValues[`:New${index}`] = newState[key]
+                    draft.setExpressions.push(`${key} = :New${index}`)
+                }
+            })
+        })
+    }
+}
+
+type UpdateExtendedProps<T extends Record<string, any>> = {
+    updateKeys: string[],
+    updateReducer: (draft: WritableDraft<T>) => void,
+    ExpressionAttributeNames?: Record<string, any>,
+    ReturnValues?: 'NONE' | 'ALL_NEW',
+    maxRetries?: number,
+    catchException?: (err: any) => Promise<void>
+}
+
+export const withUpdate = <KIncoming extends string, KInternal extends string, T extends string, GBase extends ReturnType<typeof withGetOperations<KIncoming, KInternal, T, Constructor<DBHandlerBase<KIncoming, KInternal, T>>>>>(Base: GBase) => {
+    return class UpdateDBHandler extends Base {
+        //
+        // optimisticUpdate fetches the current state of the keys you intend to update,
+        // and then runs that record through a change procedure (as in Immer) to change
+        // their values.  If the reducer causes some changes, then the update attempts
+        // to update the DB record ... conditional on all of those values still being
+        // the same they were when they were first fetched.  Otherwise, it knows that
+        // it is in a potential deadlock condition with other updates, and so pauses for
+        // incremental backoff, and tries again (fetching and reducing from scratch)
+        // until such time as either (a) the reducer causes no changes or (b) it succeeds
+        // in completing a cycle without any other process side-effecting the same fields.
+        //
+        async optimisticUpdate<Update extends Record<string, any>>(props: { Key: ({ [key in KIncoming]: T } & { DataCategory: string }) } & UpdateExtendedProps<Update>): Promise<Update | undefined> {
+            const {
+                Key,
+                updateKeys,
+                updateReducer,
+                maxRetries
+            } = props
+            if (!updateKeys) {
+                return undefined
+            }
+            let retries = 0
+            let exponentialBackoff = 100
+            let returnValue: Record<string, any> = {}
+            let completed = false
+            while(!completed && (retries <= (maxRetries ?? 5))) {
+                completed = true
+                const stateFetch = await this.getItem<Update>({
+                    Key,
+                    ProjectionFields: updateKeys,
+                })
+                const state = stateFetch || {}
+                const updateOutput = updateByReducer({ updateKeys, reducer: updateReducer })(stateFetch)
+                if (!isDynamicUpdateOutput(updateOutput)) {
+                    returnValue = state || returnValue
+                    break
+                }
+                else {
+                    const { ExpressionAttributeNames, ExpressionAttributeValues, setExpressions, removeExpressions, conditionExpressions } = updateOutput
+                    try {
+                        const UpdateExpression = [
+                            setExpressions.length ? `SET ${setExpressions.join(', ')}` : '',
+                            removeExpressions.length ? `REMOVE ${removeExpressions.join(', ')}` : ''
+                        ].filter((value) => (value)).join(' ')
+                        if (!UpdateExpression) {
+                            returnValue = state
+                            break
+                        }
+                        const { Attributes = {} } = await this._client.send(new UpdateItemCommand({
+                            TableName: this._tableName,
+                            Key: marshall(this._remapIncomingObject(Key)),
+                            UpdateExpression,
+                            ...(conditionExpressions.length ? {
+                                ConditionExpression: conditionExpressions.join(' AND ')
+                            } : {}),
+                            ...(ExpressionAttributeValues ? { ExpressionAttributeValues: marshall(ExpressionAttributeValues, { removeUndefinedValues: true }) } : {}),
+                            ...((ExpressionAttributeNames && Object.values(ExpressionAttributeNames).length > 0) ? { ExpressionAttributeNames } : {}),
+                            ReturnValues: props.ReturnValues
+                        }))
+                        returnValue = this._remapOutgoingObject(unmarshall(Attributes) as any)
+                    }
+                    catch (err: any) {
+                        if (err.code === 'ConditionalCheckFailedException') {
+                            await delayPromise(exponentialBackoff)
+                            exponentialBackoff = exponentialBackoff * 2
+                            retries++
+                            completed = false
+                        }
+                        else {
+                            if (DEVELOPER_MODE) {
+                                throw err
+                            }
+                            else {
+                                returnValue = props.catchException?.(err) ?? {}
+                            }
+                        }
+                    }
+                }
+            }
+            //
+            // TODO: Create custom error type to throw when the optimisticUpdate fails
+            // entirely
+            //
+            return returnValue as Update
+        }
+    }
+}
+
+export default withUpdate
