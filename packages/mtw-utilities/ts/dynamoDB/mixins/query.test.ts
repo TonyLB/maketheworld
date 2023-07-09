503de521144b31ddae68b19069fb756f8f51bd9e
TonyLB <anthony.lowerbasch@gmail.com>
2023-07-09 13:45:56 -0400

    ISS2397: Added query mixin to DBHandler

---
+++ packages/mtw-utilities/ts/dynamoDB/mixins/query.test.ts
@@ -0,0 +1,107 @@
+import { BatchWriteItemCommand } from '@aws-sdk/client-dynamodb'
+import { DBHandlerBase } from '../baseClasses'
+import withQuery from './query'
+import { marshall } from '@aws-sdk/util-dynamodb'
+
+const dbMock = {
+    send: jest.fn()
+}
+
+describe('withQuery', () => {
+    const dbHandler = new (withQuery(DBHandlerBase))({
+        client: dbMock as any,
+        tableName: 'Ephemera',
+        incomingKeyLabel: 'PrimaryKey',
+        internalKeyLabel: 'EphemeraId',
+        options: { getBatchSize: 3 }
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+    })
+
+    it('should remap incoming primary key', async () => {
+        dbMock.send.mockResolvedValue({ Items: [marshall({
+            EphemeraId: 'TestOne',
+            DataCategory: 'DC1',
+            TestValue: 5
+        })] })
+        const output = await dbHandler.query({ Key: { PrimaryKey: 'TestOne' } })
+        expect(dbMock.send).toHaveBeenCalledTimes(1)
+        expect(dbMock.send.mock.calls[0][0].input).toEqual({
+            TableName: 'Ephemera',
+            KeyConditionExpression: `EphemeraId = :keyId`,
+            ExpressionAttributeValues: marshall({ ':keyId': 'TestOne' }),
+            ProjectionExpression: "DataCategory"
+        })
+        expect(output).toEqual([{
+            PrimaryKey: 'TestOne',
+            DataCategory: 'DC1',
+            TestValue: 5
+        }])
+    })
+
+    it('should remap reserved attributes', async () => {
+        dbMock.send.mockResolvedValue({ Items: [marshall({
+            EphemeraId: 'TestOne',
+            DataCategory: 'DC1',
+            Name: 'TestName',
+            zone: 'TestZone'
+        })] })
+        const output = await dbHandler.query({ Key: { PrimaryKey: 'TestOne' }, ProjectionFields: ['PrimaryKey', 'Name', 'Key', 'zone'] })
+        expect(dbMock.send).toHaveBeenCalledTimes(1)
+        expect(dbMock.send.mock.calls[0][0].input).toEqual({
+            TableName: 'Ephemera',
+            KeyConditionExpression: `EphemeraId = :keyId`,
+            ExpressionAttributeValues: marshall({ ':keyId': 'TestOne' }),
+            ProjectionExpression: "EphemeraId, #name, #key, #zone",
+            ExpressionAttributeNames: {
+                '#name': 'Name',
+                '#key': 'Key',
+                '#zone': 'zone'
+            }
+        })
+        expect(output).toEqual([{
+            PrimaryKey: 'TestOne',
+            DataCategory: 'DC1',
+            Name: 'TestName',
+            zone: 'TestZone'
+        }])
+    })
+
+    it('should not remap expression attribute values', async () => {
+        dbMock.send.mockResolvedValue({ Items: [marshall({
+            EphemeraId: 'TestOne',
+            DataCategory: 'DC1',
+            Name: 'TestName',
+            zone: 'TestZone'
+        })] })
+        const output = await dbHandler.query({
+            Key: { PrimaryKey: 'TestOne' },
+            ProjectionFields: ['PrimaryKey', 'Name', 'Key', 'zone'],
+            ExpressionAttributeValues: { ':zone': 'TestZone' },
+            FilterExpression: 'zone = :zone'
+        })
+        expect(dbMock.send).toHaveBeenCalledTimes(1)
+        expect(dbMock.send.mock.calls[0][0].input).toEqual({
+            TableName: 'Ephemera',
+            KeyConditionExpression: `EphemeraId = :keyId`,
+            ExpressionAttributeValues: marshall({ ':keyId': 'TestOne', ':zone': 'TestZone' }),
+            ProjectionExpression: "EphemeraId, #name, #key, #zone",
+            ExpressionAttributeNames: {
+                '#name': 'Name',
+                '#key': 'Key',
+                '#zone': 'zone'
+            },
+            FilterExpression: '#zone = :zone'
+        })
+        expect(output).toEqual([{
+            PrimaryKey: 'TestOne',
+            DataCategory: 'DC1',
+            Name: 'TestName',
+            zone: 'TestZone'
+        }])
+    })
+
+})
---
+++ packages/mtw-utilities/ts/dynamoDB/mixins/query.ts
@@ -0,0 +1,145 @@
+import { QueryCommand } from "@aws-sdk/client-dynamodb"
+import { Constructor, DBHandlerBase } from "../baseClasses"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
+import { asyncSuppressExceptions } from "../../errors"
+import mapProjectionFields from "./utils/mapProjectionFields"
+
+type QueryExtendedProps = Partial<{
+    ProjectionFields: string[];
+    KeyConditionExpression: string;
+    ExpressionAttributeValues: Record<string, any>;
+    FilterExpression: string;
+}>
+
+type QueryKeyPropsDataCategoryIndex = {
+    IndexName: 'DataCategoryIndex';
+    Key: { DataCategory: string; };
+}
+
+type QueryKeyPropsScopedIdIndex = {
+    IndexName: 'ScopedIdIndex';
+    Key: { scopedId: string; };
+}
+
+type QueryKeyPropsPlayerIndex = {
+    IndexName: 'PlayerIndex';
+    Key: { player: string; };
+}
+
+type QueryKeyPropsZoneIndex = {
+    IndexName: 'ZoneIndex';
+    Key: { zone: string; };
+}
+
+type QueryKeyPropsConnectionIndex = {
+    IndexName: 'ConnectionIndex';
+    Key: { ConnectionId: string; };
+}
+
+type ExplicitIndexNameQueryKeyProps = QueryKeyPropsDataCategoryIndex | QueryKeyPropsScopedIdIndex | QueryKeyPropsPlayerIndex | QueryKeyPropsZoneIndex | QueryKeyPropsConnectionIndex
+
+type QueryKeyProps<KIncoming extends Exclude<string, 'ProjectionFields' | 'KeyConditionExpression' | 'ExpressionAttributeValues' | 'FilterExpression'>, T extends string> = {
+    Key: { [key in KIncoming]: T };
+    IndexName?: ''
+} | ExplicitIndexNameQueryKeyProps
+
+type ExtractKeyReturn = {
+    KeyConditionExpression: string;
+    keyId: string;
+}
+
+export const withQuery = <KIncoming extends string, KInternal extends string, T extends string, GBase extends Constructor<DBHandlerBase<KIncoming, KInternal, T>>>(Base: GBase) => {
+    return class QueryDBHandler extends Base {
+        _queryExtractKeyInfo(props: QueryKeyProps<KIncoming, T>): ExtractKeyReturn {
+            switch(props.IndexName) {
+                case 'PlayerIndex':
+                    return {
+                        KeyConditionExpression: 'player = :keyId',
+                        keyId: props.Key.player
+                    }
+                case 'ScopedIdIndex':
+                    return {
+                        KeyConditionExpression: 'scopedId = :keyId',
+                        keyId: props.Key.scopedId
+                    }
+                case 'ConnectionIndex':
+                    return {
+                        KeyConditionExpression: 'ConnectionId = :keyId',
+                        keyId: props.Key.ConnectionId
+                    }
+                case 'ZoneIndex':
+                    return {
+                        KeyConditionExpression: '#zone = :keyId',
+                        keyId: props.Key.zone
+                    }
+                case 'DataCategoryIndex':
+                    return {
+                        KeyConditionExpression: 'DataCategory = :keyId',
+                        keyId: props.Key.DataCategory
+                    }
+                default:
+                    return {
+                        KeyConditionExpression: `${this._internalKeyLabel} = :keyId`,
+                        keyId: props.Key[this._incomingKeyLabel]
+                    }
+            }
+        }
+
+        async query<Query extends Record<string, any>>(props: QueryKeyProps<KIncoming, T> & QueryExtendedProps): Promise<Query[]> {
+            const {
+                IndexName = '',
+                ProjectionFields: passedProjectionFields,
+                KeyConditionExpression: extraExpression,
+                ExpressionAttributeValues,
+                FilterExpression
+            } = props
+            const { Items = [] } = await asyncSuppressExceptions(async () => {
+                const ProjectionFields = passedProjectionFields || 
+                    (IndexName
+                        ? [this._incomingKeyLabel, 'DataCategory']
+                        : ['DataCategory']
+                    )
+                const { ExpressionAttributeNames } = mapProjectionFields(ProjectionFields)
+                const translateToExpressionAttributeNames = Object.entries(ExpressionAttributeNames).reduce<Record<string, string>>((previous, [key, value]) => ({ ...previous, [value]: key }), {})            
+                const { KeyConditionExpression: baseExpression, keyId } = this._queryExtractKeyInfo(props)
+                const replaceAttributeNames = (incoming: string): string => (
+                    Object.entries(translateToExpressionAttributeNames).reduce<string>(
+                        (previous, [key, translated]) => {
+                            return previous.replace(new RegExp(`\\b(?<!:)${key}\\b`, 'g'), translated)
+                        },
+                        incoming
+                    )
+                )
+                const KeyConditionExpression = replaceAttributeNames(
+                    extraExpression
+                        ? `${baseExpression} AND ${extraExpression}`
+                        : baseExpression
+                )
+                const revisedExpressionAttributeNames = {
+                    ...ExpressionAttributeNames,
+                    ...(IndexName === 'ZoneIndex' ? { '#zone': 'zone' } : {})
+                }
+                return await this._client.send(new QueryCommand({
+                    TableName: this._tableName,
+                    KeyConditionExpression,
+                    IndexName: IndexName.length > 0 ? IndexName : undefined,
+                    ExpressionAttributeValues: marshall({
+                        ':keyId': keyId,
+                        ...(ExpressionAttributeValues || {})
+                    }),
+                    ProjectionExpression: ProjectionFields
+                        .map((key) => (key === this._incomingKeyLabel ? this._internalKeyLabel : key))
+                        .map((key) => (translateToExpressionAttributeNames[key] || key))
+                        .join(', '),
+                    ExpressionAttributeNames: (Object.keys(revisedExpressionAttributeNames).length > 0)
+                        ? revisedExpressionAttributeNames
+                        : undefined,
+                    FilterExpression: FilterExpression ? replaceAttributeNames(FilterExpression) : undefined
+                }))
+            }, async () => ({ Items: [] })) as { Items: Query[] }
+            return Items.map((value) => (this._remapOutgoingObject(unmarshall(value) as any))) as Query[]
+        }
+    }
+}
+
+export default withQuery
