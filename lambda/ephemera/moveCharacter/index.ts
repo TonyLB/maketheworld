96292f31bbf665af23b8ae00891a1396c8380ae5
TonyLB <anthony.lowerbasch@gmail.com>
2023-08-08 23:11:57 -0400

    ISS2809: Refactored moveCharacter to use ephemeraDB transactWrite

--- lambda/ephemera/moveCharacter/index.ts
+++ lambda/ephemera/moveCharacter/index.ts
@@ -1,5 +1,5 @@
 import { MoveCharacterMessage, MessageBus } from "../messageBus/baseClasses"
-import { legacyConnectionDB as connectionDB, exponentialBackoffWrapper, multiTableTransactWrite } from "@tonylb/mtw-utilities/dist/dynamoDB"
+import { legacyConnectionDB as connectionDB, ephemeraDB, exponentialBackoffWrapper, multiTableTransactWrite } from "@tonylb/mtw-utilities/dist/dynamoDB"
 import internalCache from "../internalCache"
 import { marshall } from "@aws-sdk/util-dynamodb"
 import { RoomCharacterListItem } from "../internalCache/baseClasses"
@@ -15,115 +15,108 @@ export const moveCharacter = async ({ payloads, messageBus }: { payloads: MoveCh
         await exponentialBackoffWrapper(async () => {
 
             internalCache.RoomCharacterList.invalidate(payload.roomId)
-            const [characterMeta, arrivingCharacters, connections] = await Promise.all([
+            const [characterMeta, connections] = await Promise.all([
                 internalCache.CharacterMeta.get(payload.characterId),
-                internalCache.RoomCharacterList.get(payload.roomId),
                 internalCache.CharacterConnections.get(payload.characterId)
             ])
             if (payload.roomId === characterMeta.RoomId) {
                 return
             }
             internalCache.RoomCharacterList.invalidate(characterMeta.RoomId)
-            const departingCharacters = await internalCache.RoomCharacterList.get(characterMeta.RoomId)
-            const newDepartingCharacters = departingCharacters.filter(({ EphemeraId }) => (EphemeraId !== characterMeta.EphemeraId))
-            const newArrivingCharacters: RoomCharacterListItem[] = [
-                ...arrivingCharacters
-                    .filter(({ EphemeraId }) => (EphemeraId !== characterMeta.EphemeraId)),
+            await ephemeraDB.transactWrite([
                 {
-                    EphemeraId: characterMeta.EphemeraId,
-                    Name: characterMeta.Name,
-                    fileURL: characterMeta.fileURL,
-                    Color: characterMeta.Color,
-                    ConnectionIds: connections || []
+                    PrimitiveUpdate: {
+                        Key: {
+                            EphemeraId: characterMeta.EphemeraId,
+                            DataCategory: 'Meta::Character'
+                        },
+                        ProjectionFields: ['RoomId'],
+                        UpdateExpression: 'SET RoomId = :newRoomId',
+                        ExpressionAttributeValues: {
+                            ':newRoomId': splitType(payload.roomId)[1]
+                        }
+                    }
+                },
+                {
+                    Update: {
+                        Key: {
+                            EphemeraId: characterMeta.RoomId,
+                            DataCategory: 'Meta::Room'
+                        },
+                        updateKeys: ['activeCharacters'],
+                        updateReducer: (draft) => {
+                            draft.activeCharacters = draft.activeCharacters.filter(({ EphemeraId }) => (EphemeraId !== characterMeta.EphemeraId))
+                        },
+                        successCallback: ({ activeCharacters }) => {
+                            internalCache.RoomCharacterList.set({ key: characterMeta.RoomId, value: activeCharacters })
+                            messageBus.send({
+                                type: 'PublishMessage',
+                                targets: [characterMeta.RoomId, payload.characterId],
+                                displayProtocol: 'WorldMessage',
+                                message: [{
+                                    tag: 'String',
+                                    value: `${characterMeta.Name || 'Someone'}${payload.leaveMessage || ' has left.'}`
+                                }]
+                            })
+                            messageBus.send({
+                                type: 'RoomUpdate',
+                                roomId: characterMeta.RoomId
+                            })
+                        }
+                    }
+                },
+                {
+                    Update: {
+                        Key: {
+                            EphemeraId: payload.roomId,
+                            DataCategory: 'Meta::Room'
+                        },
+                        updateKeys: ['activeCharacters'],
+                        updateReducer: (draft) => {
+                            draft.activeCharacters = [
+                                ...draft.activeCharacters
+                                    .filter(({ EphemeraId }) => (EphemeraId !== characterMeta.EphemeraId)),
+                                {
+                                    EphemeraId: characterMeta.EphemeraId,
+                                    Name: characterMeta.Name,
+                                    fileURL: characterMeta.fileURL,
+                                    Color: characterMeta.Color,
+                                    ConnectionIds: connections || []
+                                }
+                            ]
+                        },
+                        successCallback: ({ activeCharacters }) => {
+                            internalCache.RoomCharacterList.set({ key: payload.roomId, value: activeCharacters })
+                            messageBus.send({
+                                type: 'Perception',
+                                characterId: payload.characterId,
+                                ephemeraId: payload.roomId,
+                                header: true
+                            })
+                
+                            messageBus.send({
+                                type: 'PublishMessage',
+                                targets: [payload.roomId, payload.characterId],
+                                displayProtocol: 'WorldMessage',
+                                message: [{
+                                    tag: 'String',
+                                    value: `${characterMeta.Name || 'Someone'} has arrived.`
+                                }]
+                            })
+                            messageBus.send({
+                                type: 'RoomUpdate',
+                                roomId: payload.roomId
+                            })
+                            messageBus.send({
+                                type: 'MapUpdate',
+                                characterId: payload.characterId,
+                                previousRoomId: characterMeta.RoomId,
+                                roomId: payload.roomId
+                            })
+                        }
+                    }
                 }
-            ]
-            await multiTableTransactWrite([{
-                Update: {
-                    TableName: 'Ephemera',
-                    Key: marshall({
-                        EphemeraId: characterMeta.EphemeraId,
-                        DataCategory: 'Meta::Character'
-                    }),
-                    UpdateExpression: 'SET RoomId = :newRoomId',
-                    ExpressionAttributeValues: marshall({
-                        ':newRoomId': splitType(payload.roomId)[1]
-                    }, { removeUndefinedValues: true})
-                }
-            },
-            {
-                Update: {
-                    TableName: 'Ephemera',
-                    Key: marshall({
-                        EphemeraId: characterMeta.RoomId,
-                        DataCategory: 'Meta::Room'
-                    }),
-                    UpdateExpression: 'SET activeCharacters = :newActiveCharacters',
-                    ExpressionAttributeValues: marshall({
-                        ':oldActiveCharacters': departingCharacters,
-                        ':newActiveCharacters': newDepartingCharacters
-                    }, { removeUndefinedValues: true}),
-                    ConditionExpression: 'activeCharacters = :oldActiveCharacters'
-                }
-            },
-            {
-                Update: {
-                    TableName: 'Ephemera',
-                    Key: marshall({
-                        EphemeraId: payload.roomId,
-                        DataCategory: 'Meta::Room'
-                    }),
-                    UpdateExpression: 'SET activeCharacters = :newActiveCharacters',
-                    ExpressionAttributeValues: marshall({
-                        ':oldActiveCharacters': arrivingCharacters,
-                        ':newActiveCharacters': newArrivingCharacters
-                    }, { removeUndefinedValues: true}),
-                    ConditionExpression: 'activeCharacters = :oldActiveCharacters'
-                }
-            }])
-
-            internalCache.RoomCharacterList.set({ key: characterMeta.RoomId, value: newDepartingCharacters })
-            internalCache.RoomCharacterList.set({ key: payload.roomId, value: newArrivingCharacters })
-
-            messageBus.send({
-                type: 'PublishMessage',
-                targets: [characterMeta.RoomId, payload.characterId],
-                displayProtocol: 'WorldMessage',
-                message: [{
-                    tag: 'String',
-                    value: `${characterMeta.Name || 'Someone'}${payload.leaveMessage || ' has left.'}`
-                }]
-            })
-            messageBus.send({
-                type: 'RoomUpdate',
-                roomId: characterMeta.RoomId
-            })
-
-            messageBus.send({
-                type: 'Perception',
-                characterId: payload.characterId,
-                ephemeraId: payload.roomId,
-                header: true
-            })
-
-            messageBus.send({
-                type: 'PublishMessage',
-                targets: [payload.roomId, payload.characterId],
-                displayProtocol: 'WorldMessage',
-                message: [{
-                    tag: 'String',
-                    value: `${characterMeta.Name || 'Someone'} has arrived.`
-                }]
-            })
-            messageBus.send({
-                type: 'RoomUpdate',
-                roomId: payload.roomId
-            })
-            messageBus.send({
-                type: 'MapUpdate',
-                characterId: payload.characterId,
-                previousRoomId: characterMeta.RoomId,
-                roomId: payload.roomId
-            })
+            ])
     
         }, { retryErrors: ['TransactionCanceledException']})
     }))
