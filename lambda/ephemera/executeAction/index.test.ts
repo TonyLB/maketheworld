c211f11ff997ae717aa5f81a9444adbad6fb9a92
TonyLB <anthony.lowerbasch@gmail.com>
2023-08-08 22:47:20 -0400

    ISS2809: Removed unused multiTableTransactWrite imports

--- lambda/ephemera/executeAction/index.test.ts
+++ lambda/ephemera/executeAction/index.test.ts
@@ -2,7 +2,7 @@ jest.mock('../messageBus')
 import messageBus from '../messageBus'
 
 jest.mock('@tonylb/mtw-utilities/dist/dynamoDB')
-import { ephemeraDB, multiTableTransactWrite, exponentialBackoffWrapper } from "@tonylb/mtw-utilities/dist/dynamoDB"
+import { ephemeraDB, exponentialBackoffWrapper } from "@tonylb/mtw-utilities/dist/dynamoDB"
 
 jest.mock('../internalCache')
 import internalCache from '../internalCache'
