82a8454dfa2d4d7930dba10057b988f0d93e5eaf
TonyLB <anthony.lowerbasch@gmail.com>
2023-09-04 18:33:44 -0400

    ISS2990: Corrected messageSync state machine CF syntax

--- lambda/deliverMessageSync/app.ts
+++ lambda/deliverMessageSync/app.ts
@@ -6,6 +6,7 @@ export const handler = async (event: any) => {
     const { connectionId } = event.requestContext || {}
 
     console.log(`deliverMessageSync called on connectionId: ${connectionId}`)
+    console.log(`event: ${JSON.stringify(event, null, 4)}`)
 
     return { statusCode: 200 }
 
