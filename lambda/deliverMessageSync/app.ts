6fb247b5226b5eb4e5c082dd1a8d54416f6d956e
TonyLB <anthony.lowerbasch@gmail.com>
2023-09-05 08:58:24 -0400

    ISS2990: Added functionality to deliverMessageSync lambda

--- lambda/deliverMessageSync/app.ts
+++ lambda/deliverMessageSync/app.ts
@@ -1,12 +1,41 @@
 // Copyright 2023 Tony Lower-Basch. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+import { apiClient } from "@tonylb/mtw-utilities/dist/apiManagement/apiManagementClient"
+import { unmarshall } from "@aws-sdk/util-dynamodb"
+
 export const handler = async (event: any) => {
 
-    const { connectionId } = event.requestContext || {}
+    const { ConnectionId, RequestId, Target: TargetId, Items } = event
 
-    console.log(`deliverMessageSync called on connectionId: ${connectionId}`)
-    console.log(`event: ${JSON.stringify(event, null, 4)}`)
+    //
+    // TODO: Adapt translation function from syncHandler to apply to Items and generate
+    // messages
+    //
+    const messages = Items
+        .map(unmarshall)
+        .map(({ Target, DeltaId, RowId, ...rest }) => ({ Target: TargetId, MessageId: RowId, ...rest }))
+
+    if (RequestId) {
+        await apiClient.send({
+            ConnectionId,
+            Data: JSON.stringify({
+                messageType: 'Messages',
+                messages,
+                LastSync: Date.now(),
+                RequestId
+            })
+        })
+    }
+    else {
+        await apiClient.send({
+            ConnectionId,
+            Data: JSON.stringify({
+                messageType: 'Messages',
+                messages
+            })
+        })    
+    }
 
     return { statusCode: 200 }
 
