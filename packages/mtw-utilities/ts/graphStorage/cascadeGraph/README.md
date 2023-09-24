4232dd8a08bbc83173f12f16476175984ef4c82f
TonyLB <anthony.lowerbasch@gmail.com>
2023-09-24 14:28:13 -0400

    ISS3046: Rearchitected CascadeGraph preProcessing

--- packages/mtw-utilities/ts/graphStorage/cascadeGraph/README.md
+++ packages/mtw-utilities/ts/graphStorage/cascadeGraph/README.md
@@ -3,9 +3,17 @@
 
 # Cascade Graph
 
-A Cascade Graph is a graph-encoded representation of a cascading data pipeline ... one which conceptually
-operates on successive generations of nodes to transform and side-effect them, then uses the results of
-that to inform the processing of the next generation (i.e., children know the results of their parents).
+A Cascade Graph is a graph-encoded representation of a cascading data pipeline that changes graph-connected
+data in a cascade process.
+
+In concept, this means that the Cascade Graph does the following:
+    - Accepts a template telling it how the topology and meta-data of the data
+    - Looks at the current state of data node by node and cache it
+    - Operate on the nodes in topological sort order, creating a new value from:
+        - The result values of any parent nodes
+        - The existing value of the current node
+    - Cascade that new value forward into the process of child nodes
+    - Write changes *through* the cache into the underlying persistent layer
 
 ## Template
 
@@ -18,19 +26,31 @@ part of the run itself.
 A working graph is in the same topology as the template, but each node has additional data that is side-effected
 by the process as it proceeds.
 
-## preProcess
+## fetch
 
-The first stage of a cascade is an asynchronous preProcess function run on each node **individually**.
-Failure of a preProcess is unrecoverable (so it should handle its own exceptions), since there is no
+The first stage of a cascade is to fetch data on enough starting nodes to begin processing.  The Cascade Graph
+has a *fetch* function which takes a list of node keys and returns a list of NodeFetchData records, one for each
+key.
+
+## process
+
+The next stage of a cascade is an asynchronous process function run on each node **individually**.
+Failure of a process is unrecoverable (so it should handle its own exceptions), since there is no
 orchestration for the Cascade Graph system to do in retrying such a function.
+
+If writes back to the persistence layer should happen at each individual node, than persistence
+can be addressed at this stage.
 
 ## aggregate (optional)
 
-The second stage of the cascade is an asynchronous aggregate function that runs on subGraphs of the
+The optional final stage of the cascade is an asynchronous aggregate function that runs on subGraphs of the
 working set (split according to a *threshold* function) and operates on many nodes simultaneously.
 Failure of an aggregate function can be retried by Cascade Graph:  A failed graph has the *retry*
-function executed upon its nodes, and then is started again from a fresh copy of template at the
-preProcess step (with limits and exponential backoff).
+function executed upon its nodes, and then is started again from a fresh copy of sub-template at the
+fetch step (with limits and exponential backoff).
+
+If writes back to the persistence layer can be profitably grouped (so as to prevent delays due to needing
+to re-check data that was just written in a previous stage) then it should be done in this stage.
 
 ---
 ---
--- packages/mtw-utilities/ts/graphStorage/cascadeGraph/index.ts
+++ packages/mtw-utilities/ts/graphStorage/cascadeGraph/index.ts
@@ -1,61 +1,102 @@
+import { unique } from "../../lists";
 import { Graph } from "../utils/graph";
 
-type CascadeGraphPriorResult<KeyType extends string, NodeTemplateData extends {}, EdgeTemplateData extends {}, NodeWorkingData extends {}> = {
+type CascadeGraphPriorResult<KeyType extends string, NodeTemplateData extends {}, NodeFetchData extends{}, EdgeTemplateData extends {}, NodeWorkingData extends {}> = {
     key: KeyType;
     edge: EdgeTemplateData;
-    node: NodeWorkingData;
+    fetch: NodeFetchData;
+    result: NodeWorkingData;
 }
 
-export class CascadeGraphWorkspace<KeyType extends string, NodeTemplateData extends {}, EdgeTemplateData extends {}, NodeWorkingData extends {}> {
+export class CascadeGraphWorkspace<
+        KeyType extends string,
+        NodeTemplateData extends {},
+        EdgeTemplateData extends {},
+        NodeFetchData extends {},
+        NodeWorkingData extends {}
+    > {
     _template: Graph<KeyType, { key: KeyType } & NodeTemplateData, EdgeTemplateData>;
-    _working: Graph<KeyType, { key: KeyType } & Partial<NodeWorkingData>, {}>;
+    _working?: Graph<KeyType, { key: KeyType } & ({} | NodeFetchData | (NodeFetchData & NodeWorkingData)), {}>;
 
     constructor(props: {
         template: Graph<KeyType, { key: KeyType } & NodeTemplateData, EdgeTemplateData>;
-        nodeData: ({ key: KeyType } & Partial<NodeWorkingData>)[];
+        nodeData: ({ key: KeyType } & NodeFetchData)[];
     }) {
         this._template = props.template.filter({ keys: props.nodeData.map(({ key }) => (key)) })
-        this._working = new Graph<KeyType, { key: KeyType } & Partial<NodeWorkingData>, {}>(
+        this._working = new Graph<KeyType, { key: KeyType } & ({} | NodeFetchData | (NodeFetchData & NodeWorkingData)), {}>(
             Object.assign({}, ...props.nodeData.map(({ key, ...rest }) => ({ [key]: { key, ...rest }}))),
             [],
-            {} as Omit<{ key: KeyType } & Partial<NodeWorkingData>, "key">
+            {} as Omit<{ key: KeyType }, "key">
         )
     }
 
 }
-export class CascadeGraph<KeyType extends string, NodeTemplateData extends {}, EdgeTemplateData extends {}, NodeWorkingData extends {}> {
+export class CascadeGraph<KeyType extends string, NodeTemplateData extends {}, NodeFetchData extends {}, EdgeTemplateData extends {}, NodeWorkingData extends {}> {
     _template: Graph<KeyType, { key: KeyType } & NodeTemplateData, EdgeTemplateData>
-    _preProcess: (props: {
-        template: { key: KeyType } & NodeTemplateData;
-        priors: ({ key: KeyType } & EdgeTemplateData)[];
-    }) => Promise<NodeWorkingData>;
+    _fetch: (nodes: KeyType[]) => Promise<{ key: KeyType } & NodeFetchData>[];
     _process: (props: {
             template: { key: KeyType } & NodeTemplateData;
-            working: NodeWorkingData;
-            priors: CascadeGraphPriorResult<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>[];
+            fetch: NodeFetchData;
+            priors: CascadeGraphPriorResult<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>[];
         }) => Promise<NodeWorkingData>;
-    _aggregate: (graph: CascadeGraphWorkspace<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>) => Promise<CascadeGraphWorkspace<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>>;
+    _aggregate: (graph: CascadeGraphWorkspace<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>) => Promise<CascadeGraphWorkspace<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>>;
 
     constructor(props: {
         template: Graph<KeyType, { key: KeyType } & NodeTemplateData, EdgeTemplateData>;
-        preProcess: (props: {
-            template: { key: KeyType } & NodeTemplateData;
-            priors: ({ key: KeyType } & EdgeTemplateData)[];
-        }) => Promise<NodeWorkingData>;
+        fetch: (nodes: KeyType[]) => Promise<{ key: KeyType } & NodeFetchData>[];
         process: (props: {
                 template: { key: KeyType } & NodeTemplateData;
-                working: NodeWorkingData;
-                priors: CascadeGraphPriorResult<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>[];
+                fetch: NodeFetchData;
+                priors: CascadeGraphPriorResult<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>[];
             }) => Promise<NodeWorkingData>;
-        aggregate: (graph: CascadeGraphWorkspace<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>) => Promise<CascadeGraphWorkspace<KeyType, NodeTemplateData, EdgeTemplateData, NodeWorkingData>>;
+        aggregate: (graph: CascadeGraphWorkspace<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>) => Promise<CascadeGraphWorkspace<KeyType, NodeTemplateData, NodeFetchData, EdgeTemplateData, NodeWorkingData>>;
     }) {
         this._template = props.template
-        this._preProcess = props.preProcess
+        this._fetch = props.fetch
         this._process = props.process
         this._aggregate = props.aggregate
     }
 
     async execute(): Promise<void> {
+        const generationOrderOutput = this._template.generationOrder()
+        const stronglyConnectedComponentByContents = generationOrderOutput.flat(1).reduce<Partial<Record<KeyType, KeyType>>>(
+            (previous, stronglyConnectedComponent) => (stronglyConnectedComponent.reduce<Partial<Record<KeyType, KeyType>>>((aggregator, key) => ({ ...aggregator, [key]: stronglyConnectedComponent[0] }), previous)),
+            {}
+        )
 
+        //
+        // TODO: Refactor code borrowed from sortedWalk to:
+        //    - fetch all nodes in the current generation
+        //    - Create a promise to process each stronglyConnectedComponent
+        //
+        let resultPromises: Partial<Record<KeyType, Promise<Previous>>> = {}
+        for (const generation of generationOrderOutput) {
+            for (const stronglyConnectedComponent of generation) {
+                //
+                // Find all Edges leading from *outside* keys to *inside* keys. By definition of generationOrder,
+                // all those exits should lead to keys that have already been processed, and (therefore) have promises
+                // in the resultPromises records defined above.  Collect all unique SCC representatives that this new set of
+                // keys depends from, and deliver the saved Previous outputs to the callback.
+                //
+                resultPromises[stronglyConnectedComponent[0]] = (async (): Promise<Previous> => {
+                    if (stronglyConnectedComponent.length === 0) {
+                        throw new Error('sortedWalk error, empty strongly-connected-component encountered')
+                    }
+                    const dependencyEdges = this._template.edges
+                        .filter(({ to }) => (stronglyConnectedComponent.includes(to)))
+                        .filter(({ from }) => (!stronglyConnectedComponent.includes(from)))
+                    const dependencyResultPromises = unique(dependencyEdges.map(({ from }) => (from)))
+                        .map((dependency) => (stronglyConnectedComponentByContents[dependency]))
+                        .map((stronglyConnectedComponentRepresentative) => (
+                            stronglyConnectedComponentRepresentative &&
+                            resultPromises[stronglyConnectedComponentRepresentative]
+                        ))
+                        .filter((results) => (typeof results !== 'undefined'))
+                    const dependencyResults: Previous[] = (await Promise.all(dependencyResultPromises)).filter((results) => (typeof results !== 'undefined')) as Previous[]
+                    return await callback({ keys: stronglyConnectedComponent, previous: dependencyResults })
+                })()
+            }
+        }
+        await Promise.all(Object.values(resultPromises))
     }
 }
