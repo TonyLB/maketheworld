fe61dbd39bb9ca698c83211696dc8ccf49560be6
TonyLB <anthony.lowerbasch@gmail.com>
2023-10-02 08:35:24 -0400

    ISS3050: Added stub wml lambda

---
+++ lambda/wml/app.ts
@@ -0,0 +1,7 @@
+import { AssetWorkspaceAddress } from '@tonylb/mtw-asset-workspace/ts/readOnly'
+
+export const handler = async (event: { address?: AssetWorkspaceAddress }) => {
+
+    const { address } = event
+
+}
