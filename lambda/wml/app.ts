83c234f6d1e386b1721749afc91903e5afc57e9e
TonyLB <anthony.lowerbasch@gmail.com>
2023-10-03 20:44:57 -0400

    ISS3050: Added player and requestId arguments to parseWML step function

--- lambda/wml/app.ts
+++ lambda/wml/app.ts
@@ -1,8 +1,8 @@
 import { AssetWorkspaceAddress } from '@tonylb/mtw-asset-workspace/ts/readOnly'
 
-export const handler = async (event: { address?: AssetWorkspaceAddress }) => {
+export const handler = async (event: { address?: AssetWorkspaceAddress; player: string; requestId: string }) => {
 
-    const { address } = event
+    const { address, player, requestId } = event
 
     console.log(`Address to Parse: ${JSON.stringify(address, null, 4)}`)
 }
