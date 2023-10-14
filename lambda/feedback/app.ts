3749acd2905397c7c0c183f67dc19687d0aa8944
TonyLB <anthony.lowerbasch@gmail.com>
2023-10-14 17:49:31 -0400

    ISS3101: Added Success path to feedback lambda

--- lambda/feedback/app.ts
+++ lambda/feedback/app.ts
@@ -1,5 +1,31 @@
+import { apiClient } from "./clients"
+
 export const handler = async (event) => {
 
     console.log(`event: ${JSON.stringify(event, null, 4)}`)
 
+    await Promise.all(event.Records.map(async ({ Sns }) => {
+        if (
+            Sns.MessageAttributes.ConnectionIds?.Type !== 'String' ||
+            !Array.isArray(JSON.parse(Sns.MessageAttributes.ConnectionIds.Value)) ||
+            Sns.MessageAttributes.RequestId?.Type !== 'String' ||
+            Sns.MessageAttributes.Type?.Type !== 'String'
+        ) {
+            throw new Error(`Incoming message format failure (${JSON.stringify(Sns.MessageAttributes, null, 4)})`)
+        }
+        const connectionIds = JSON.parse(Sns.MessageAttributes.ConnectionIds.Value) as string[]
+        const RequestId = Sns.MessageAttributes.RequestId.Value
+        switch(Sns.MessageAttributes.Type.Value) {
+            case 'Success':
+                const Data = JSON.stringify({
+                    ...JSON.parse(Sns.Message),
+                    RequestId
+                })
+                await Promise.all(connectionIds.map((ConnectionId) => (apiClient.send({
+                    ConnectionId,
+                    Data
+                }))))
+                break
+        }
+    }))
 }
