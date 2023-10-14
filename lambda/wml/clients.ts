05de2f78566e30e94e5ca9027ec911e6c4a1c314
TonyLB <anthony.lowerbasch@gmail.com>
2023-10-14 18:23:54 -0400

    ISS3101: Refactored wml lambda to use MTWFeedback topic

--- lambda/wml/clients.ts
+++ lambda/wml/clients.ts
@@ -1,9 +1,12 @@
 import { EventBridgeClient, PutEventsCommand } from "@aws-sdk/client-eventbridge"
 import { apiClient as apiClientImport } from "@tonylb/mtw-utilities/dist/apiManagement/apiManagementClient"
 import { SFNClient } from "@aws-sdk/client-sfn"
+import { SNSClient } from "@aws-sdk/client-sns"
+import AWSXRay from 'aws-xray-sdk'
 
-export const ebClient = new EventBridgeClient({ region: process.env.AWS_REGION })
-export const sfnClient = new SFNClient({ region: process.env.AWS_REGION })
+export const ebClient = AWSXRay.captureAWSv3Client(new EventBridgeClient({ region: process.env.AWS_REGION }))
+export const sfnClient = AWSXRay.captureAWSv3Client(new SFNClient({ region: process.env.AWS_REGION }))
+export const snsClient = AWSXRay.captureAWSv3Client(new SNSClient({ region: process.env.AWS_REGION }))
 
 export const apiClient = {
     send: (message: any) => {
