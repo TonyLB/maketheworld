7314d1c357257d234f0786bdf4c21069dc349570
TonyLB <anthony.lowerbasch@gmail.com>
2023-10-19 14:31:09 -0400

    ISS3149: Added deIndentWML testing utility

--- packages/mtw-wml/ts/simpleSchema/index.test.ts
+++ packages/mtw-wml/ts/simpleSchema/index.test.ts
@@ -3,6 +3,7 @@ import tokenizer from '../parser/tokenizer'
 import SourceStream from '../parser/tokenizer/sourceStream'
 
 import { schemaFromParse, schemaToWML } from '.'
+import { deIndentWML } from './utils'
 
 describe('schemaFromParse', () => {
     beforeEach(() => {
@@ -624,188 +625,210 @@ describe('schemaToWML', () => {
     })
 
     it('should correctly round-trip complicated rooms', () => {
-        const testWML = `<Asset key=(Test)>
-    <Room key=(VORTEX)>
-        <Name>Vortex</Name>
-        <Description>
-            You float in a swirling mass of energy and debris.
-            <Link to=(doors)>Doors</Link>
-            to other realms drift around you.
-        </Description>
-        <Exit to=(welcome)>Welcome room</Exit>
-    </Room>
-    <Feature key=(doors)>
-        <Name>Drifting doors</Name>
-        <Description>Doors drifting in space</Description>
-    </Feature>
-    <Room key=(welcome)>
-        <Name>Welcome room</Name>
-        <Description>
-            A clean and sterile welcome room. The lights are
-            <If {lights}>on</If><Else>off</Else>.
-        </Description>
-        <Exit to=(VORTEX)>vortex</Exit>
-    </Room>
-    <Variable key=(lights) default={true} />
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(VORTEX)>
+                    <Name>Vortex</Name>
+                    <Description>
+                        You float in a swirling mass of energy and debris.
+                        <Link to=(doors)>Doors</Link>
+                        to other realms drift around you.
+                    </Description>
+                    <Exit to=(welcome)>Welcome room</Exit>
+                </Room>
+                <Feature key=(doors)>
+                    <Name>Drifting doors</Name>
+                    <Description>Doors drifting in space</Description>
+                </Feature>
+                <Room key=(welcome)>
+                    <Name>Welcome room</Name>
+                    <Description>
+                        A clean and sterile welcome room. The lights are
+                        <If {lights}>on</If><Else>off</Else>.
+                    </Description>
+                    <Exit to=(VORTEX)>vortex</Exit>
+                </Room>
+                <Variable key=(lights) default={true} />
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip variables and actions', () => {
-        const testWML = `<Asset key=(Test)>
-    <Variable key=(lights) default={true} />
-    <Variable key=(power) default={true} />
-    <Computed key=(illumination) src={lights && power} />
-    <Action key=(flipLightSwitch) src={
-        lights = !lights
-    } />
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Variable key=(lights) default={true} />
+                <Variable key=(power) default={true} />
+                <Computed key=(illumination) src={lights && power} />
+                <Action key=(flipLightSwitch) src={
+                    lights = !lights
+                } />
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip knowledge items', () => {
-        const testWML = `<Asset key=(Test)>
-    <Knowledge key=(test)>
-        <Name>Learning is power!</Name>
-        <Description>There is so very much to see and discover!</Description>
-    </Knowledge>
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Knowledge key=(test)>
+                    <Name>Learning is power!</Name>
+                    <Description>There is so very much to see and discover!</Description>
+                </Knowledge>
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip a character', () => {
-        const testWML = `<Character key=(TESS)>
-    <Name>Tess</Name>
-    <Pronouns
-        subject="she"
-        object="her"
-        possessive="hers"
-        adjective="her"
-        reflexive="herself"
-    />
-    <FirstImpression>Frumpy Goth</FirstImpression>
-    <Outfit>
-        A tattered frock-coat kitbashed out of a black hoodie and dyed lace.
-    </Outfit>
-    <OneCoolThing>Fuchsia Eyes</OneCoolThing>
-    <Image key=(TESSIcon) />
-    <Import from=(base) />
-</Character>`
+        const testWML = deIndentWML(`
+            <Character key=(TESS)>
+                <Name>Tess</Name>
+                <Pronouns
+                    subject="she"
+                    object="her"
+                    possessive="hers"
+                    adjective="her"
+                    reflexive="herself"
+                />
+                <FirstImpression>Frumpy Goth</FirstImpression>
+                <Outfit>
+                    A tattered frock-coat kitbashed out of a black hoodie and dyed lace.
+                </Outfit>
+                <OneCoolThing>Fuchsia Eyes</OneCoolThing>
+                <Image key=(TESSIcon) />
+                <Import from=(base) />
+            </Character>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip asset-level conditions', () => {
-        const testWML = `<Asset key=(Test)>
-    <Feature key=(doors)>
-        <Name>Drifting doors</Name>
-        <Description>Doors drifting in space</Description>
-    </Feature>
-    <If {lights}>
-        <Feature key=(doors)>
-            <Description>, lit from a distant star</Description>
-        </Feature>
-    </If>
-    <Else>
-        <Feature key=(doors)>
-            <Description>, dark and cold</Description>
-        </Feature>
-    </Else>
-    <Variable key=(lights) default={true} />
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Feature key=(doors)>
+                    <Name>Drifting doors</Name>
+                    <Description>Doors drifting in space</Description>
+                </Feature>
+                <If {lights}>
+                    <Feature key=(doors)>
+                        <Description>, lit from a distant star</Description>
+                    </Feature>
+                </If>
+                <Else>
+                    <Feature key=(doors)>
+                        <Description>, dark and cold</Description>
+                    </Feature>
+                </Else>
+                <Variable key=(lights) default={true} />
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
         it('should correctly round-trip nested description conditions', () => {
-        const testWML = `<Asset key=(Test)>
-    <Feature key=(doors)>
-        <Name>Drifting doors</Name>
-        <Description>
-            Doors drifting in space,
-            <If {lights}>
-                <If {solar}>lit from a distant star</If>
-                <Else>lit by a swelling moon</Else>
-            </If>
-            <Else>dark and cold</Else>
-        </Description>
-    </Feature>
-    <Variable key=(lights) default={true} />
-    <Variable key=(solar) default={true} />
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Feature key=(doors)>
+                    <Name>Drifting doors</Name>
+                    <Description>
+                        Doors drifting in space,
+                        <If {lights}>
+                            <If {solar}>lit from a distant star</If>
+                            <Else>lit by a swelling moon</Else>
+                        </If>
+                        <Else>dark and cold</Else>
+                    </Description>
+                </Feature>
+                <Variable key=(lights) default={true} />
+                <Variable key=(solar) default={true} />
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip nested line-wrapped text', () => {
-        const testWML = `<Asset key=(Test)>
-    <Feature key=(doors)>
-        <Name>Drifting doors</Name>
-        <Description>
-            <If {lights}>
-                Testing a long text string that will require line wrapping to
-                render in its entirety
-            </If>
-        </Description>
-    </Feature>
-    <Variable key=(lights) default={true} />
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Feature key=(doors)>
+                    <Name>Drifting doors</Name>
+                    <Description>
+                        <If {lights}>
+                            Testing a long text string that will require line wrapping to
+                            render in its entirety
+                        </If>
+                    </Description>
+                </Feature>
+                <Variable key=(lights) default={true} />
+            </Asset>
+        `)
         expect(schemaToWML(schemaFromParse(parse(tokenizer(new SourceStream(testWML)))))).toEqual(testWML)
     })
 
     it('should correctly round-trip display tags', () => {
-        const testWML = `<Asset key=(Test)>
-    <Feature key=(doors)>
-        <Description>
-            <After>Dingy doors</After>
-            <Replace>portals</Replace>
-            <Before>Clean<Space /></Before>
-        </Description>
-    </Feature>
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Feature key=(doors)>
+                    <Description>
+                        <After>Dingy doors</After>
+                        <Replace>portals</Replace>
+                        <Before>Clean<Space /></Before>
+                    </Description>
+                </Feature>
+            </Asset>
+        `)
         const schema = schemaFromParse(parse(tokenizer(new SourceStream(testWML))))
         expect(schemaToWML(schema)).toEqual(testWML)
     })
 
     it('should correctly round-trip space tags in connected conditionals', () => {
-        const testWML = `<Asset key=(Test)>
-    <Variable key=(testVar) default={false} />
-    <Room key=(test)>
-        <Description>
-            Test
-            <If {testVar}>
-                <Space />
-                TestTwo
-            </If><If {!testVar}>
-                <Space />
-                TestThree
-            </If><Bookmark key=(testBookmark) />
-        </Description>
-    </Room>
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Variable key=(testVar) default={false} />
+                <Room key=(test)>
+                    <Description>
+                        Test
+                        <If {testVar}>
+                            <Space />
+                            TestTwo
+                        </If><If {!testVar}>
+                            <Space />
+                            TestThree
+                        </If><Bookmark key=(testBookmark) />
+                    </Description>
+                </Room>
+            </Asset>
+        `)
         const schema = schemaFromParse(parse(tokenizer(new SourceStream(testWML))))
         expect(schemaToWML(schema)).toEqual(testWML)
     })
 
     it('should correctly escape special characters', () => {
-        const testWML = `<Asset key=(Test)>
-    <Room key=(test)>
-        <Description>
-            Test \\\\ \\< \\>
-        </Description>
-    </Room>
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(test)>
+                    <Description>
+                        Test \\\\ \\< \\>
+                    </Description>
+                </Room>
+            </Asset>
+        `)
         const schema = schemaFromParse(parse(tokenizer(new SourceStream(testWML))))
         expect(schemaToWML(schema)).toEqual('<Asset key=(Test)>\n    <Room key=(test)><Description>Test \\\\ \\< \\></Description></Room>\n</Asset>')
     })
 
     it('should correctly round-trip import and export', () => {
-        const testWML = `<Asset key=(Test)>
-    <Import from=(BASE)>
-        <Room key=(test) from=(Room1) />
-        <Room key=(testTwo) />
-    </Import>
-    <Variable key=(testVar) default={false} />
-    <Room key=(test)><Description>Test</Description></Room>
-    <Export><Room key=(test) as=(Room2) /></Export>
-</Asset>`
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Import from=(BASE)>
+                    <Room key=(test) from=(Room1) />
+                    <Room key=(testTwo) />
+                </Import>
+                <Variable key=(testVar) default={false} />
+                <Room key=(test)><Description>Test</Description></Room>
+                <Export><Room key=(test) as=(Room2) /></Export>
+            </Asset>
+        `)
         const schema = schemaFromParse(parse(tokenizer(new SourceStream(testWML))))
         expect(schemaToWML(schema)).toEqual(testWML)
     })
--- packages/mtw-wml/ts/simpleSchema/utils.test.ts
+++ packages/mtw-wml/ts/simpleSchema/utils.test.ts
@@ -1,4 +1,4 @@
-import { compressWhitespace } from './utils'
+import { compressWhitespace, deIndentWML } from './utils'
 
 describe('compressWhitespace', () => {
     it('should return empty on an empty input', () => {
@@ -117,4 +117,21 @@ describe('compressWhitespace', () => {
         ])
     })
 
+})
+
+describe('deIndentWML', () => {
+    it('should leave unindented WML unchanged', () => {
+        const testWML = '<Asset key=(Test)>\n    <Room key=(ABC)>\n        <Exit to=(DEF)>Test Exit</Exit>\n    </Room>\n</Asset>'
+        expect(deIndentWML(testWML)).toEqual(testWML)
+    })
+
+    it('should unindent', () => {
+        expect(deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <Exit to=(DEF)>Test Exit</Exit>
+                </Room>
+            </Asset>
+        `)).toEqual('<Asset key=(Test)>\n    <Room key=(ABC)>\n        <Exit to=(DEF)>Test Exit</Exit>\n    </Room>\n</Asset>')
+    })
 })
