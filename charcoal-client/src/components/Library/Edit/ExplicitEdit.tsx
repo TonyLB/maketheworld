d5de617744d15e0083383b4ecbc9baf93426ccd5
TonyLB <anthony.lowerbasch@gmail.com>
2023-11-04 09:44:15 -0400

    ISS3165: Added validation and helperText to ExplicitEdit

--- charcoal-client/src/components/Library/Edit/ExplicitEdit.tsx
+++ charcoal-client/src/components/Library/Edit/ExplicitEdit.tsx
@@ -4,8 +4,9 @@ import EditIcon from '@mui/icons-material/Edit'
 import CheckIcon from '@mui/icons-material/Check'
 import ClearIcon from '@mui/icons-material/Clear'
 
-export const ExplicitEdit: FunctionComponent<{ value: string; onChange: (value: string) => void }> = ({ value, onChange }) => {
+export const ExplicitEdit: FunctionComponent<{ value: string; onChange: (value: string) => void; validate?: (value: string) => boolean; helperText?: string }> = ({ value, onChange, validate, helperText }) => {
     const [currentValue, setCurrentValue] = useState<string>(value)
+    const [isValid, setIsValid] = useState<boolean>(true)
     useEffect(() => {
         setCurrentValue(value)
     }, [value])
@@ -16,20 +17,38 @@ export const ExplicitEdit: FunctionComponent<{ value: string; onChange: (value: 
                     size="small"
                     variant="standard"
                     value={currentValue}
+                    error={!isValid}
+                    helperText={!isValid && helperText}
                     onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        setCurrentValue(event.target.value);
+                        setCurrentValue(event.target.value)
+                        if (validate && !validate(event.target.value)) {
+                            setIsValid(false)
+                        }
+                        else {
+                            setIsValid(true)
+                        }
                     }}
                 />
                 <IconButton
                     size="small"
+                    disabled={!isValid}
                     onClick={() => {
-                        onChange(value)
-                        setEditing(false)
+                        if (isValid) {
+                            onChange(value)
+                            setEditing(false)
+                        }
                     } }
                 >
                     <CheckIcon fontSize="inherit" />
                 </IconButton>
-                <IconButton size="small" onClick={() => { setEditing(false) } }>
+                <IconButton
+                    size="small"
+                    onClick={() => {
+                        setEditing(false)
+                        setCurrentValue(value)
+                        setIsValid(true)
+                    } }
+                >
                     <ClearIcon fontSize="inherit" />
                 </IconButton>
             </React.Fragment>
