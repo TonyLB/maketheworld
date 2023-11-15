11a12627188ca541e6cca337c2a6efe7c3e7f139
TonyLB <anthony.lowerbasch@gmail.com>
2023-11-14 20:47:31 -0500

    ISS3203: Lifted MapLayer conditional toggle into redux slice

--- charcoal-client/src/slices/UI/index.ts
+++ charcoal-client/src/slices/UI/index.ts
@@ -4,13 +4,15 @@ import feedback from './feedback'
 import choiceDialog from './choiceDialog'
 import navigationTabs from './navigationTabs'
 import lineEntry from './lineEntry'
+import mapEdit from './mapEdit'
 
 export const reducer = combineReducers({
     feedback,
     characterEdit,
     choiceDialog,
     navigationTabs,
-    lineEntry
+    lineEntry,
+    mapEdit
 })
 
 export default reducer
---
+++ charcoal-client/src/slices/UI/mapEdit/index.ts
@@ -0,0 +1,42 @@
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { Selector } from '../../../store'
+
+//
+// UI/MapEdit redux slice stores a list of conditionals that have been hidden in the UI
+//
+type MapEditData = Record<string, string[]>
+
+const initialState = {} as MapEditData
+
+const mapEditSlice = createSlice({
+    name: 'mapEdit',
+    initialState,
+    reducers: {
+        toggle(state, action: PayloadAction<{ mapId: string, key: string }>) {
+            const { mapId, key } = action.payload
+            if (mapId in state) {
+                const mapState = state[mapId]
+                if (mapState.includes(key)) {
+                    state[mapId] = mapState.filter((conditional) => (conditional !== key))
+                }
+                else {
+                    state[mapId] = [...mapState, key]
+                }
+            }
+            else {
+                state[mapId] = [key]
+            }
+        }
+    }
+})
+
+export const { toggle } = mapEditSlice.actions
+
+export const mapEditAllConditions: Selector<Record<string, string[]>> = ({ UI: { mapEdit = {} } }) => (mapEdit)
+
+export const mapEditConditionState = (mapId: string, key: string): Selector<boolean> => createSelector(
+    mapEditAllConditions,
+    (mapEditAllConditions) => (mapEditAllConditions[mapId].includes(key))
+)
+
+export default mapEditSlice.reducer
