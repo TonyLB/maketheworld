05699facfa99e3c27e13bcd674f618176b187076
TonyLB <anthony.lowerbasch@gmail.com>
2023-11-22 08:58:33 -0500

    ISS3221: Added mergeTrees function

--- packages/mtw-sequence/ts/tree/merge.ts
+++ packages/mtw-sequence/ts/tree/merge.ts
@@ -1,32 +1,16 @@
+import shortestCommonSupersequence from "../shortestCommonSupersequence";
 import { GenericTree } from "./baseClasses"
+import { TreeUtility } from "./utilityClass";
 
-//
-// TODO: Definte an index-substitution schema for the generic tree (probably accepting
-// a comparison helper method as an argument)
-//
-
-//
-// TODO: Lift navigationSequenceReducer methods from orderedConditionalTree and generalize
-//
-
-//
-// TODO: Lift and generalize mergeOrderedConditionalTrees from orderedConditionalTree
-//
-
-//
-// TODO: Define P-${index} property nodes that are the first listed "contents" of a
-// node's appearance in the tree, and which identify a set of properties for that node
-// (independent of the node's children).  This lets the item defining the node ITSELF
-// be identical (and matchable), while also allowing the merging of items with different
-// property values.
-//
-
-//
-// TODO: Add a mergedNode function that takes all incoming P-${index} properties, as well
-// as the merged set of children, and exports the final node value back into GenericTree
-// structure.
-//
-
-export const mergeTrees = <N extends {}>(...args: GenericTree<N>[]): GenericTree<N> => {
-    return []
+export const mergeTrees = <N extends {}, InternalNode extends {}>(options: {
+    compare: (A: N, B: N) => boolean;
+    extractProperties: (value: N) => InternalNode | undefined;
+    rehydrateProperties: (baseValue: N, properties: InternalNode[]) => N;
+}) => (...args: GenericTree<N>[]): GenericTree<N> => {
+    const treeUtility = new TreeUtility(options)
+    const sequences = args.map((tree) => (treeUtility.treeToSequence(tree)))
+    const mergedSequence = sequences.reduce<number[]>((previous, sequence) => (shortestCommonSupersequence(previous, sequence)), [])
+    return treeUtility.sequenceToTree(mergedSequence)
 }
+
+export default mergeTrees
