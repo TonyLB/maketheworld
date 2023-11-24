76bbf3ada727aa7f85512720a34dff9d87ce88dd
TonyLB <anthony.lowerbasch@gmail.com>
2023-11-24 16:26:15 -0500

    ISS3193: Refactored map controller to combine trees for all kinds of relevant components

--- charcoal-client/src/components/Maps/Edit/Controller/index.tsx
+++ charcoal-client/src/components/Maps/Edit/Controller/index.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent, useContext, useMemo, useState } from "react"
 import { useLibraryAsset } from "../../../Library/Edit/LibraryAsset"
-import { isNormalMap } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
-import { GenericTree  } from '@tonylb/mtw-sequence/dist/tree/baseClasses'
+import { isNormalExit, isNormalMap } from "@tonylb/mtw-wml/dist/normalize/baseClasses"
+import { GenericTree, GenericTreeNode  } from '@tonylb/mtw-sequence/dist/tree/baseClasses'
 import { mergeTrees } from '@tonylb/mtw-sequence/dist/tree/merge'
 import { MapTreeItem, ToolSelected } from "./baseClasses"
 import Normalizer from "@tonylb/mtw-wml/dist/normalize"
-import { isSchemaExit, isSchemaRoom } from "@tonylb/mtw-wml/dist/simpleSchema/baseClasses"
+import { SchemaConditionTag, SchemaRoomTag, isSchemaCondition, isSchemaExit, isSchemaRoom } from "@tonylb/mtw-wml/dist/simpleSchema/baseClasses"
 import { deepEqual } from "../../../../lib/objects"
+import { unique } from "../../../../lib/lists"
 
 //
 // extractMapTree takes a standardized normalizer, and a mapId, and generates a generic tree of MapTreeItems
@@ -17,26 +18,85 @@ const extractMapTree = ({ normalizer, mapId }: { normalizer: Normalizer, mapId: 
     if (!mapItem || !isNormalMap(mapItem)) {
         return []
     }
-    return mapItem.appearances.map<GenericTree<MapTreeItem>[]>(
+    const wrapConditionals = (conditions: SchemaConditionTag[], item: GenericTreeNode<MapTreeItem>): GenericTreeNode<MapTreeItem> => {
+        return conditions.reduceRight<GenericTreeNode<MapTreeItem>>((previous, condition) => ({
+            data: condition,
+            children: [previous]
+        }), item)
+    }
+    const baseRoomTags: Record<string, SchemaRoomTag> = mapItem.appearances.reduce<string[]>(
+        (previous, { rooms }) => (
+            unique(
+                previous,
+                rooms.map(({ key }) => (key))
+            )
+        ), [])
+        .reduce<Record<string, SchemaRoomTag>>((previous, key) => {
+            const roomTag = normalizer.referenceToSchema({ key, tag: 'Room', index: 0 })
+            if (!(roomTag && isSchemaRoom(roomTag))) {
+                throw new Error('Room lookup failure')
+            }
+            return {
+                ...previous,
+                [key]: roomTag
+            }
+        }, {})
+    const allRooms = mapItem.appearances.map<GenericTree<MapTreeItem>[]>(
         ({ rooms }) => (
             rooms
-                .filter(({ conditions }) => (conditions.length === 0))
-                .map(({ key }) => {
-                    const roomTag = normalizer.referenceToSchema({ key, tag: 'Room', index: 0 })
-                    if (roomTag && isSchemaRoom(roomTag)) {
-                        return [{
-                            data: roomTag,
-                            children: roomTag.contents
-                                .filter((childItem) => (isSchemaExit(childItem)))
-                                .map((childItem) => ({ data: childItem, children: [] }))
-                        }] as GenericTree<MapTreeItem>
-                    }
-                    else {
-                        return []
-                    }
+                .map(({ conditions, key }) => {
+                    return [wrapConditionals(
+                        conditions.length ? [{
+                            tag: 'If',
+                            conditions,
+                            contents: [],
+                        }]: [],
+                        {
+                            data: baseRoomTags[key],
+                            children: []
+                        }
+                    )]
                 })
         )
     ).flat(2)
+    const allExits: GenericTree<MapTreeItem>[] = Object.values(normalizer.normal)
+        .filter(isNormalExit)
+        .filter(({ to, from }) => (to in baseRoomTags && from in baseRoomTags))
+        .map<GenericTree<MapTreeItem>>((exitTag) => {
+            return exitTag.appearances.map<GenericTreeNode<MapTreeItem>>(({ contextStack, name }) => {
+                const conditions = contextStack
+                    .filter(({ tag } ) => (tag === 'If'))
+                    .map((reference) => {
+                        const condition = normalizer.referenceToSchema(reference)
+                        if (!(condition && isSchemaCondition(condition))) {
+                            throw new Error('Condition lookup failure')
+                        }
+                        return condition
+                    })
+                return wrapConditionals(conditions, {
+                    data: baseRoomTags[exitTag.from],
+                    children: [{
+                        data: { tag: 'Exit', key: exitTag.key, to: exitTag.to, from: exitTag.from, name, contents: [] },
+                        children: []
+                    }]
+                })
+            })
+        })
+    
+    const mergeTreeOptions = {
+        compare: (a: MapTreeItem, b: MapTreeItem) => (deepEqual(a, b)),
+        extractProperties: (item: MapTreeItem): MapTreeItem | undefined => {
+            return item
+        },
+        rehydrateProperties: (baseItem: MapTreeItem, properties: MapTreeItem[]) => (baseItem)
+    }
+    console.log(`allRooms: ${JSON.stringify(allRooms, null,)}`)
+    return mergeTrees(mergeTreeOptions)(
+        allRooms.filter(({ data }) => (data.tag === 'Room')),
+        allRooms.filter(({ data }) => (data.tag === 'If')),
+        ...allExits
+    )
+
 }
 
 type MapEditContextType = {
@@ -61,18 +121,9 @@ export const MapEditController: FunctionComponent<{ mapId: string }> = ({ childr
         normalizer.standardize()
         return normalizer
     }, [normalForm])
-    const tree = useMemo<GenericTree<MapTreeItem>>(() => {
-        const mergeTreeOptions = {
-            compare: (a: MapTreeItem, b: MapTreeItem) => (deepEqual(a, b)),
-            extractProperties: (item: MapTreeItem): MapTreeItem | undefined => {
-                return undefined
-            },
-            rehydrateProperties: (baseItem: MapTreeItem, properties: MapTreeItem[]) => (baseItem)
-        }
-        return mergeTrees(mergeTreeOptions)(
-            extractMapTree({ normalizer: standardizedNormalizer, mapId })
-        )
-        }, [standardizedNormalizer, mapId])
+    const tree = useMemo<GenericTree<MapTreeItem>>(() => (
+        extractMapTree({ normalizer: standardizedNormalizer, mapId })
+    ), [standardizedNormalizer, mapId])
 
     return <MapEditContext.Provider
             value={{
