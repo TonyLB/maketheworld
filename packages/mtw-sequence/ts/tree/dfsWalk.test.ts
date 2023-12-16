2ff3eeb350c6bdbc736456861fc14803e6338bb5
TonyLB <anthony.lowerbasch@gmail.com>
2023-12-16 12:54:33 -0500

    ISS3197: Added aggregate option to dfsWalk

--- packages/mtw-sequence/ts/tree/dfsWalk.test.ts
+++ packages/mtw-sequence/ts/tree/dfsWalk.test.ts
@@ -87,4 +87,28 @@ describe('dfsWalk', () => {
         }]
         expect(verboseTestWalk(incomingTree)).toEqual({ output: ['Test-1', 'Test-1a', 'Test-1b', 'Test-1bi', 'Test-1bii', 'Test-2'], state: 6 })
     })
+
+    it('should apply aggregate function when provided', () => {
+        const verboseTestWalk = dfsWalk<string, string[], number>({
+            default: { output: [], state: 0},
+            callback: ({ state, output }: { output: string[]; state }, value: string) => ({ output: [...output, value], state: state + 1 }),
+            aggregate: ({ direct, children }) => ({ output: [[...direct.output, ...children.output].join(', ')], state: children.state })
+        })
+        const incomingTree: GenericTree<string> = [{
+            data: 'Test-1',
+            children: [
+                { data: 'Test-1a', children: [] },
+                { data: 'Test-1b', children: [
+                    { data: 'Test-1bi', children: [] },
+                    { data: 'Test-1bii', children: [] }
+                ] }
+            ]
+        },
+        {
+            data: 'Test-2',
+            children: []
+        }]
+        expect(verboseTestWalk(incomingTree)).toEqual(['Test-1, Test-1a, Test-1b, Test-1bi, Test-1bii, Test-2'])
+    })
+
 })
