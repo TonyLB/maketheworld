fb2bf334784dca2c25d99218248b1da17aea119f
TonyLB <anthony.lowerbasch@gmail.com>
2024-01-07 19:30:41 -0500

    ISS3330: Added asyncFilter to tree

--- packages/mtw-wml/ts/sequence/tree/filter.ts
+++ packages/mtw-wml/ts/sequence/tree/filter.ts
@@ -6,6 +6,22 @@ export const filter = <NodeData extends {}>({ tree, callback }: { tree: GenericT
         .map(({ data, children }) => ({ data, children: filter({ tree: children, callback })}))
 )
 
+export const asyncFilter = async <NodeData extends {}>({ tree, callback }: { tree: GenericTree<NodeData>, callback: (value: NodeData) => Promise<boolean> }): Promise<GenericTree<NodeData>> => {
+    return (await Promise.all(
+        tree.map(async ({ data, children }) => {
+            if (await callback(data)) {
+                return [{
+                    data,
+                    children: await asyncFilter({ tree: children, callback })
+                }]
+            }
+            else {
+                return []
+            }
+        })
+    )).flat(1)
+}
+
 export const treeTypeGuard = <NodeData extends {}, NewNodeData extends NodeData>({ tree, typeGuard }: { tree: GenericTree<NodeData>, typeGuard: (value: NodeData) => value is NewNodeData }): GenericTree<NewNodeData> => (
     tree.map(({ data, children }) => (
         typeGuard(data)
