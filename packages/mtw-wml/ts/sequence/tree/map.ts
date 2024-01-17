4ab0ad0963434bb36b9a1bad01b4fa3f06bd101b
TonyLB <anthony.lowerbasch@gmail.com>
2024-01-16 19:23:16 -0500

    ISS3357: Refactored type constraints of tree map

--- packages/mtw-wml/ts/sequence/tree/map.ts
+++ packages/mtw-wml/ts/sequence/tree/map.ts
@@ -1,9 +1,14 @@
-import { GenericTree, GenericTreeNode } from "./baseClasses";
+import { GenericTree } from "./baseClasses";
 
-export const map = <T extends {}, O extends {}>(tree: GenericTree<T>, callback: (incoming: { data: T, children: GenericTree<O> }) => GenericTree<O>): GenericTree<O> => {
-    return tree.map(({ data, children }) => (callback({ data, children: map(children, callback) }))).flat(1)
+type TreeMapCallback = (incoming) => typeof incoming extends { data, children } ? typeof incoming["children"] : never
+
+export const map = <Callback extends TreeMapCallback>(tree: GenericTree<Parameters<Callback>[0]["data"], Omit<Parameters<Callback>[0], 'data' | 'children'>>, callback: Callback): ReturnType<Callback> => {
+    return tree.map(({ data, children, ...rest }) => (callback({ data, children: map(children, callback), ...rest }))).flat(1) as ReturnType<Callback>
 }
 
-export const asyncMap = async <T extends {}, O extends {}>(tree: GenericTree<T>, callback: (incoming: { data: T, children: GenericTree<O> }) => Promise<GenericTree<O>>): Promise<GenericTree<O>> => {
-    return (await Promise.all(tree.map(async ({ data, children }) => (await callback({ data, children: await asyncMap(children, callback) }))))).flat(1)
+type TreeMapCallbackAsync = (incoming) => typeof incoming extends { data, children } ? Promise<typeof incoming["children"]> : never
+type CallbackReturnPromise<Callback extends TreeMapCallbackAsync> = ReturnType<Callback> extends Promise<any> ? ReturnType<Callback> : never
+
+export const asyncMap = <Callback extends TreeMapCallbackAsync>(tree: GenericTree<Parameters<Callback>[0]["data"], Omit<Parameters<Callback>[0], 'data' | 'children'>>, callback: Callback): CallbackReturnPromise<Callback> => {
+    return Promise.all(tree.map(async ({ data, children, ...rest }) => (await callback({ data, children: await asyncMap(children, callback), ...rest })))).then((value) => (value.flat(1))) as CallbackReturnPromise<Callback>
 }
