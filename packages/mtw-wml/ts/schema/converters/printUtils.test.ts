337d885e32f777a80e1c48a55c20247a6b7acdf6
TonyLB <anthony.lowerbasch@gmail.com>
2024-02-21 19:33:20 -0500

    ISS3461: Deprecated combineResults helper function

--- packages/mtw-wml/ts/schema/converters/printUtils.test.ts
+++ packages/mtw-wml/ts/schema/converters/printUtils.test.ts
@@ -1,41 +1,11 @@
 import { PrintMode } from "./baseClasses"
-import { isNestedPrint, combineResults, optimalLineResults } from "./printUtils"
+import { isNestedPrint, optimalLineResults } from "./printUtils"
 
 describe('nestingLevel utilities', () => {
     it('should correctly identify a nested tag', () => {
         expect(isNestedPrint('<Description>\n    Test\n</Description>')).toBe(true)
         expect(isNestedPrint('<Description>Test</Description>')).toBe(false)
         expect(isNestedPrint('<Description\n>\n    Test\n</Description>')).toBe(false)
-    })
-
-    it('should properly combine both naive and nested contents without options set', () => {
-        expect(combineResults()(
-            [{ printMode: PrintMode.naive, output: '<Name>Test</Name>' }, { printMode: PrintMode.nested, output: '<Name>\n    Test\n</Name>' }],
-            [{ printMode: PrintMode.naive, output: '<Description>Pretty</Description>'}, { printMode: PrintMode.nested, output: '<Description>\n    Pretty\n</Description>' }]
-        )).toEqual([
-            { printMode: PrintMode.naive, output: '<Name>Test</Name><Description>Pretty</Description>' },
-            { printMode: PrintMode.nested, output: '<Name>\n    Test\n</Name><Description>\n    Pretty\n</Description>'}
-        ])
-    })
-
-    it('should properly combine naive and nested contents with separateLines true', () => {
-        expect(combineResults({ separateLines: true, multipleInCategory: true })(
-            [{ printMode: PrintMode.naive, output: '<Name>Test</Name>' }, { printMode: PrintMode.nested, output: '<Name>\n    Test\n</Name>' }],
-            [{ printMode: PrintMode.naive, output: '<Description>Pretty</Description>'}, { printMode: PrintMode.nested, output: '<Description>\n    Pretty\n</Description>' }]
-        )).toEqual([
-            { printMode: PrintMode.nested, output: '<Name>Test</Name>\n<Description>Pretty</Description>' },
-            { printMode: PrintMode.nested, output: '<Name>\n    Test\n</Name>\n<Description>\n    Pretty\n</Description>'}
-        ])
-    })
-
-    it('should properly ignore whitespace lines in combineResults', () => {
-        expect(combineResults({ separateLines: true, ignoreWhitespace: true })(
-            [{ printMode: PrintMode.naive, output: '<Name>Test</Name>' }, { printMode: PrintMode.nested, output: '<Name>\n    Test\n</Name>' }],
-            [{ printMode: PrintMode.naive, output: ' ' }, { printMode: PrintMode.nested, output: ' ' }],
-            [{ printMode: PrintMode.naive, output: '<Description>Pretty</Description>'}, { printMode: PrintMode.nested, output: '<Description>\n    Pretty\n</Description>' }]
-        )).toEqual([
-            { printMode: PrintMode.nested, output: '<Name>Test</Name>\n<Description>Pretty</Description>'}
-        ])
     })
 
     it('should correctly choose optimalLineResults when all are within limits', () => {
