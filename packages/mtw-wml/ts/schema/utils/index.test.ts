d2300051f68ddbaf41b419f83c22af83608aac72
TonyLB <anthony.lowerbasch@gmail.com>
2024-04-13 14:36:49 -0400

    ISS3738: Added defaultSelected schema utility

--- packages/mtw-wml/ts/schema/utils/index.test.ts
+++ packages/mtw-wml/ts/schema/utils/index.test.ts
@@ -1,4 +1,7 @@
-import { deIndentWML, } from '.'
+import { deIndentWML, defaultSelected, } from '.'
+import { Schema, schemaToWML } from '..'
+import { GenericTree } from '../../tree/baseClasses'
+import { SchemaTag } from '../baseClasses'
 
 describe('deIndentWML', () => {
     it('should leave unindented WML unchanged', () => {
@@ -15,4 +18,63 @@ describe('deIndentWML', () => {
             </Asset>
         `)).toEqual('<Asset key=(Test)>\n    <Room key=(ABC)>\n        <Exit to=(DEF)>Test Exit</Exit>\n    </Room>\n</Asset>')
     })
+})
+
+describe('defaultSelected', () => {
+    const schemaTest = (wml: string): GenericTree<SchemaTag> => {
+        const schema = new Schema()
+        schema.loadWML(wml)
+        return schema.schema
+    }
+    
+    it('should leave WML unchanged when selected exists', () => {
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <If {true}><Exit to=(DEF)>Test Exit</Exit></If>
+                    <ElseIf {false} selected><Exit to=(GHI)>Test Exit</Exit></ElseIf>
+                </Room>
+            </Asset>
+        `)
+        expect(schemaToWML(defaultSelected(schemaTest(testWML)))).toEqual(testWML)
+    })
+
+    it('should add default select on first statement when no fallthrough', () => {
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <If {true}><Exit to=(DEF)>Test Exit</Exit></If>
+                    <ElseIf {false}><Exit to=(GHI)>Test Exit</Exit></ElseIf>
+                </Room>
+            </Asset>
+        `)
+        expect(schemaToWML(defaultSelected(schemaTest(testWML)))).toEqual(deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <If {true} selected><Exit to=(DEF)>Test Exit</Exit></If>
+                    <ElseIf {false}><Exit to=(GHI)>Test Exit</Exit></ElseIf>
+                </Room>
+            </Asset>
+        `))
+    })
+
+    it('should add default select on fallthrough when available', () => {
+        const testWML = deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <If {true}><Exit to=(DEF)>Test Exit</Exit></If>
+                    <Else><Exit to=(GHI)>Test Exit</Exit></Else>
+                </Room>
+            </Asset>
+        `)
+        expect(schemaToWML(defaultSelected(schemaTest(testWML)))).toEqual(deIndentWML(`
+            <Asset key=(Test)>
+                <Room key=(ABC)>
+                    <If {true}><Exit to=(DEF)>Test Exit</Exit></If>
+                    <Else selected><Exit to=(GHI)>Test Exit</Exit></Else>
+                </Room>
+            </Asset>
+        `))
+    })
+
 })
--- packages/mtw-wml/ts/schema/utils/index.ts
+++ packages/mtw-wml/ts/schema/utils/index.ts
@@ -1,11 +1,13 @@
-import { GenericTree, GenericTreeNodeFiltered } from "../../tree/baseClasses"
+import { GenericTree, GenericTreeNodeFiltered, treeNodeTypeguard } from "../../tree/baseClasses"
 import {
     isSchemaCondition,
     isSchemaDescription,
     isSchemaName,
     SchemaTag,
     SchemaTaggedMessageLegalContents,
-    isSchemaTaggedMessageLegalContents
+    isSchemaTaggedMessageLegalContents,
+    isSchemaConditionStatement,
+    isSchemaConditionFallthrough
 } from "../baseClasses"
 
 export const extractNameFromContents = (contents: GenericTree<SchemaTag>): GenericTree<SchemaTag> => {
@@ -68,3 +70,36 @@ export const deIndentWML = (wml: string): string => {
         .map((line) => (line.slice(deIndentAmount)))
         .join('\n')
 }
+
+export const defaultSelected = <Extra extends {}>(tree: GenericTree<SchemaTag, Extra>): GenericTree<SchemaTag, Extra> => (
+    tree.map((node) => {
+        if (treeNodeTypeguard(isSchemaCondition)(node)) {
+            const indexOfFirstSelected = node.children.findIndex(({ data }) => ((isSchemaConditionStatement(data) || isSchemaConditionFallthrough(data)) && (data.selected ?? false) ))
+            if (indexOfFirstSelected !== -1) {
+                return {
+                    ...node,
+                    children: defaultSelected(node.children.map((child, index) => (
+                        treeNodeTypeguard(isSchemaConditionStatement)(child) || treeNodeTypeguard(isSchemaConditionFallthrough)(child)
+                            ? { ...child, data: { ...child.data, selected: index === indexOfFirstSelected ? true : undefined } }
+                            : child
+                    )))
+                }
+            }
+            else {
+                const fallThroughIndex = node.children.findIndex(treeNodeTypeguard(isSchemaConditionFallthrough))
+                return {
+                    ...node,
+                    children: defaultSelected(node.children.map((child, index) => (
+                        treeNodeTypeguard(isSchemaConditionStatement)(child) || treeNodeTypeguard(isSchemaConditionFallthrough)(child)
+                            ? { ...child, data: { ...child.data, selected: index === (fallThroughIndex > -1 ? fallThroughIndex : 0) } }
+                            : child
+                    )))
+                }
+            }
+        }
+        return {
+            ...node,
+            children: defaultSelected(node.children)
+        }
+    })
+)
