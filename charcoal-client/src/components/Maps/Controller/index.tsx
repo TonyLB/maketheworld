66a2ee59d6fcefb64ffc2dd62d0208f3bd8db1bb
TonyLB <anthony.lowerbasch@gmail.com>
2024-04-27 14:08:05 -0400

    ISS3774: Refactored MapController SelectItem to update ParentId

--- charcoal-client/src/components/Maps/Controller/index.tsx
+++ charcoal-client/src/components/Maps/Controller/index.tsx
@@ -32,6 +32,26 @@ const MapContext = React.createContext<MapContextType>({
 })
 export const useMapContext = () => (useContext(MapContext))
 
+//
+// ancestryFromId returns the nodes in the ancestry of a given tree node (identified by searchID), starting from the direct parent and
+// proceeding up the tree.
+//
+const ancestryFromId = (searchID: string) => (tree: GenericTree<SchemaTag, TreeId>): GenericTreeNode<SchemaTag, TreeId>[] | undefined => {
+    return tree.reduce<GenericTreeNode<SchemaTag, TreeId>[] | undefined>((previous, { data, id, children }) => {
+        if (previous) {
+            return previous
+        }
+        if (id === searchID) {
+            return []
+        }
+        const recurse = ancestryFromId(searchID)(children)
+        if (typeof recurse !== 'undefined') {
+            return [...recurse, { data, id, children: [] }]
+        }
+        return previous
+    }, undefined)
+}
+
 export const MapController: FunctionComponent<{ mapId: string }> = ({ children, mapId }) => {
     const { schema, standardForm, updateSchema } = useLibraryAsset()
     const [toolSelected, setToolSelected] = useState<ToolSelected>('Select')
@@ -164,6 +184,16 @@ export const MapController: FunctionComponent<{ mapId: string }> = ({ children, 
                 mapD3.dragExit({ roomId: action.sourceRoomId, x: action.x, y: action.y, double: action.double })
                 return
             case 'SelectItem':
+                if (action.item && action.item.type === 'Layer') {
+                    const ancestry = ancestryFromId(action.item.key)(schema) ?? []
+                    const conditionParent = ancestry.find(treeNodeTypeguard(isSchemaCondition))
+                    if (conditionParent) {
+                        setParentID(conditionParent.id)
+                    }
+                    else {
+                        setParentID(undefined)
+                    }
+                }
                 setItemSelected(action.item)
                 return
             case 'SelectParent':
