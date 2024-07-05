439ed46385a5c2591ade7ce7c922a5111f605ca2
TonyLB <anthony.lowerbasch@gmail.com>
2024-07-05 08:14:04 -0400

    ISS3945: Applied lower-case to default exits in addExit

--- charcoal-client/src/components/Maps/Controller/addExit.ts
+++ charcoal-client/src/components/Maps/Controller/addExit.ts
@@ -1,5 +1,14 @@
+import { GenericTree, TreeId } from "@tonylb/mtw-wml/dist/tree/baseClasses";
 import { UpdateSchemaPayload } from "../../../slices/personalAssets/reducers"
 import { isStandardRoom, StandardForm } from "@tonylb/mtw-wml/dist/standardize/baseClasses";
+import { isSchemaString, SchemaOutputTag } from "@tonylb/mtw-wml/dist/schema/baseClasses";
+
+const schemaOutputLowerCase = (tree: GenericTree<SchemaOutputTag>): GenericTree<SchemaOutputTag> => (
+    tree.map(({ data, children }) => ({
+        data: isSchemaString(data) ? { ...data, value: data.value.toLowerCase() } : data,
+        children: schemaOutputLowerCase(children)
+    }))
+)
 
 export const addExitFactory = ({ standardForm, combinedStandardForm, updateSchema, addImport, parentId }: { standardForm: StandardForm, combinedStandardForm: StandardForm, updateSchema: (action: UpdateSchemaPayload) => void, addImport: (key: string) => void, parentId: string }) => ({ to, from }: { to: string; from: string }) => {
     const destinationComponent = combinedStandardForm.byId[to]
@@ -18,7 +27,10 @@ export const addExitFactory = ({ standardForm, combinedStandardForm, updateSchem
         item: {
             data: { tag: 'Room', key: from },
             children: [
-                { data: { tag: 'Exit', key: `${from}#${to}`, from, to }, children }
+                {
+                    data: { tag: 'Exit', key: `${from}#${to}`, from, to },
+                    children: schemaOutputLowerCase(children)
+                }
             ]
         }
     })
