2bf7a6614ee1f5c78a43e408ae659471066a9a58
TonyLB <anthony.lowerbasch@gmail.com>
2024-07-07 17:47:26 -0400

    ISS3979: Added feedback message to LLMGenerate parse

--- lambda/llm/app.ts
+++ lambda/llm/app.ts
@@ -1,4 +1,8 @@
 import { roomGeneratePrompt } from "./roomGenerate"
+import { snsClient } from './clients'
+import { PublishCommand } from "@aws-sdk/client-sns"
+
+const { FEEDBACK_TOPIC } = process.env
 
 export const handler = async (event) => {
 
@@ -6,6 +10,26 @@ export const handler = async (event) => {
         case 'RoomGenerate':
             const prompt = await roomGeneratePrompt(event.name)
             return { prompt }
+        case 'ParseRoomGenerate':
+            const body = event.results.Body
+            if (body) {
+                if ('results' in body) {
+                    await snsClient.send(new PublishCommand({
+                        TopicArn: FEEDBACK_TOPIC,
+                        Message: JSON.stringify({
+                            messageType: 'LLMGenerate',
+                            description: body.results[0].outputText
+                        }),
+                        MessageAttributes: {
+                            RequestId: { DataType: 'String', StringValue: event.RequestId },
+                            ConnectionIds: { DataType: 'String.Array', StringValue: JSON.stringify([event.ConnectionId]) },
+                            Type: { DataType: 'String', StringValue: 'Success' }
+                        }
+                    }))
+
+                    return {}
+                }
+            }
     }
     throw new Error('Invalid input type')
 }
