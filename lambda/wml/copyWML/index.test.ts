f0eaf5d4d9bfbcc560edbcfde12184e7fba4de07
TonyLB <anthony.lowerbasch@gmail.com>
2024-07-14 17:11:08 -0400

    ISS4016: Extended copyWML to handle JSON file

--- lambda/wml/copyWML/index.test.ts
+++ lambda/wml/copyWML/index.test.ts
@@ -1,8 +1,11 @@
 jest.mock('@tonylb/mtw-asset-workspace/ts/clients')
 import { s3Client } from '@tonylb/mtw-asset-workspace/ts/clients'
+
 import { deIndentWML } from '@tonylb/mtw-wml/ts/schema/utils'
 
 import copyWML from '.'
+import { NormalForm } from '@tonylb/mtw-wml/ts/normalize/baseClasses'
+import { SerializableStandardForm } from '@tonylb/mtw-wml/ts/standardize/baseClasses'
 
 const s3ClientMock = s3Client as jest.Mocked<typeof s3Client>
 
@@ -15,11 +18,38 @@ describe('copyWML', () => {
     })
 
     it('should replace asset key', async () => {
-        s3ClientMock.get.mockResolvedValue(deIndentWML(`
+        s3ClientMock.get.mockResolvedValueOnce(deIndentWML(`
             <Asset key=(draft)>
-                <Room key=(VORTEX)><Name>Test Name</Name></Room>
+                <Room key=(TestRoom)><Name>Test Name</Name></Room>
             </Asset>
         `))
+        const testJSON: { assetId: string; namespaceIdToDB: any[], normal: NormalForm; standard: SerializableStandardForm; properties: any } = {
+            assetId: "ASSET#draft[Test]",
+            namespaceIdToDB: [{ internalKey: 'TestRoom', universalKey: 'ROOM#UUID' }],
+            normal: {
+                testCopy: { tag: 'Asset', key: 'draft[Test]', appearances: [] },
+                TestRoom: { tag: 'Room', key: 'TestRoom', appearances: [] }
+            },
+            standard: {
+                key: 'testCopy',
+                tag: 'Asset',
+                byId: {
+                    TestRoom: {
+                        tag: 'Room',
+                        key: 'TestRoom',
+                        name: { data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Test Name'}, children: [] }] },
+                        shortName: { data: { tag: 'ShortName' }, children: [] },
+                        summary: { data: { tag: 'Summary' }, children: [] },
+                        description: { data: { tag: 'Description' }, children: [] },
+                        exits: [],
+                        themes: []
+                    }
+                },
+                metaData: []
+            },
+            properties: {}
+        }
+        s3ClientMock.get.mockResolvedValueOnce(JSON.stringify(testJSON))
 
         await copyWML({
             key: 'testCopy',
@@ -34,10 +64,23 @@ describe('copyWML', () => {
                 subFolder: 'Assets'
             }
         })
-        expect(s3Client.put).toHaveBeenCalledWith({
+        
+        expect(s3ClientMock.put).toHaveBeenCalledWith({
             Key: 'Personal/Test/Assets/testCopy.wml',
-            Body: `<Asset key=(testCopy)><Room key=(VORTEX)><Name>Test Name</Name></Room></Asset>`
+            Body: deIndentWML(`
+                <Asset key=(testCopy)>
+                    <Room key=(TestRoom)><Name>Test Name</Name></Room>
+                </Asset>
+            `)
         })
+        expect(s3ClientMock.put.mock.calls[1][0].Key).toEqual('Personal/Test/Assets/testCopy.json')
+        expect(JSON.parse(s3ClientMock.put.mock.calls[1][0].Body)).toEqual({
+            ...testJSON,
+            assetId: 'ASSET#testCopy',
+            normal: expect.any(Object)
+        })
+            // `{"assetId":"ASSET#Test","namespaceIdToDB":[],"normal":{"Test":{"tag":"Asset","key":"Test","fileName":"Test","appearances":[]}},"standard":{"key":"Test","tag":"Asset","byId":{},"metaData":[]},"properties":{}}`
+        
 
     })
 
--- lambda/wml/copyWML/index.ts
+++ lambda/wml/copyWML/index.ts
@@ -9,11 +9,20 @@ type CopyWMLArguments = {
     to: AssetWorkspaceAddress;
 }
 
+//
+// copyWML takes the WML and JSON files from a certain address, renames the asset, and writes
+// the updated files to a second address
+//
 export const copyWML = async (args: CopyWMLArguments) => {
+    //
+    // Fetch all information you will need in order to change the internal structure of the file
+    //
     const fromWorkspace = new AssetWorkspace(args.from)
     await fromWorkspace.loadWML()
+    await fromWorkspace.loadJSON()
+
     //
-    // TODO: Load the JSON to find meta-data like image files
+    // Load and parse the WML into schema format
     //
     const { wml } = fromWorkspace
     if (!wml) {
@@ -21,17 +30,24 @@ export const copyWML = async (args: CopyWMLArguments) => {
     }
     const schema = new Schema()
     schema.loadWML(wml)
+
+    //
+    // Update the key of the outermost element of the schema
+    //
     const schemaRaw = schema.schema[0]
     if (!(treeNodeTypeguard(isSchemaAsset)(schemaRaw) || treeNodeTypeguard(isSchemaCharacter)(schemaRaw))) {
         throw new Error('Invalid WML source in copyWML')
     }
-    const toWorkspace = new AssetWorkspace(args.to)
     schema._schema = [{ ...schema.schema[0], data: { ...schemaRaw.data, key: args.key } }]
-    await toWorkspace.setWML(schemaToWML(schema.schema))
-    await toWorkspace.pushWML()
+
     //
-    // TODO: PushWML and JSON to the new address
+    // Reload the new schema and new address over the existing data, and push to S3
     //
+    await fromWorkspace.setWML(schemaToWML(schema.schema))
+    fromWorkspace.changeAddress(args.to)
+    await fromWorkspace.pushWML()
+    await fromWorkspace.pushJSON()
+
 }
 
 export default copyWML
