85bfe5989ca78994bdaf924093074c26007b5246
TonyLB <anthony.lowerbasch@gmail.com>
2024-07-22 08:43:14 -0400

    ISS4077: Added inviteCode validation and first-time login to signUp lambda outlet

--- lambda/connections/createUser/index.ts
+++ lambda/connections/createUser/index.ts
@@ -1,5 +1,6 @@
-import { AdminCreateUserCommand, AdminSetUserPasswordCommand } from "@aws-sdk/client-cognito-identity-provider";
+import { AdminCreateUserCommand, AdminSetUserPasswordCommand, InitiateAuthCommand } from "@aws-sdk/client-cognito-identity-provider";
 import { cognitoClient } from "../clients";
+import { connectionDB } from "@tonylb/mtw-utilities/ts/dynamoDB";
 
 type CreateUserRequest = {
     inviteCode: string;
@@ -8,15 +9,50 @@ type CreateUserRequest = {
 }
 
 type CreateUserResultSuccess = {
-
+    AccessToken?: string;
+    IdToken: string;
+    RefreshToken: string;
 }
 
-export const createCognitoUser = async ({ inviteCode, userName, password }: CreateUserRequest) => {
+type CreateUserResultsFailureErrorField = 'inviteCode' | 'userName' | 'password' | 'system'
+type CreateUserResultsFailure = {
+    errorField: CreateUserResultsFailureErrorField;
+    errorMessage: string;
+}
+
+type CreateUserResult = CreateUserResultSuccess | CreateUserResultsFailure
+
+export const createCognitoUser = async ({ inviteCode, userName, password }: CreateUserRequest): Promise<CreateUserResult> => {
+    let errorField: CreateUserResultsFailureErrorField | undefined
+    let errorMessage = ''
+    let inviteCodeClaimed = false
     try {
         //
-        // TODO: Validate inviteCode
+        // Validate and claim inviteCode
         //
-
+        await connectionDB.optimisticUpdate({
+            Key: {
+                ConnectionId: `INVITATION#${inviteCode}`,
+                DataCategory: 'Meta::Invitation'
+            },
+            updateKeys: ['claimedBy'],
+            updateReducer: (draft) => {
+                if (!draft.DataCategory) {
+                    errorField = 'inviteCode'
+                    errorMessage = 'Invalid invitation'
+                    throw new Error('Invalid invitation')
+                }
+                if (draft.claimedBy) {
+                    errorField = 'inviteCode'
+                    errorMessage = 'Invitation already claimed'
+                    throw new Error('Invitation already claimed')
+                }
+                draft.claimedBy = userName
+            },
+            successCallback: () => {
+                inviteCodeClaimed = true
+            }
+        })
         //
         // Create the user without a temporary password
         //
@@ -42,7 +78,37 @@ export const createCognitoUser = async ({ inviteCode, userName, password }: Crea
     }
     catch (err) {
         //
-        // TODO: Process errors
+        // Process errors
         //
+        throw err
+    }
+    //
+    // Now sign in with the new username and password, in order to generate tokens to return in exchange for
+    // the signUp request
+    //
+    try {
+        const results = await cognitoClient.send(new InitiateAuthCommand({
+            AuthFlow: 'USER_PASSWORD_AUTH',
+            AuthParameters: {
+                USERNAME: userName,
+                PASSWORD: password
+            },
+            ClientId: process.env.COGNITO_USER_POOL_CLIENT
+        }))
+        const { AuthenticationResult } = results
+        if (!(AuthenticationResult?.IdToken && AuthenticationResult.RefreshToken)) {
+            throw new Error('Tokens not returned from signin')
+        }
+        return {
+            AccessToken: AuthenticationResult?.AccessToken,
+            IdToken: AuthenticationResult?.IdToken ?? '',
+            RefreshToken: AuthenticationResult?.RefreshToken ?? ''
+        }
+    }
+    catch (error: any) {
+        return {
+            errorField: 'system',
+            errorMessage: 'Internal Error'
+        }
     }
 }
