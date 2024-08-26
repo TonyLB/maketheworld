f4dc49946822bbb18cb614375ca335e3fa2cb82d
TonyLB <anthony.lowerbasch@gmail.com>
2024-08-26 08:25:42 -0400

    ISS4303: Added EditSubListSchema

--- charcoal-client/src/components/Library/Edit/EditContext.tsx
+++ charcoal-client/src/components/Library/Edit/EditContext.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useContext } from "react"
 import { GenericTree, GenericTreeNode } from "@tonylb/mtw-wml/dist/tree/baseClasses";
 import { SchemaTag } from "@tonylb/mtw-wml/dist/schema/baseClasses";
 import { TreeId } from "@tonylb/mtw-wml/dist/tree/baseClasses";
+import { maybeGenericIDFromTree } from "@tonylb/mtw-wml/dist/tree/genericIDTree";
 
 type EditContextType = {
     field: GenericTreeNode<SchemaTag, TreeId>;
@@ -23,6 +24,45 @@ export const EditSchema: FunctionComponent<EditContextType> = ({ field, inherite
     return <EditContext.Provider value={{ field, inherited, subIndex, value, onChange, onDelete }}>
         { children }
     </EditContext.Provider>
+}
+
+type EditSubListArguments = {
+    index: number;
+}
+export const EditSubListSchema: FunctionComponent<EditSubListArguments> = ({ index, children }) => {
+    const { value, onChange } = useEditContext()
+    if (value.length === 0) {
+        return null
+    }
+    const nodeChildren = value[0].children
+    if (index >= nodeChildren.length) {
+        return null
+    }
+    return <EditSchema
+        field={maybeGenericIDFromTree([nodeChildren[index]])[0]}
+        value={[nodeChildren[index]]}
+        onChange={(newValue) => {
+            onChange(maybeGenericIDFromTree([{
+                ...value[0],
+                children: [
+                    ...nodeChildren.slice(0, index),
+                    ...newValue,
+                    ...nodeChildren.slice(index + 1)
+                ]
+            }]))
+        }}
+        onDelete={() => {
+            onChange(maybeGenericIDFromTree([{
+                ...value[0],
+                children: [
+                    ...nodeChildren.slice(0, index),
+                    ...nodeChildren.slice(index + 1)
+                ]
+            }]))
+        }}
+    >
+        { children }
+    </EditSchema>
 }
 
 export const useEditContext = () => (useContext(EditContext))
