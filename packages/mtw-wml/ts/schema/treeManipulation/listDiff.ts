879d36d2ad5c8864cd902e9ab84a4a83b020e13c
TonyLB <anthony.lowerbasch@gmail.com>
2024-09-17 08:26:21 -0400

    ISS4410: Refactored listDiff to handle added items

--- packages/mtw-wml/ts/schema/treeManipulation/listDiff.ts
+++ packages/mtw-wml/ts/schema/treeManipulation/listDiff.ts
@@ -1,6 +1,42 @@
-import { GenericTree } from "../../tree/baseClasses";
-import { SchemaTag } from "../baseClasses";
+import { diffTrees } from '../../tree/diff'
+import { GenericTree, GenericTreeDiffAction } from "../../tree/baseClasses"
+import { isSchemaCondition, isSchemaConditionStatement, isSchemaImport, isSchemaWithKey, SchemaTag } from "../baseClasses"
+import { deepEqual } from '../../lib/objects'
 
 export const listDiff = (a: GenericTree<SchemaTag>, b: GenericTree<SchemaTag>): GenericTree<SchemaTag> => {
-    return []
+    const diff = diffTrees<SchemaTag, SchemaTag>({
+        compare: (a, b) => {
+            if (isSchemaWithKey(a)) {
+                return (isSchemaWithKey(b) && a.key === b.key)
+            }
+            if (isSchemaConditionStatement(a) && isSchemaConditionStatement(b)) {
+                return a.if === b.if
+            }
+            if (isSchemaCondition(a) && isSchemaCondition(b)) {
+                return a.wrapperKey === b.wrapperKey
+            }
+            if (isSchemaImport(a) && isSchemaImport(b)) {
+                return a.from === b.from
+            }
+            return deepEqual(a, b)
+        },
+        extractProperties: (value) => (value),
+        rehydrateProperties: (base, properties) => ({ ...base, ...properties })
+    })(a, b)
+    let aIndex = 0
+    let bIndex = 0
+    let returnValue: GenericTree<SchemaTag> = []
+    diff.forEach((diffNode) => {
+        switch(diffNode.action) {
+            case GenericTreeDiffAction.Add:
+                returnValue = [...returnValue, b[bIndex]]
+                bIndex++
+                break
+            // case GenericTreeDiffAction.Exclude:
+            default:
+                aIndex++
+                bIndex++
+        }
+    })
+    return returnValue
 }
