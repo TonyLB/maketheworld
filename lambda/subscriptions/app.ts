cf5326986bb27391e767e8ef36f70c67c0b9dcdd
TonyLB <anthony.lowerbasch@gmail.com>
2024-10-02 08:55:28 -0400

    ISS4439: Extended subscriptionLibrary framework with object-oriented calling pattersn

--- lambda/subscriptions/app.ts
+++ lambda/subscriptions/app.ts
@@ -1,9 +1,33 @@
 // Copyright 2024 Tony Lower-Basch. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+import { subscriptionLibrary } from "./handlerFramework"
+
 export const handler = async (event: any) => {
 
-    console.log(`event: ${JSON.stringify(event, null, 4)}`)
+    //
+    // Handle Websocket calls to update the subscriber lists
+    //
+    if (event?.requestContext?.routeKey === 'subscriptions') {
+        const request = JSON.parse(event.body ?? "{}")
+        //
+        // TODO: Create typeguard in mtw-interfaces and apply here
+        //
+        const match = subscriptionLibrary.match(request)
+        if (match) {
+            console.log(`Matched [${match._source}]: ${JSON.stringify(request, null, 4)}`)
+        }
+        else {
+            console.log(`No match: ${JSON.stringify(request, null, 4)}`)
+        }
+    
+    }
+    //
+    // Handle EventBridge events that may need to be forwarded to subscribers
+    //
+    if (event?.source) {
+
+    }
     return {
         statusCode: 200,
         body: "{}"
--- lambda/subscriptions/handlerFramework/index.ts
+++ lambda/subscriptions/handlerFramework/index.ts
@@ -1,11 +1,13 @@
-import { SubscriptionHandler } from "./baseClasses";
+import { SubscriptionHandler, SubscriptionLibrary } from "./baseClasses";
 
 type LibraryEntry = {
     source: string;
 }
 
-const subscriptionLibraryConstructor = (entries: LibraryEntry[]): SubscriptionHandler[] => {
-    return entries.map((entry) => (new SubscriptionHandler(entry)))
+const subscriptionLibraryConstructor = (entries: LibraryEntry[]): SubscriptionLibrary => {
+    return new SubscriptionLibrary({
+        library: entries.map((entry) => (new SubscriptionHandler(entry)))
+    })
 }
 
 export const subscriptionLibrary = subscriptionLibraryConstructor([
