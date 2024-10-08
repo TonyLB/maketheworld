e37502cd5b6908af261459761029e4adc964a945
TonyLB <anthony.lowerbasch@gmail.com>
2024-10-07 20:15:10 -0400

    ISS4395: Refactored applyEdit to wait for atomicLock

--- lambda/wml/applyEdit/index.ts
+++ lambda/wml/applyEdit/index.ts
@@ -1,8 +1,36 @@
+import { Schema } from "@tonylb/mtw-wml/ts/schema";
+import { Standardizer } from "@tonylb/mtw-wml/ts/standardize";
+import assetAtomicLock from "../atomicLock";
+
 export type ApplyEditArguments = {
     schema: string;
 }
 
 export const applyEdit = async (args: ApplyEditArguments): Promise<Record<string, any>> => {
+    //
+    // Create an editStandardizer to be merged with ndjson. Complete
+    // this step before requesting a lock on the files.
+    //
+    const editSchema = new Schema()
+    editSchema.loadWML(args.schema)
+    const editStandardizer = new Standardizer(editSchema.schema)
+    
+    //
+    // Await atomicLock for asset
+    //
+    const lock = await assetAtomicLock(`ASSET#${editStandardizer._assetKey}`)
+
+    //
+    // TODO: Write ndjson
+    //
+
+    //
+    // TODO: Write wml
+    //
+
+    //
+    // TODO: Yield atomicLock for asset
+    //
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Success' })
--- lambda/wml/atomicLock/index.test.ts
+++ lambda/wml/atomicLock/index.test.ts
@@ -1,3 +1,6 @@
+jest.mock('uuid', () => ({ v4: jest.fn() }))
+import { v4 as uuidv4 } from 'uuid'
+
 jest.mock('./mockableTime', () => (jest.fn()))
 import now from './mockableTime'
 
@@ -12,6 +15,7 @@ import delayPromise from '@tonylb/mtw-utilities/ts/dynamoDB/delayPromise'
 import atomicLock from '.'
 import { produce } from 'immer'
 
+const uuidv4Mock = uuidv4 as jest.Mock
 const assetDBMock = assetDB as jest.Mocked<typeof assetDB>
 const nowMock = now as jest.Mock
 const delayPromiseMock = delayPromise as jest.Mock
@@ -24,6 +28,7 @@ describe('atomicLock', () => {
     })
 
     it('should grant a lock immediately when there is no conflict', async () => {
+        uuidv4Mock.mockReturnValue('ABCDEF')
         nowMock.mockReturnValue(1000)
         assetDBMock.optimisticUpdate.mockResolvedValue({
             AssetId: 'ASSET#ABC',
@@ -35,7 +40,7 @@ describe('atomicLock', () => {
             DataCategory: 'Meta::Asset',
             atomicLocks: ['ABCDEF']
         })
-        await atomicLock('ASSET#ABC', 'ABCDEF')
+        await atomicLock('ASSET#ABC')
         expect(assetDBMock.optimisticUpdate.mock.calls[0][0]).toEqual({
             Key: { AssetId: 'ASSET#ABC', DataCategory: 'Meta::Asset' },
             updateKeys: ['atomicLocks'],
@@ -51,6 +56,7 @@ describe('atomicLock', () => {
     })
 
     it('should wait to grant a lock when there is a conflict', async () => {
+        uuidv4Mock.mockReturnValue('2')
         nowMock.mockReturnValueOnce(1000).mockReturnValue(3000)
         assetDBMock.optimisticUpdate.mockResolvedValueOnce({
             AssetId: 'ASSET#ABC',
@@ -79,7 +85,7 @@ describe('atomicLock', () => {
             atomicLocks: ['2'],
             timeToUnlock: 3000
         })
-        await atomicLock('ASSET#ABC', '2')
+        await atomicLock('ASSET#ABC')
         expect(delayPromiseMock).toHaveBeenCalled()
         expect(assetDBMock.optimisticUpdate.mock.calls[0][0]).toEqual({
             Key: { AssetId: 'ASSET#ABC', DataCategory: 'Meta::Asset' },
@@ -96,6 +102,7 @@ describe('atomicLock', () => {
     })
 
     it('should force unlock when timeout expires', async () => {
+        uuidv4Mock.mockReturnValue('2')
         nowMock.mockReturnValueOnce(1000).mockReturnValue(3000)
         assetDBMock.optimisticUpdate.mockResolvedValueOnce({
             AssetId: 'ASSET#ABC',
@@ -119,7 +126,7 @@ describe('atomicLock', () => {
             atomicLocks: ['1', '2'],
             timeToUnlock: 2000
         })
-        await atomicLock('ASSET#ABC', '2')
+        await atomicLock('ASSET#ABC')
         expect(delayPromiseMock).toHaveBeenCalled()
         expect(assetDBMock.optimisticUpdate.mock.calls[0][0]).toEqual({
             Key: { AssetId: 'ASSET#ABC', DataCategory: 'Meta::Asset' },
