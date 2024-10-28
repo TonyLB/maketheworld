59916f20e0a23e4bbafae46bfba4a7d348cefadd
TonyLB <anthony.lowerbasch@gmail.com>
2024-10-27 20:11:42 -0400

    ISS4551: Added StandardKnowledge class

---
+++ packages/mtw-wml/ts/standardize/components/knowledge.test.ts
@@ -0,0 +1,66 @@
+import { Schema, schemaToWML } from "../../schema"
+import { deIndentWML } from "../../schema/utils"
+import { StandardKnowledgeData } from "./dataTypes/knowledge"
+import { StandardKnowledge } from './knowledge'
+
+describe('StandardKnowledge class', () => {
+    it('should construct StandardKnowledge from schema', () => {
+        const schema = new Schema()
+        const testSource = deIndentWML(`
+            <Knowledge key=(test)>
+                <Name>Name Test</Name>
+                <Description>Description Test</Description>
+            </Knowledge>
+        `)
+        schema.loadWML(testSource)
+        const testKnowledge = new StandardKnowledge(schema.schema[0])
+        expect(testKnowledge.key).toEqual('test')
+        expect(testKnowledge.name).toEqual({ data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Name Test' }, children: [] }] })
+        expect(testKnowledge.description).toEqual({ data: { tag: 'Description' }, children: [{ data: { tag: 'String', value: 'Description Test' }, children: [] }] })
+        expect(schemaToWML([testKnowledge.schema])).toEqual(testSource)
+    })
+
+    it('should construct StandardKnowledge from StandardKnowledgeData', () => {
+        const testKnowledgeData: StandardKnowledgeData = {
+            key: 'test',
+            tag: 'Knowledge',
+            name: { data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Name Test' }, children: [] }] },
+            description: { data: { tag: 'Description' }, children: [{ data: { tag: 'String', value: 'Description Test' }, children: [] }] },
+        }
+        const testKnowledge = new StandardKnowledge(testKnowledgeData)
+        expect(testKnowledge.key).toEqual('test')
+        expect(testKnowledge.name).toEqual({ data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Name Test' }, children: [] }] })
+        expect(testKnowledge.description).toEqual({ data: { tag: 'Description' }, children: [{ data: { tag: 'String', value: 'Description Test' }, children: [] }] })
+        expect(testKnowledge.toJSON()).toEqual(testKnowledgeData)
+    })
+
+    it('should merge correctly', () => {
+        const baseSource = deIndentWML(`
+            <Knowledge key=(testKnowledge)>
+                <Name>Lobby</Name>
+                <Description>A plain lobby.</Description>
+            </Knowledge>
+        `)
+        const baseSchema = new Schema()
+        baseSchema.loadWML(baseSource)
+        const baseStandard = new StandardKnowledge(baseSchema.schema[0])
+        const testSource = deIndentWML(`
+            <Knowledge key=(testKnowledge)>
+                <Replace><Name>Lobby</Name></Replace><With><Name>Spooky Lobby</Name></With>
+                <Description><Space />Shadows cling to the corners of the room.</Description>
+            </Knowledge>
+        `)
+        const testSchema = new Schema()
+        testSchema.loadWML(testSource)
+        const testStandard = new StandardKnowledge(testSchema.schema[0])
+        const mergedStandard = baseStandard.merge(testStandard)
+        expect(schemaToWML([mergedStandard.schema])).toEqual(deIndentWML(`
+            <Knowledge key=(testKnowledge)>
+                <Name>Spooky Lobby</Name>
+                <Description>
+                    A plain lobby.<Space />Shadows cling to the corners of the room.
+                </Description>
+            </Knowledge>
+        `))
+    })
+})
