8d73c1b603432a8cc39e215073f3eb1ec7e10bc6
TonyLB <anthony.lowerbasch@gmail.com>
2024-10-29 20:29:09 -0400

    ISS4559: Added StandardAction class

---
+++ packages/mtw-wml/ts/standardize/components/action.test.ts
@@ -0,0 +1,38 @@
+import { Schema, schemaToWML } from "../../schema"
+import { deIndentWML } from "../../schema/utils"
+import { StandardActionData } from "./dataTypes/action"
+import { StandardAction } from './action'
+import { mergeTest } from './utils/testing'
+
+describe('StandardAction class', () => {
+    it('should construct StandardAction from schema', () => {
+        const schema = new Schema()
+        const testSource = deIndentWML(`
+            <Action key=(test) src={testVar = false} />
+        `)
+        schema.loadWML(testSource)
+        const testAction = new StandardAction(schema.schema[0])
+        expect(testAction.key).toEqual('test')
+        expect(testAction.src).toEqual('testVar = false')
+        expect(schemaToWML([testAction.schema])).toEqual(testSource)
+    })
+
+    it('should construct StandardComputed from StandardComputedData', () => {
+        const testComputedData: StandardActionData = {
+            key: 'test',
+            tag: 'Action',
+            src: 'testVar = !testVar'
+        }
+        const testComputed = new StandardAction(testComputedData)
+        expect(testComputed.src).toEqual('testVar = !testVar')
+        expect(testComputed.toJSON()).toEqual(testComputedData)
+    })
+
+    it('should merge correctly', () => {
+        expect(mergeTest(
+            '<Action key=(test) src={testVar = true} />',
+            StandardAction,
+            '<Action key=(test) src={testVar = false} />'
+        )).toEqual(deIndentWML('<Action key=(test) src={testVar = false} />'))
+    })
+})
