b38a242dd6731525836851febf40031d5a53fba8
TonyLB <anthony.lowerbasch@gmail.com>
2024-10-30 20:11:06 -0400

    ISS4571: Added StandardCharacter class

---
+++ packages/mtw-wml/ts/standardize/components/character.test.ts
@@ -0,0 +1,101 @@
+import { Schema, schemaToWML } from "../../schema"
+import { deIndentWML } from "../../schema/utils"
+import { StandardCharacterData } from "./dataTypes/character"
+import { StandardCharacter } from './character'
+import { mergeTest } from "./utils/testing"
+
+describe('StandardCharacter class', () => {
+    it('should construct StandardCharacter from schema', () => {
+        const schema = new Schema()
+        const testSource = deIndentWML(`
+            <Character key=(test)>
+                <Name>Tess</Name>
+                <Pronouns
+                    subject="she"
+                    object="her"
+                    possessive="her"
+                    adjective="hers"
+                    reflexive="herself"
+                />
+                <FirstImpression>Ragged waif</FirstImpression>
+                <OneCoolThing>Silvery eyes</OneCoolThing>
+                <Outfit>Rags</Outfit>
+            </Character>
+        `)
+        schema.loadWML(testSource)
+        const testRoom = new StandardCharacter(schema.schema[0])
+        expect(testRoom.key).toEqual('test')
+        expect(testRoom.pronouns).toEqual({ data: { tag: 'Pronouns', subject: 'she', object: 'her', possessive: 'her', adjective: 'hers', reflexive: 'herself' }, children: [] })
+        expect(testRoom.name).toEqual({ data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Tess' }, children: [] }] })
+        expect(testRoom.firstImpression).toEqual({ data: { tag: 'FirstImpression', value: 'Ragged waif' }, children: [] })
+        expect(testRoom.oneCoolThing).toEqual({ data: { tag: 'OneCoolThing', value: 'Silvery eyes' }, children: [] })
+        expect(testRoom.outfit).toEqual({ data: { tag: 'Outfit', value:  'Rags' }, children: [] })
+        expect(schemaToWML([testRoom.schema])).toEqual(testSource)
+    })
+
+    it('should construct StandardCharacter from StandardCharacterData', () => {
+        const testRoomData: StandardCharacterData = {
+            key: 'test',
+            tag: 'Character',
+            pronouns: { data: { tag: 'Pronouns', subject: 'she', object: 'her', possessive: 'her', adjective: 'hers', reflexive: 'herself' }, children: [] },
+            name: { data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Tess' }, children: [] }] },
+            firstImpression: { data: { tag: 'FirstImpression', value: 'Ragged waif' }, children: [] },
+            oneCoolThing: { data: { tag: 'OneCoolThing', value: 'Silvery eyes' }, children: [] },
+            outfit: { data: { tag: 'Outfit', value:  'Rags' }, children: [] }
+        }
+        const testRoom = new StandardCharacter(testRoomData)
+        expect(testRoom.key).toEqual('test')
+        expect(testRoom.pronouns).toEqual({ data: { tag: 'Pronouns', subject: 'she', object: 'her', possessive: 'her', adjective: 'hers', reflexive: 'herself' }, children: [] })
+        expect(testRoom.name).toEqual({ data: { tag: 'Name' }, children: [{ data: { tag: 'String', value: 'Tess' }, children: [] }] })
+        expect(testRoom.firstImpression).toEqual({ data: { tag: 'FirstImpression', value: 'Ragged waif' }, children: [] })
+        expect(testRoom.oneCoolThing).toEqual({ data: { tag: 'OneCoolThing', value: 'Silvery eyes' }, children: [] })
+        expect(testRoom.outfit).toEqual({ data: { tag: 'Outfit', value:  'Rags' }, children: [] })
+        expect(testRoom.toJSON()).toEqual(testRoomData)
+    })
+
+    it('should merge correctly', () => {
+        expect(mergeTest(
+            `<Character key=(test)>
+                <Name>Tess</Name>
+                <Pronouns
+                    subject="she"
+                    object="her"
+                    possessive="her"
+                    adjective="hers"
+                    reflexive="herself"
+                />
+                <FirstImpression>Ragged waif</FirstImpression>
+                <OneCoolThing>Silvery eyes</OneCoolThing>
+                <Outfit>Rags</Outfit>
+            </Character>`,
+            StandardCharacter,
+            `<Character key=(test)>
+                <Name>Tess</Name>
+                <Pronouns
+                    subject="they"
+                    object="them"
+                    possessive="their"
+                    adjective="theirs"
+                    reflexive="themself"
+                />
+                <FirstImpression>Ragged waif</FirstImpression>
+                <OneCoolThing>Silvery eyes</OneCoolThing>
+                <Outfit>Rags</Outfit>
+            </Character>`
+        )).toEqual(deIndentWML(`
+            <Character key=(test)>
+                <Name>Tess</Name>
+                <Pronouns
+                    subject="they"
+                    object="them"
+                    possessive="their"
+                    adjective="theirs"
+                    reflexive="themself"
+                />
+                <FirstImpression>Ragged waif</FirstImpression>
+                <OneCoolThing>Silvery eyes</OneCoolThing>
+                <Outfit>Rags</Outfit>
+            </Character>
+        `))
+    })
+})
---
+++ packages/mtw-wml/ts/standardize/components/character.ts
@@ -0,0 +1,104 @@
+import { excludeUndefined } from "../../lib/lists"
+import { isSchemaCharacter, isSchemaFirstImpression, isSchemaImage, isSchemaName, isSchemaOneCoolThing, isSchemaOutfit, isSchemaOutputTag, isSchemaPronouns, isSchemaVariable, SchemaFirstImpressionTag, SchemaImageTag, SchemaNameTag, SchemaOneCoolThingTag, SchemaOutfitTag, SchemaOutputTag, SchemaPronounsTag, SchemaTag } from "../../schema/baseClasses"
+import { GenericTreeNode, GenericTreeNodeFiltered, treeNodeTypeguard } from "../../tree/baseClasses"
+import { treeTypeGuard } from "../../tree/filter"
+import { EditWrappedStandardNode } from "../baseClasses"
+import StandardComponentAbstract from "./abstract"
+import { StandardCharacterData } from "./dataTypes/character"
+import { isSchemaTreeNode } from "./utils"
+
+export class StandardCharacter extends StandardComponentAbstract {
+    _name?: EditWrappedStandardNode<SchemaNameTag, SchemaOutputTag>;
+    _firstImpression?: EditWrappedStandardNode<SchemaFirstImpressionTag, SchemaTag>;
+    _oneCoolThing?: EditWrappedStandardNode<SchemaOneCoolThingTag, SchemaTag>;
+    _outfit?: EditWrappedStandardNode<SchemaOutfitTag, SchemaTag>;
+    _pronouns?: EditWrappedStandardNode<SchemaPronounsTag, SchemaTag>;
+    _image?: EditWrappedStandardNode<SchemaImageTag, SchemaTag>;
+    tag = 'Character' as const
+    constructor(args: StandardCharacterData | GenericTreeNode<SchemaTag>) {
+        super(args)
+        if (isSchemaTreeNode(args)) {
+            const { data } = args
+            if (!isSchemaCharacter(data)) {
+                throw new Error('Type mismatch in StandardCharacter constructor')
+            }
+            this._pronouns = (args.children.find(treeNodeTypeguard(isSchemaPronouns)) ?? { children: [], data: { tag: 'Pronouns', subject: 'they', object: 'them', possessive: 'theirs', adjective: 'their', reflexive: 'themself' } })
+            const confirmOutputChildren = <InputNode extends SchemaTag>(node: GenericTreeNodeFiltered<InputNode, SchemaTag> |  undefined): GenericTreeNodeFiltered<InputNode, SchemaOutputTag> | undefined => (node ? { data: node.data, children: treeTypeGuard({ tree: node.children, typeGuard: isSchemaOutputTag })} : undefined)
+            this._name = confirmOutputChildren(args.children.find(treeNodeTypeguard(isSchemaName)))
+            this._firstImpression = args.children.find(treeNodeTypeguard(isSchemaFirstImpression))
+            this._oneCoolThing = args.children.find(treeNodeTypeguard(isSchemaOneCoolThing))
+            this._outfit = args.children.find(treeNodeTypeguard(isSchemaOutfit))
+            this._image = args.children.find(treeNodeTypeguard(isSchemaImage))
+        }
+        else {
+            this._pronouns = args.pronouns
+            this._name = args.name
+            this._firstImpression = args.firstImpression
+            this._oneCoolThing = args.oneCoolThing
+            this._outfit = args.outfit
+            this._image = args.image
+        }
+    }
+
+    get pronouns() { return this._pronouns }
+    get name() { return this._name }
+    get firstImpression() { return this._firstImpression }
+    get oneCoolThing() { return this._oneCoolThing }
+    get outfit() { return this._outfit }
+    get image() { return this._image }
+
+
+    override toJSON(): StandardCharacterData {
+        return {
+            key: this.key,
+            tag: 'Character',
+            pronouns: this.pronouns,
+            name: this.name,
+            firstImpression: this.firstImpression,
+            oneCoolThing: this.oneCoolThing,
+            outfit: this.outfit,
+            image: this.image
+        }
+    }
+
+    override get schema(): GenericTreeNode<SchemaTag> {
+        const pronounsFinalItem: Omit<SchemaPronounsTag, 'tag'> | undefined = this.pronouns
+            ? treeNodeTypeguard(isSchemaPronouns)(this.pronouns)
+                ? (
+                    (this.pronouns.data.subject === 'they') &&
+                    (this.pronouns.data.object === 'them') &&
+                    (this.pronouns.data.possessive === 'theirs') &&
+                    (this.pronouns.data.adjective === 'their') &&
+                    (this.pronouns.data.reflexive === 'themself')
+                )
+                    ? undefined
+                    : this.pronouns.data
+                : undefined
+            : undefined
+        return {
+            data: { tag: 'Character', key: this.key, Pronouns: pronounsFinalItem ?? { subject: 'they', object: 'them', possessive: 'theirs', adjective: 'their', reflexive: 'themself' } },
+            children: [
+                this.name,
+                pronounsFinalItem ? { data: { ...pronounsFinalItem, tag: 'Pronouns' as const }, children: [] } : undefined,
+                this.firstImpression,
+                this.oneCoolThing,
+                this.outfit,
+                this.image
+            ].filter(excludeUndefined).flat(1)
+        }
+    }
+
+    override merge(incoming: StandardComponentAbstract): StandardCharacter {
+        if (!(incoming instanceof StandardCharacter)) {
+            throw new Error('Type mistmatch on StandardComponent merge')
+        }
+        // const args: StandardCharacterData = {
+        //     key: this.key,
+        //     tag: 'Character',
+        // }
+        // return new StandardCharacter(args)
+        return incoming
+    }
+}
+
+export default StandardCharacter
