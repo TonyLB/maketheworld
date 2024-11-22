8438cb86cae5dbe2d8eb770cb3cfcecd99fdb2ef
TonyLB <anthony.lowerbasch@gmail.com>
2024-11-22 15:36:39 -0500

    ISS4463: Adde fromNDJSON to StandardImage

--- packages/mtw-wml/ts/standardize/components/ndjson.ts
+++ packages/mtw-wml/ts/standardize/components/ndjson.ts
@@ -2,8 +2,7 @@ import StandardComponentAbstract from "./abstract"
 
 export const ndjsonWrap = <TBase extends new (...args: any[]) => StandardComponentAbstract>(Base: TBase, label: string) => {
     return class NDJSONWrapped extends Base {
-        static fromNDJSON(...args: any[]): NDJSONWrapped {
-            const payload = args[0]
+        static fromNDJSON(payload: any): NDJSONWrapped {
             if (payload && ('key' in payload)) {
                 const returnValue = ('universalKey' in payload)
                     ? (new NDJSONWrapped(payload)).withUniversalKey(payload.universalKey)
