70c54f4ef538a1a0fac26ec158a5ba65f2222e8e
TonyLB <anthony.lowerbasch@gmail.com>
2024-11-22 14:03:18 -0500

    ISS4663: Refactored ndjsonWrap

--- packages/mtw-wml/ts/standardize/components/ndjson.ts
+++ packages/mtw-wml/ts/standardize/components/ndjson.ts
@@ -2,13 +2,13 @@ import StandardComponentAbstract from "./abstract"
 
 export const ndjsonWrap = <TBase extends new (...args: any[]) => StandardComponentAbstract>(Base: TBase, label: string) => {
     return class NDJSONWrapped extends Base {
-        static fromNDJSON(...args: any[]): typeof Base {
+        static fromNDJSON(...args: any[]): NDJSONWrapped {
             const payload = args[0]
             if (payload && ('key' in payload)) {
                 const returnValue = ('universalKey' in payload)
-                    ? (new Base(payload)).withUniversalKey(payload.universalKey)
-                    : new Base(payload)
-                return returnValue as unknown as typeof Base
+                    ? (new NDJSONWrapped(payload)).withUniversalKey(payload.universalKey)
+                    : new NDJSONWrapped(payload)
+                return returnValue
             }
             else {
                 throw new Error(`Type mismatch in ${label} fromNDJSON`)
