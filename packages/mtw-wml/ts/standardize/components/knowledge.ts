2d4e5aa7664f8bec301d4766e9fc62f3d3bb6266
TonyLB <anthony.lowerbasch@gmail.com>
2024-12-26 12:31:34 -0500

    ISS4878: Added examples property to StandardKnowledge class

--- packages/mtw-wml/ts/standardize/components/knowledge.ts
+++ packages/mtw-wml/ts/standardize/components/knowledge.ts
@@ -1,5 +1,5 @@
 import { excludeUndefined } from "../../lib/lists"
-import { isSchemaDescription, isSchemaKnowledge, isSchemaName, isSchemaOutputTag, SchemaDescriptionTag, SchemaNameTag, SchemaOutputTag, SchemaTag } from "../../schema/baseClasses"
+import { isSchemaDescription, isSchemaExample, isSchemaKnowledge, isSchemaName, isSchemaOutputTag, SchemaDescriptionTag, SchemaNameTag, SchemaOutputTag, SchemaTag } from "../../schema/baseClasses"
 import { wrappedNodeTypeGuard } from "../../schema/utils"
 import SchemaTagTree from "../../tagTree/schema"
 import { GenericTree, GenericTreeNode, GenericTreeNodeFiltered, treeNodeTypeguard } from "../../tree/baseClasses"
@@ -8,30 +8,33 @@ import { componentClassFactory, ComponentConstructorMethods, StandardComponent }
 import { StandardKnowledgeData } from "./dataTypes/knowledge"
 import { StandardComponentExport, StandardComponentImport } from "./dataTypes/metaData"
 import { StandardExportItem, StandardImportItem } from "./metaData"
-import { outputNodeToStandardItem } from "./utils/constructor"
-import linkReferenceKeys, { dependencyReferenceKeys } from "./utils/references"
-import { combineTaggedChildren } from "./utils/merge"
-import { applyTreeCallbackToNode } from "./utils/mapContents"
+import linkReferenceKeys, { dependencyReferenceKeys, mergeUniqueReferences } from "./utils/references"
 import { StandardRender } from "../render"
 import { extractStandardRender, rebuildSchemaFromStandardRender } from "./utils/extractStandardRender"
 import { stripUIFields } from "../render/utils"
+import { StandardToJSONOptions } from "./baseClasses"
+import StandardReference from "./reference"
+import { StandardReferenceData } from "./dataTypes"
 
 export class StandardKnowledgePayload implements ComponentConstructorMethods<StandardKnowledgeData> {
     _name?: StandardRender;
     _description?: StandardRender;
+    _examples: StandardReference[] = [];
     tag = 'Knowledge' as const
 
     constructor(previous?: StandardKnowledgePayload) {
         if (previous) {
             this._name = previous._name
             this._description = previous._description
+            this._examples = previous._examples
         }
     }
 
     fromJSON(props: StandardKnowledgeData) {
-        const { name, description } = props
+        const { name, description, examples } = props
         this._name = extractStandardRender(name, isSchemaName, 'Schema mismatch in StandardKnowledge constructor')
         this._description = extractStandardRender(description, isSchemaDescription, 'Schema mismatch in StandardKnowledge constructor')
+        this._examples = examples?.map((example) => new StandardReference(example)) ?? []
     }
 
     fromSchema(node: GenericTreeNode<SchemaTag>) {
@@ -41,6 +44,7 @@ export class StandardKnowledgePayload implements ComponentConstructorMethods<Sta
             const descriptionItem = tagTree.filter({ match: 'Description' }).tree.find(wrappedNodeTypeGuard(isSchemaDescription))
             this._name = extractStandardRender(nameItem as EditWrappedStandardNode<SchemaNameTag, SchemaOutputTag>, isSchemaName, 'Schema mismatch in StandardKnowledge constructor')
             this._description = extractStandardRender(descriptionItem as EditWrappedStandardNode<SchemaDescriptionTag, SchemaOutputTag>, isSchemaDescription, 'Schema mismatch in StandardKnowledge constructor')
+            this._examples = node.children.filter(treeNodeTypeguard(isSchemaExample)).map((reference) => (new StandardReference(reference)))
             return
         }
         throw new Error('Schema mismatch in StandardKnowledge constructor')
@@ -48,7 +52,8 @@ export class StandardKnowledgePayload implements ComponentConstructorMethods<Sta
 
     get name() { return rebuildSchemaFromStandardRender(this._name, { tag: 'Name' as const }) }
     get description() { return rebuildSchemaFromStandardRender(this._description, { tag: 'Description' as const }) }
-    
+    get examples() { return this._examples }
+
     toJSON(options): Omit<StandardKnowledgeData, 'key' | 'universalKey'> {
         const { stripUIFields: stripUI } = options ?? {}
         return {
@@ -58,14 +63,18 @@ export class StandardKnowledgePayload implements ComponentConstructorMethods<Sta
                 : this.name,
             description: stripUI
                 ? rebuildSchemaFromStandardRender(this._description?.mapContents(stripUIFields), { tag: 'Description' as const })
-                : this.description
+                : this.description,
+            ...(this.examples.length ? { examples: this.examples.map((reference) => (reference.toJSON() as StandardReferenceData)) } : {})
         }
     }
 
     schema(key: string): GenericTreeNode<SchemaTag> {
         return {
             data: { tag: 'Knowledge', key },
-            children: [this.name, this.description].filter(excludeUndefined).filter(({ children }) => (children.length))
+            children: [
+                ...this.examples.map((reference) => (reference.schema)),
+                ...[this.name, this.description].filter(excludeUndefined).filter(({ children }) => (children.length))
+            ]
         }
     }
 
@@ -73,6 +82,7 @@ export class StandardKnowledgePayload implements ComponentConstructorMethods<Sta
         const returnValue = new StandardKnowledgePayload()
         returnValue._name = (this._name && incoming._name) ? this._name.merge(incoming._name) : this._name ?? incoming._name
         returnValue._description = (this._description && incoming._description) ? this._description.merge(incoming._description) : this._description ?? incoming._description
+        returnValue._examples = mergeUniqueReferences(this.examples, incoming.examples)
         return returnValue as this
     }
 
@@ -81,7 +91,9 @@ export class StandardKnowledgePayload implements ComponentConstructorMethods<Sta
             ...linkReferenceKeys(this.description ? [this.description] : [])
                 .map((key) => ({ referenceType: 'Link' as const, key })),
             ...dependencyReferenceKeys(this.description ? [this.description] : [])
-                .map((key) => ({ referenceType: 'Dependency' as const, key }))
+                .map((key) => ({ referenceType: 'Dependency' as const, key })),
+            ...this.examples.map(({ key }) => ({ referenceType: 'Direct' as const, key })),
+            ...this.examples.map((example) => (example.referencedKeys())).flat(1)
         ]
     }
 
