c2ef7a7e45503e70f022cc0283e0a19b575940a0
TonyLB <anthony.lowerbasch@gmail.com>
2025-01-05 10:28:42 -0500

    ISS4969: Copied render subdirectory from mtw-wml to mtw-base

---
+++ packages/mtw-base/ts/render/space.ts
@@ -0,0 +1,30 @@
+import { GenericTreeNode } from "@tonylb/mtw-base/ts/genericTree"
+import { StandardRenderElement, StandardRenderAbstract } from "./baseClasses"
+import { isRenderTreeNode } from "./utils"
+import { isSchemaSpacer, SchemaSpacerTag } from "@tonylb/mtw-base/ts/schema/renderTree"
+
+export class StandardRenderSpace extends StandardRenderAbstract implements StandardRenderElement {
+    constructor(arg: any) {
+        super()
+        if (!(isRenderTreeNode(arg) && (typeof arg !== 'string') && isSchemaSpacer(arg.data) && arg.children.length === 0)) {
+            throw new Error('Invalid argument to StandardRenderSpace constructor')
+        }
+    }
+
+    override get plainString() {
+        return ' '
+    }
+
+    override toJSON(): GenericTreeNode<SchemaSpacerTag> {
+        return {
+            data: { tag: 'Space' as const },
+            children: []
+        }
+    }
+
+    override toNDJSON() {
+        return this.toJSON()
+    }    
+}
+
+export default StandardRenderSpace
