3813438856837ca19875e77bf37a948c96a3db4a
TonyLB <anthony.lowerbasch@gmail.com>
2025-01-05 20:36:34 -0500

    ISS4990: Fixed jest configuration for Vite

--- charcoal-client/src/components/DraggableTree/flatToNested.test.ts
+++ charcoal-client/src/components/DraggableTree/flatToNested.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from '@jest/globals'
 import flatToNested, { consolidateToLevel } from './flatToNested'
 
 interface ITest {
--- charcoal-client/src/components/Library/Edit/LibraryAsset.test.tsx
+++ charcoal-client/src/components/Library/Edit/LibraryAsset.test.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import LibraryAsset, { useLibraryAsset } from './LibraryAsset'
+import { beforeEach, describe, expect, it, jest } from '@jest/globals'
 
 jest.mock('../../../cacheDB')
 import { Schema } from '@tonylb/mtw-wml/ts/schema'
