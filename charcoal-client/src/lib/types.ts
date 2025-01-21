cafcedab6257689ead63ff7eb62361912a566285
TonyLB <anthony.lowerbasch@gmail.com>
2025-01-21 09:16:52 -0500

    ISS5107: Resolved remaining typescript warnings

--- charcoal-client/src/lib/types.ts
+++ charcoal-client/src/lib/types.ts
@@ -1,6 +1,6 @@
 export type NarrowOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never
 
-export const assertTypeguard = <T extends any, G extends T>(value: T, typeguard: (value) => value is G): G | undefined => {
+export const assertTypeguard = <T extends any, G extends T>(value: T, typeguard: (value: T) => value is G): G | undefined => {
     if (value && typeguard(value)) {
         return value
     }
