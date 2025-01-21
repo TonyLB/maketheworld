fe639ccf0f1f058cae4e9a6bfe9cbf1c1ff866ca
TonyLB <anthony.lowerbasch@gmail.com>
2025-01-21 11:26:41 -0500

    ISS5075: Added stub ExampleComponent

---
+++ charcoal-client/src/components/Library/Edit/ExampleEditor/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react"
+
+type ExampleEditorProps = {
+    componentId: string;
+}
+
+export const ExampleEditor: FunctionComponent<ExampleEditorProps> = ({ componentId }) => {
+    return null
+}
+
+export default ExampleEditor
