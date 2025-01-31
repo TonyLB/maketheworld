6f789c45fd3a755685a1175519cbda49210b22e1
TonyLB <anthony.lowerbasch@gmail.com>
2025-01-31 10:37:00 -0500

    ISS5054: Deprecated StandardComponentAbstract class

--- packages/mtw-wml/ts/standardize/components/abstract.ts
+++ packages/mtw-wml/ts/standardize/components/abstract.ts
@@ -18,78 +18,6 @@ export interface ComponentInterface {
     withUniversalKey(key: string): this;
 }
 
-export class StandardComponentAbstract implements ComponentInterface {
-    _key: string;
-    _remove?: boolean;
-    _universalKey?: string;
-    constructor(...args: any[]) {
-        const payload = args[0]
-        if (!payload) {
-            this._key = ''
-            return
-        }
-        if (typeof payload === 'string' && isLegalKey(payload)) {
-            this._key = payload
-            return
-        }
-        if (isSchemaTreeNode(payload) || typeof payload === 'string') {
-            const node = typeof payload === 'string'
-                ? nodeFromWML(payload)
-                : payload
-            if (treeNodeTypeguard((data): data is { key: string } => (Boolean(data && ('key' in data) && (data as any).key)))(node)) {
-                const { data } = node
-                this._key = data.key
-                return    
-            }
-        }
-        if (payload && ('key' in payload)) {
-            this._key = payload.key
-            if ('universalKey' in payload) {
-                this._universalKey = payload.universalKey
-            }
-            return
-        }
-        throw new Error('Cannot convert non-keyed schema item to StandardComponent')
-    }
-
-    get key(): string {
-        return this._key
-    }
-
-    get schema(): GenericTreeNode<SchemaTag> {
-        throw new Error('Cannot call schema on abstract class')
-    }
-
-    get isRemove() { return false }
-    get isReplace() { return false }
-    get match(): StandardComponentAbstract | undefined { return undefined }
-    get payload(): StandardComponentAbstract { return this }
-    get universalKey(): string | undefined { return this._universalKey}
-
-    toJSON({ stripUniversalKey = false }): StandardBaseData & SerializeNDJSONMixin {
-        return stripUniversalKey
-            ? { key: this.key }
-            : {
-                key: this.key,
-                universalKey: this.universalKey
-            }
-    }
-
-    clone(): this {
-        throw new Error('Cannot call clone on abstract class')
-    }
-    
-    merge(incoming: this): this | undefined {
-        throw new Error('Cannot call merge on abstract class')
-    }
-
-    withUniversalKey(key: string): this {
-        const returnValue = this.clone()
-        returnValue._universalKey = key
-        return returnValue
-    }
-}
-
 export interface HasName {
     name?: EditWrappedStandardNode<SchemaNameTag, SchemaOutputTag>;
 }
@@ -106,5 +34,3 @@ export interface HasFileAssociation {
     fileAssociation?: string;
     withFileAssociation(fileName: string): this;
 }
-
-export default StandardComponentAbstract
--- packages/mtw-wml/ts/standardize/components/character.ts
+++ packages/mtw-wml/ts/standardize/components/character.ts
@@ -1,12 +1,9 @@
 import { excludeUndefined } from "../../lib/lists"
 import { GenericTree, GenericTreeNode, GenericTreeNodeFiltered, treeNodeTypeguard } from "@tonylb/mtw-base/ts/genericTree"
 import { treeTypeGuard } from "../../tree/filter"
-import { EditWrappedStandardNode, SerializeNDJSONMixin } from "../baseClasses"
-import StandardComponentAbstract, { ComponentInterface } from "./abstract"
+import { EditWrappedStandardNode } from "../baseClasses"
 import { componentClassFactory, ComponentConstructorMethods } from "./component"
-import { StandardComponentData } from "./dataTypes"
 import { StandardCharacterData } from "./dataTypes/character"
-import { isSchemaTreeNode } from "./utils"
 import { isSchemaName, SchemaNameTag } from "@tonylb/mtw-base/ts/schema/example"
 import { isSchemaCharacter, isSchemaOutputTag, SchemaOutputTag, SchemaTag } from "@tonylb/mtw-base/ts/schema"
 import { isSchemaFirstImpression, isSchemaOneCoolThing, isSchemaOutfit, isSchemaPronouns, SchemaFirstImpressionTag, SchemaOneCoolThingTag, SchemaOutfitTag, SchemaPronounsTag } from "@tonylb/mtw-base/ts/schema/character"
@@ -118,107 +115,6 @@ export class StandardCharacter extends componentClassFactory(StandardCharacterPa
     get oneCoolThing() { return this._payload.oneCoolThing }
     get outfit() { return this._payload.outfit }
     get image() { return this._payload.image }
-}
-
-export class StandardCharacterLegacy extends StandardComponentAbstract implements ComponentInterface {
-    _name?: EditWrappedStandardNode<SchemaNameTag, SchemaOutputTag>;
-    _firstImpression?: EditWrappedStandardNode<SchemaFirstImpressionTag, SchemaTag>;
-    _oneCoolThing?: EditWrappedStandardNode<SchemaOneCoolThingTag, SchemaTag>;
-    _outfit?: EditWrappedStandardNode<SchemaOutfitTag, SchemaTag>;
-    _pronouns?: EditWrappedStandardNode<SchemaPronounsTag, SchemaTag>;
-    _image?: EditWrappedStandardNode<SchemaImageTag, SchemaTag>;
-    tag = 'Character' as const
-    constructor(args: StandardCharacterData | GenericTreeNode<SchemaTag>) {
-        super(args)
-        if (typeof args === 'string' || !args) {
-            this._pronouns = { children: [], data: { tag: 'Pronouns', subject: 'they', object: 'them', possessive: 'theirs', adjective: 'their', reflexive: 'themself' } }
-        }
-        else if (isSchemaTreeNode(args)) {
-            const { data } = args
-            if (!isSchemaCharacter(data)) {
-                throw new Error('Type mismatch in StandardCharacter constructor')
-            }
-            this._pronouns = (args.children.find(treeNodeTypeguard(isSchemaPronouns)) ?? { children: [], data: { tag: 'Pronouns', subject: 'they', object: 'them', possessive: 'theirs', adjective: 'their', reflexive: 'themself' } })
-            const confirmOutputChildren = <InputNode extends SchemaTag>(node: GenericTreeNodeFiltered<InputNode, SchemaTag> |  undefined): GenericTreeNodeFiltered<InputNode, SchemaOutputTag> | undefined => (node ? { data: node.data, children: treeTypeGuard({ tree: node.children, typeGuard: isSchemaOutputTag })} : undefined)
-            this._name = confirmOutputChildren(args.children.find(treeNodeTypeguard(isSchemaName)))
-            this._firstImpression = args.children.find(treeNodeTypeguard(isSchemaFirstImpression))
-            this._oneCoolThing = args.children.find(treeNodeTypeguard(isSchemaOneCoolThing))
-            this._outfit = args.children.find(treeNodeTypeguard(isSchemaOutfit))
-            this._image = args.children.find(treeNodeTypeguard(isSchemaImage))
-        }
-        else {
-            this._pronouns = args.pronouns
-            this._name = args.name
-            this._firstImpression = args.firstImpression
-            this._oneCoolThing = args.oneCoolThing
-            this._outfit = args.outfit
-            this._image = args.image
-        }
-    }
-
-    get pronouns() { return this._pronouns }
-    get name() { return this._name }
-    get firstImpression() { return this._firstImpression }
-    get oneCoolThing() { return this._oneCoolThing }
-    get outfit() { return this._outfit }
-    get image() { return this._image }
-
-    override toJSON(): StandardCharacterData {
-        return {
-            key: this.key,
-            tag: 'Character',
-            pronouns: this.pronouns,
-            name: this.name,
-            firstImpression: this.firstImpression,
-            oneCoolThing: this.oneCoolThing,
-            outfit: this.outfit,
-            image: this.image
-        }
-    }
-
-    toNDJSON(): StandardComponentData & SerializeNDJSONMixin { return this.toJSON() }
-
-    override get schema(): GenericTreeNode<SchemaTag> {
-        const pronounsFinalItem: Omit<SchemaPronounsTag, 'tag'> | undefined = this.pronouns
-            ? treeNodeTypeguard(isSchemaPronouns)(this.pronouns)
-                ? (
-                    (this.pronouns.data.subject === 'they') &&
-                    (this.pronouns.data.object === 'them') &&
-                    (this.pronouns.data.possessive === 'theirs') &&
-                    (this.pronouns.data.adjective === 'their') &&
-                    (this.pronouns.data.reflexive === 'themself')
-                )
-                    ? undefined
-                    : this.pronouns.data
-                : undefined
-            : undefined
-        return {
-            data: { tag: 'Character', key: this.key, Pronouns: pronounsFinalItem ?? { subject: 'they', object: 'them', possessive: 'theirs', adjective: 'their', reflexive: 'themself' } },
-            children: [
-                this.name,
-                pronounsFinalItem ? { data: { ...pronounsFinalItem, tag: 'Pronouns' as const }, children: [] } : undefined,
-                this.firstImpression,
-                this.oneCoolThing,
-                this.outfit,
-                this.image
-            ].filter(excludeUndefined).flat(1)
-        }
-    }
-
-    override merge(incoming: this): this {
-        if (!(incoming instanceof StandardCharacterLegacy)) {
-            throw new Error('Type mistmatch on StandardComponent merge')
-        }
-        // const args: StandardCharacterData = {
-        //     key: this.key,
-        //     tag: 'Character',
-        // }
-        // return new StandardCharacter(args)
-        return incoming
-    }
-
-    get fileName() { return undefined }
-    withFileName(key: string): this { return this }
 }
 
 export default StandardCharacter
