d050a10cfee5f179f3226342de40bf783444dff1
TonyLB <anthony.lowerbasch@gmail.com>
2025-02-03 11:40:11 -0500

    ISS5225: Lifted binarySearch unit tests to their own file

--- charcoal-client/src/slices/messages/index.test.ts
+++ charcoal-client/src/slices/messages/index.test.ts
@@ -1,6 +1,5 @@
 import { vi } from 'vitest'
 import reducer, {
-    binarySearch,
     receiveMessages,
     getMessages
 } from './index'
@@ -11,67 +10,6 @@ import {
 vi.mock('../../cacheDB')
 
 describe('messages reducer', () => {
-    describe('binarySearch', () => {
-        const testArray = [
-            [2, 'Test-2'],
-            [5, 'Test-5'],
-            [7, 'Test-7'],
-            [8, 'Test-8'],
-            [100, 'Test-B'],
-            [100, 'Test-D'],
-            [100, 'Test-H'],
-            [1000, 'Test-1000'],
-            [10000, 'Test-10000']
-        ].map(([value, label]) => ({
-            DisplayProtocol: 'WorldMessage',
-            MessageId: label,
-            Message: ['Test'],
-            CreatedTime: value,
-            Target: 'CHARACTER#Test'
-        })) as WorldMessage[]
-
-        it('should place elements at beginning of array', () => {
-            expect(binarySearch(testArray, 1, 'Test-1')).toEqual({ exactMatch: false, index: 0 })
-        })
-        it('should place elements at end of array', () => {
-            expect(binarySearch(testArray, 20000, 'Test-20000')).toEqual({ exactMatch: false, index: 9 })
-        })
-        it('should place elements inside array', () => {
-            expect(binarySearch(testArray, 6, 'Test-6')).toEqual({ exactMatch: false, index: 2 })
-        })
-        it('should exact match at beginning of array', () => {
-            expect(binarySearch(testArray, 2, 'Test-2')).toEqual({ exactMatch: true, index: 0 })
-        })
-        it('should exact match at end of array', () => {
-            expect(binarySearch(testArray, 10000, 'Test-10000')).toEqual({ exactMatch: true, index: 8 })
-        })
-        it('should exact match inside array', () => {
-            expect(binarySearch(testArray, 8, 'Test-8')).toEqual({ exactMatch: true, index: 3 })
-        })
-
-        it('should place elements at beginning of range', () => {
-            expect(binarySearch(testArray, 100, 'Test-A')).toEqual({ exactMatch: false, index: 4 })
-        })
-        it('should place elements at end of range', () => {
-            expect(binarySearch(testArray, 100, 'Test-Z')).toEqual({ exactMatch: false, index: 7 })
-        })
-        it('should place elements inside range', () => {
-            expect(binarySearch(testArray, 100, 'Test-G')).toEqual({ exactMatch: false, index: 6 })
-        })
-        it('should match elements at beginning of range', () => {
-            expect(binarySearch(testArray, 100, 'Test-B')).toEqual({ exactMatch: true, index: 4 })
-        })
-        it('should match elements at end of range', () => {
-            expect(binarySearch(testArray, 100, 'Test-H')).toEqual({ exactMatch: true, index: 6 })
-        })
-        it('should match elements inside range', () => {
-            expect(binarySearch(testArray, 100, 'Test-D')).toEqual({ exactMatch: true, index: 5 })
-        })
-
-        it('should return 0 on empty array', () => {
-            expect(binarySearch([], 100, 'Test-D')).toEqual({ exactMatch: false, index: 0 })
-        })
-    })
 
     describe('receiveMessages', () => {
         const testArray = [
