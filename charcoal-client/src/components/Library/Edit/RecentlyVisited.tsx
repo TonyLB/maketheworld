77735865a54e9f6d6ee33adb077ec7cd9ffd0730
TonyLB <anthony.lowerbasch@gmail.com>
2025-02-05 10:55:32 -0500

    ISS5227: Refactored stub with Material list

--- charcoal-client/src/components/Library/Edit/RecentlyVisited.tsx
+++ charcoal-client/src/components/Library/Edit/RecentlyVisited.tsx
@@ -1,5 +1,10 @@
 import * as React from "react"
 import { FunctionComponent, useMemo } from "react"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import { blue } from "@mui/material/colors"
+
 import { useLibraryAsset } from "./LibraryAsset"
 import { getRecentlyVisited } from "../../../slices/messages/selectors"
 import { useSelector } from "react-redux"
@@ -14,7 +19,17 @@ export const RecentlyVisited: FunctionComponent<RecentlyVisitedProps> = () => {
     const recentlyVisitedTimestamp = useMemo(() => (Date.now() - 1000 * 60 * 15), [])
     const recentlyVisited = useSelector(getRecentlyVisited(recentlyVisitedTimestamp))
 
-    return <React.Fragment>
-        { recentlyVisited.map(({ fromAssetId, key, name }, index) => (<div key={`recentlyVisited-${index}`}>{ `${fromAssetId}[${key}]: ${name}` }</div>)) }
-    </React.Fragment>
+    return <List dense>
+        { recentlyVisited.map(({ fromAssetId, key, name }, index) => (
+            <ListItem
+                key={`recentlyVisited-${index}`}
+                sx={{ borderRadius: '0.5em',
+                    border: '1px solid',
+                    borderColor: blue[400],
+                }}
+            >
+                <ListItemText primary={name} secondary={`${fromAssetId.split('#').slice(-1)[0]}[${key}]`} />
+            </ListItem>
+        )) }
+    </List>
 }
