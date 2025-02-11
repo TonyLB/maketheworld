427e7a06821ad8d34d66a98dc88785dcc92a46ea
TonyLB <anthony.lowerbasch@gmail.com>
2025-02-11 08:38:50 -0500

    ISS5263: Added standard grant datatype

---
+++ packages/mtw-wml/ts/standardize/authorization/components/dataTypes/grant.ts
@@ -0,0 +1,21 @@
+import checkTypes, { CheckTypes } from "@tonylb/mtw-base/ts/utils/checkTypes";
+import { StandardBaseData } from "../../../components/dataTypes/abstract"
+import { checkAll } from "../../../components/dataTypes/typeguards"
+
+export type StandardGrantData = {
+    tag: 'Grant';
+    player: string;
+    actions: string[];
+} & StandardBaseData
+
+export const isStandardAction = (arg: any): arg is StandardGrantData => {
+    if (typeof arg !== 'object') {
+        return false
+    }
+
+    return checkAll(
+        ('tag' in arg && arg.tag === 'Grant'),
+        checkTypes({ required: { tag: CheckTypes.STRING, player: CheckTypes.STRING }, values: { tag: 'Grant' } })(arg),
+        Array.isArray(arg.actions) && arg.actions.every((action: any) => typeof action === 'string')
+    )
+}
