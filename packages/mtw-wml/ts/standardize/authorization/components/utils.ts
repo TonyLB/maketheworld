ac73c5e25461f853e73de9d1a5b53cc01f092bc2
TonyLB <anthony.lowerbasch@gmail.com>
2025-02-13 10:15:49 -0500

    ISS5280: Added SignedStringSet to authorization utils

--- packages/mtw-wml/ts/standardize/authorization/components/utils.ts
+++ packages/mtw-wml/ts/standardize/authorization/components/utils.ts
@@ -7,3 +7,19 @@ export const removeStringsFromList = (base: string[], remove: string[]): string[
 export const addStringsToList = (base: string[], add: string[]): string[] => {
     return unique(base, add)
 }
+
+export type SignedStringSet = {
+    add: string[]
+    remove: string[]
+}
+
+export const diffSignedStringSets = (base: SignedStringSet, incoming: SignedStringSet): SignedStringSet => {
+    const absentAdditions = removeStringsFromList(base.add, incoming.add)
+    const absentRemovals = removeStringsFromList(base.remove, incoming.remove)
+    const newAdditions = removeStringsFromList(incoming.add, base.add)
+    const newRemovals = removeStringsFromList(incoming.remove, base.remove)
+    return {
+        add: [...absentRemovals, ...newAdditions],
+        remove: [...absentAdditions, ...newRemovals]
+    }
+}
