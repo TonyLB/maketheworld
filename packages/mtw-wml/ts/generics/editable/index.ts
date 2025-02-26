c59d6bfe3b1db43ef02d80168ec3b8c949861a4d
TonyLB <anthony.lowerbasch@gmail.com>
2025-02-25 21:57:15 -0500

    ISS5401: Added typeguard argument to standardEditableFactory

--- packages/mtw-wml/ts/generics/editable/index.ts
+++ packages/mtw-wml/ts/generics/editable/index.ts
@@ -1,21 +1,33 @@
-export type StandardEditableFactoryProps<T> = {
+import { StandardEditableData } from '@tonylb/mtw-base/ts/editable'
 
+export type StandardEditableFactoryProps<T> = {
+    typeguard: (x: any) => x is T;
 }
 
 export interface StandardEditable<T> {
 }
 
 export type StandardEditableFactoryReturn<T> = {
-    generatedClass: new () => StandardEditable<T>
+    factory: () => StandardEditable<T> | undefined;
+    typeguard: (x: any) => x is StandardEditableData<T>;
 }
 
 export const standardEditableFactory = <T>(props: StandardEditableFactoryProps<T>): StandardEditableFactoryReturn<T> => {
-    class generatedClass implements StandardEditable<T> {
-        constructor() {
-            
-        }
-    }
     return {
-        generatedClass
+        factory: () => undefined,
+        typeguard: (x: any): x is StandardEditableData<T> => {
+            if (props.typeguard(x)) {
+                return true
+            }
+            if (typeof x === 'object' && x !== null) {
+                if (x.tag === 'Remove' && props.typeguard(x.match)) {
+                    return true
+                }
+                if (x.tag === 'Replace' && props.typeguard(x.match) && props.typeguard(x.payload)) {
+                    return true
+                }
+            }
+            return false
+        }        
     }
 }
