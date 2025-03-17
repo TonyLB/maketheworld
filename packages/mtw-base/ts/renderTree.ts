f36ac32bb1b52479ff8228528f0b2ab016ee13cc
TonyLB <anthony.lowerbasch@gmail.com>
2025-03-17 08:50:35 -0400

    ISS5472: Refactored StandardRender add operation without helper subclasses

--- packages/mtw-base/ts/renderTree.ts
+++ packages/mtw-base/ts/renderTree.ts
@@ -1,4 +1,8 @@
-import { isSchemaOutputTag, SchemaOutputTag } from "./schema";
+import { GenericTree } from "./genericTree";
+import { isSchemaOutputTag, SchemaOutputTag, SchemaTag } from "./schema";
+import { isSchemaCondition, isSchemaConditionFallthrough, isSchemaConditionStatement } from "./schema/condition";
+import { isSchemaLineBreak, isSchemaLink, isSchemaSpacer, isSchemaString } from "./schema/renderTree";
+import { excludeUndefined } from "./utils/lists";
 
 export type RenderTreeNode = string | {
     data: SchemaOutputTag;
@@ -20,4 +24,38 @@ export const isRenderTreeNode = (node: any): node is RenderTreeNode => {
 
 export const isRenderTree = (node: any): node is RenderTree => {
     return Array.isArray(node) && node.every(isRenderTreeNode)
+}
+
+export const renderTreeToSchema = (tree: RenderTree): GenericTree<SchemaOutputTag> => {
+    return tree.map((node) => {
+        if (typeof node === "string") {
+            return { data: { tag: "String", value: node }, children: [] }
+        } else {
+            return { data: node.data, children: renderTreeToSchema(node.children) }
+        }
+    })
+}
+
+export const schemaToRenderTree = (tree: GenericTree<SchemaTag>): RenderTree => {
+    return tree.map<RenderTreeNode | undefined>(node => {
+        if (isSchemaString(node.data)) {
+            return node.data.value
+        }
+        else if (
+            isSchemaCondition(node.data) ||
+            isSchemaConditionStatement(node.data) ||
+            isSchemaConditionFallthrough(node.data) ||
+            isSchemaLink(node.data) ||
+            isSchemaSpacer(node.data) ||
+            isSchemaLineBreak(node.data)
+        ) {
+            return {
+                data: node.data, children: schemaToRenderTree(node.children)
+            }
+        }
+        else {
+            return undefined
+        }
+    })
+    .filter(excludeUndefined)
 }
