bd51facbd80192103dbf267283a6b962db0d5412
TonyLB <anthony.lowerbasch@gmail.com>
2025-04-10 07:26:48 -0400

    ISS5539: Added console logging to diagnose FetchImportDefaults

--- packages/mtw-wml/ts/standardize/baseClasses.test.ts
+++ packages/mtw-wml/ts/standardize/baseClasses.test.ts
@@ -1,4 +1,5 @@
 import { isStandardNDJSONLine } from './baseClasses'
+import { isStandardComponent, isStandardRoom } from './components/dataTypes'
 
 describe('StandardForm baseClasses', () => {
     it('should correctly identify an NDJSON line with additional fields', () => {
@@ -16,10 +17,31 @@ describe('StandardForm baseClasses', () => {
                 },
                 children: [{ data: { tag: "String", value: "market square" }, children: [] }]
             }],
-            themes: [],
             examples: [{ key: "base", tag: "Example" }],
             from: { action: "Content", payload: { assetId: "primitives", fromKey: "VORTEX" } }
         }
         expect(isStandardNDJSONLine(line)).toBe(true)        
     })
+
+    it('should correctly identify component data', () => {
+        const line = {
+            key: "VORTEX",
+            universalKey: "ROOM#VORTEX",
+            tag: "Room",
+            shortName: "Sturdy Mug",
+            exits: [{
+                data: {
+                    tag: "Exit",
+                    key: "VORTEX#marketSquare",
+                    from: "VORTEX",
+                    to: "marketSquare"
+                },
+                children: [{ data: { tag: "String", value: "market square" }, children: [] }]
+            }],
+            examples: [{ key: "base", tag: "Example" }],
+            from: { action: "Content", payload: { assetId: "primitives", fromKey: "VORTEX" } }
+        }
+        expect(isStandardComponent(line)).toBe(true)
+    })
+
 })
