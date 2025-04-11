2311dd8c1556c53dc38e00e40c2fab6b462a0ad9
TonyLB <anthony.lowerbasch@gmail.com>
2025-04-11 18:25:53 -0400

    ISS5546: Added stub cacheAsset messageBus handler to Asset lambda

---
+++ lambda/assets/cacheAsset/index.ts
@@ -0,0 +1,6 @@
+import { CacheAssetMessage, MessageBus } from "../messageBus/baseClasses";
+
+export const cacheAssetMessage = async ({ payloads, messageBus }: { payloads: CacheAssetMessage[], messageBus: MessageBus }): Promise<void> => {
+}
+
+export default cacheAssetMessage
