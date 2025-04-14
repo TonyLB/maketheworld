bccb51fcd2a3362953de328bf19e56905e46df4c
TonyLB <anthony.lowerbasch@gmail.com>
2025-04-14 08:48:31 -0400

    ISS5547: Extended asset lambda cacheAsset to apply diff to assetDB

--- lambda/assets/cacheAsset/index.ts
+++ lambda/assets/cacheAsset/index.ts
@@ -3,6 +3,8 @@ import internalCache from "../internalCache";
 import { CacheAssetMessage, MessageBus } from "../messageBus/baseClasses";
 import AssetWorkspace from "@tonylb/mtw-asset-workspace";
 import { schemaToWML } from "@tonylb/mtw-wml/ts/schema";
+import { StandardRemove } from "@tonylb/mtw-wml/ts/standardize/components/edits";
+import { assetDB } from "@tonylb/mtw-utilities/ts/dynamoDB";
 
 export const cacheAssetMessage = async ({ payloads, messageBus }: { payloads: CacheAssetMessage[], messageBus: MessageBus }): Promise<void> => {
     await Promise.all(
@@ -26,6 +28,31 @@ export const cacheAssetMessage = async ({ payloads, messageBus }: { payloads: Ca
             const diff = dbAsset.diff(fileAsset)
             if (diff) {
                 console.log(`Difference found for asset ${assetId}:`, schemaToWML([diff.schema]))
+                await Promise.all(Object.values(diff.byId)
+                    .map(async (component) => {
+                        if (!component.universalKey) {
+                            return
+                        }
+                        if (component instanceof StandardRemove) {
+                            await assetDB.deleteItem({
+                                AssetId: component.universalKey,
+                                DataCategory: `ASSET#${assetId}`
+                            })
+                        }
+                        else {
+                            const fileComponent = fileAsset.byId[component.key]
+                            if (!fileComponent) {
+                                console.warn(`Component ${component.key} not found in file asset`)
+                                return
+                            }
+                            await assetDB.putItem({
+                                ...(fileComponent.toJSON()),
+                                AssetId: component.universalKey,
+                                DataCategory: `ASSET#${assetId}`,
+                            })
+                        }
+                    })
+                )
             }
         })
     )
