fbb2ecbf6d78cceb5a239298d350847cdb8f9c24
TonyLB <anthony.lowerbasch@gmail.com>
2025-05-25 12:42:47 -0400

    ISS5691: Update StandardForm schema to work off of components property

--- packages/mtw-wml/ts/standardize/index.ts
+++ packages/mtw-wml/ts/standardize/index.ts
@@ -510,10 +510,10 @@ export class StandardForm {
 
     get schema(): GenericTreeNode<SchemaTag> {
         const metaData = this.metaData
-        const children = Object.values(this._byId)
+        const children = this._components
             .filter(({ key }) => (!(key ?? '').includes('.')))
-            .sort(standardComponentSortOrder(this._byId))
-            .map((component) => (component.nestedSchema(this._byId, {})))
+            .sort(standardComponentSortOrder(this.byId))
+            .map((component) => (component.nestedSchema(this.byId, {})))
         const imports = metaData.filter(wrappedNodeTypeGuard(isSchemaImport))
         const importKeys = unique(imports.map(({ children }) => (children.map(({ data }) => (data)).filter(isImportable).map(({ key, as }) => (as ?? key)))).flat(1))
         return {
