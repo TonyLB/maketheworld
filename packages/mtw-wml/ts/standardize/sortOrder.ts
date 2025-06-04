4e693980226fca177431c9831414ee0c82e4fde8
TonyLB <anthony.lowerbasch@gmail.com>
2025-06-03 20:53:05 -0400

    ISS5722: Refactored standardComponentSortOrder to use context rather than key parsing

--- packages/mtw-wml/ts/standardize/sortOrder.ts
+++ packages/mtw-wml/ts/standardize/sortOrder.ts
@@ -1,40 +1,28 @@
 import { ComponentTag } from "./components/dataTypes/abstract"
-import { StandardReferenceSimple } from "./components/reference"
+import { StandardReferenceSimple, StandardReferenceSimpleBase } from "./components/reference"
 
 export const standardComponentSortOrder = (lookup: (value: string) => StandardReferenceSimple) => (referenceA: StandardReferenceSimple, referenceB: StandardReferenceSimple): number => {
     //
-    // Subcomponents will have keys that start with their ancestry, separated by periods (i.e. "Room1.Feature1").
+    // Subcomponents will have keys that include their context of ancestry.
     // First compare the two keys to see if one is a subcomponent of the other. If so, the subcomponent should come second.
-    // Next, see if the two keys have ancestors not in common:  If so, use the passed byId object to look up the
-    // ancestor(s) and compare against those instead.
-    // Finally, if they are peers in terms of ancestry, compare the two keys directly.
+    // Next, see if the two keys have ancestors not in common:  If so, compare the first differing ancestor in place
+    // of the key provided.
+    // Lastly, if they are in the same context, compare the tags and then the keys.
     //
-    const keyA = referenceA.key ?? ''
-    const keyB = referenceB.key ?? ''
-    if (keyA.startsWith(`${keyB}.`)) {
-        return 1
-    }
-    if (keyB.startsWith(`${keyA}.`)) {
+    if ((referenceA.context ?? []).some(referenceB.payload.equals.bind(referenceB.payload))) {
         return -1
     }
-    const ancestorsA = keyA.split('.').slice(0, -1)
-    const ancestorsB = keyB.split('.').slice(0, -1)
-    const { commonAncestors } = ancestorsA.reduce<{ commonAncestors: string[]; differenceHasOccurred: boolean }>((previous, ancestor, index) => {
-        if (previous.differenceHasOccurred) {
-            return previous
-        }
-        if (ancestorsB[index] === ancestor) {
-            return { ...previous, commonAncestors: [...previous.commonAncestors, ancestor] }
-        }
-        return { ...previous, differenceHasOccurred: true }
-    }, { commonAncestors: [], differenceHasOccurred: false })
-    const commonAncestorString = commonAncestors.join('.')
-
-    const differingAncestorsA = ancestorsA.slice(commonAncestors.length)
-    const differingAncestorsB = ancestorsB.slice(commonAncestors.length)
-
-    const elementToCompareA = differingAncestorsA.length ? lookup([commonAncestorString, differingAncestorsA[0]].filter((value) => (value)).join('.')) : referenceA
-    const elementToCompareB = differingAncestorsB.length ? lookup([commonAncestorString, differingAncestorsB[0]].filter((value) => (value)).join('.')) : referenceB
+    if ((referenceB.context ?? []).some(referenceA.payload.equals.bind(referenceA.payload))) {
+        return 1
+    }
+    const differingA: StandardReferenceSimpleBase | undefined = (referenceA.context ?? []).find((reference) => (!referenceB.context?.some(reference.equals.bind(reference))))
+    const differingB: StandardReferenceSimpleBase | undefined = (referenceB.context ?? []).find((reference) => (!referenceA.context?.some(reference.equals.bind(reference))))
+    const elementToCompareA = differingA
+        ? new StandardReferenceSimple(differingA.toJSON())
+        : referenceA
+    const elementToCompareB = differingB
+        ? new StandardReferenceSimple(differingB.toJSON())
+        : referenceB
     
     const componentKeys: ComponentTag[] = ['Character', 'Image', 'Room', 'Feature', 'Knowledge', 'Map', 'Message', 'Moment', 'Variable', 'Computed', 'Action']
     const indexA = componentKeys.indexOf(elementToCompareA.tag)
