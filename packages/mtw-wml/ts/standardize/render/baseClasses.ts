740420bdb566805b03ea1923d24f59cfaf3cc067
TonyLB <anthony.lowerbasch@gmail.com>
2025-06-15 11:35:29 -0400

    ISS5756: Added remapReference to StandardRenderLink class

--- packages/mtw-wml/ts/standardize/render/baseClasses.ts
+++ packages/mtw-wml/ts/standardize/render/baseClasses.ts
@@ -1,6 +1,8 @@
 import { GenericTreeNode } from "@tonylb/mtw-base/ts/genericTree";
 import { RenderTreeNode } from "@tonylb/mtw-base/ts/renderTree";
 import { SchemaOutputTag } from "@tonylb/mtw-base/ts/schema";
+import { StandardKey } from "../components/reference";
+import { ReferenceFormat } from "../components/utils/references";
 
 export interface StandardRenderElement {
     plainString: string;
@@ -14,4 +16,8 @@ export class StandardRenderAbstract implements StandardRenderElement {
     toJSON(): GenericTreeNode<SchemaOutputTag> { return { data: { tag: 'String' as const, value: '' }, children: [] } }
     toNDJSON(): RenderTreeNode { return '' }
     clone(): StandardRenderElement { return new StandardRenderAbstract() }
+    remapReferences(props: { mapping: StandardKey[]; mapTo: ReferenceFormat }): this {
+        const returnValue = this.clone() as this
+        return returnValue
+    }
 }
