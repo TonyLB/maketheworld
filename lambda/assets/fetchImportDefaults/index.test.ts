3eb228aaf2c9f4d89741b3d1fb786c820d12b547
TonyLB <anthony.lowerbasch@gmail.com>
2025-07-02 07:38:46 -0400

    ISS5841: Refactored asset lambda recursiveFetchImports

--- lambda/assets/fetchImportDefaults/index.test.ts
+++ lambda/assets/fetchImportDefaults/index.test.ts
@@ -1,21 +1,113 @@
 import { fetchImportsMessage } from '.'
 
 jest.mock('../clients')
-import { sfnClient } from '../clients'
+import { snsClient } from '../clients'
 jest.mock('../internalCache')
 import internalCache from '../internalCache'
 jest.mock('../messageBus')
 import messageBus from '../messageBus'
 jest.mock('./baseClasses')
 import { Graph } from '@tonylb/mtw-utilities/ts/graphStorage/utils/graph'
+import { FetchImportsJSONHelper } from './baseClasses'
+import { StandardForm } from '@tonylb/mtw-wml/ts/standardize'
 
-const sfnClientMock = sfnClient as jest.Mocked<typeof sfnClient>
+const snsClientMock = snsClient as jest.Mocked<typeof snsClient>
 const internalCacheMock = jest.mocked(internalCache, { shallow: false })
 
 describe('fetchImportsMessage', () => {
     beforeEach(() => {
         jest.clearAllMocks()
         jest.resetAllMocks()
+        const testFinal = new StandardForm(`<Asset key=(testFinal)>
+            <Room uuid=(testNonImport) key=(testNonImport)>
+                <Example uuid=(testNonImportExample)>
+                    <Description>
+                        DescriptionOne
+                    </Description>
+                </Example>
+                <Exit to=(testNonImportStub)>test exit</Exit>
+            </Room>
+            <Room uuid=(testNonImportStub) key=(testNonImportStub)>
+                <ShortName>StubOne</ShortName>
+            </Room>
+            <Room uuid=(testImportOne) key=(testImportOne) from=(ASSET#testImportAssetOne)>
+                <Example uuid=(testImportOneExample)>
+                    <Description>
+                        Two
+                    </Description>
+                </Example>
+                <Exit to=(testImportStubOne)>test exit one</Exit>
+            </Room>
+            <Room uuid=(testImportStubOne) key=(testImportStubOne)  from=(ASSET#testImportAssetOne) />
+            <Room uuid=(testImportFoo) key=(testImportFoo) from=(ASSET#testImportAssetOne) />
+            <Room uuid=(testImportTwo) key=(testImportTwo) />
+            <Room uuid=(testNonImportTwo) key=(testNonImportTwo)>
+                <Exit to=(testImportFoo)>test exit</Exit>
+            </Room>
+            <Room uuid=(testImportThree) key=(testImportThree) from=(ASSET#testImportAssetTwo) />
+            <Room uuid=(testRoomWithFeatures) key=(testRoomWithFeatures) from=(ASSET#testImportAssetFour)>
+                <Example uuid=(testRoomWithFeaturesExample)>
+                    <Description>
+                        <Link to=(featureImport)>Test</Link>
+                    </Description>
+                </Example>
+            </Room>
+            <Feature uuid=(testFeature) key=(featureImport) from=(ASSET#testImportAssetFour) />
+        </Asset>`)
+        const testImportOne = new StandardForm(`<Asset key=(testImportAssetOne)>
+            <Room uuid=(testImportOne)>
+                <Example uuid=(testImportOneExample)>
+                    <Description>
+                        One
+                    </Description>
+                </Example>
+            </Room>
+            <Room uuid=(testImportStubOne)>
+                <ShortName>StubTwo</ShortName>
+            </Room>
+            <Room uuid=(testImportFoo)>
+                <ShortName>StubFoo</ShortName>
+                <Example uuid=(testImportFooExample)>
+                    <Description>
+                        Foo
+                    </Description>
+                </Example>
+            </Room>
+        </Asset>`)
+        const testImportTwo = new StandardForm(`<Asset key=(testImportAssetTwo)>
+            <Room uuid=(testImportThree) key=(basic) from=(ASSET#testImportAssetThree)>
+                <Example uuid=(testImportTwoExample)>
+                    <Description>
+                        Asset Two
+                    </Description>
+                </Example>
+                <Exit to=(stub)>test exit</Exit>
+            </Room>
+            <Room uuid=(testStubOne) key=(stub) from=(ASSET#testImportAssetThree) />
+        </Asset>`)
+        const testImportThree = new StandardForm(`<Asset key=(testImportAssetThree)>
+            <Room uuid=(testImportThree) key=(basicOne)>
+                <Exit to=(stub)>test exit</Exit>
+            </Room>
+            <Room uuid=(testStubOne) key=(basicTwo)>
+                <ShortName>Asset Three</ShortName>
+            </Room>
+            <Room uuid=(stub) key=(stub)>
+                <ShortName>AssetThreeStub</ShortName>
+            </Room>
+        </Asset>`)
+        const testImportFour = new StandardForm(`<Asset key=(testImportAssetFour)>
+            <Feature uuid=(testFeature) key=(featureImport)>
+                <Example uuid=(testFeatureExample)>
+                    <Description>Feature test</Description>
+                </Example>
+            </Feature>
+            <Room uuid=(testRoomWithFeatures) key=(testRoomWithFeatures)>
+                <Example uuid=(testRoomWithFeaturesExample)>
+                    <Description><Link to=(testFeature)>Test</Link></Description>
+                </Example>
+            </Room>
+        </Asset>`)
         internalCacheMock.Graph.get.mockResolvedValue(new Graph<string, { key: string }, {}>({
             'ASSET#importTestOne': { key: 'ASSET#importTestOne' },
             'ASSET#importTestTwo': { key: 'ASSET#importTestTwo' },
@@ -32,30 +124,57 @@ describe('fetchImportsMessage', () => {
             { AssetId: 'ASSET#importTestThree', address: { zone: 'Canon', fileName: 'testThree' } },
             { AssetId: 'ASSET#importTestFour', address: { zone: 'Canon', fileName: 'testFour' } }
         ])
+        jest.spyOn(FetchImportsJSONHelper.prototype, 'get').mockImplementation(async (assetId: `ASSET#${string}`) => {
+            let standard: StandardForm = new StandardForm('test')
+            switch(assetId) {
+                case 'ASSET#testFinal':
+                    standard = testFinal
+                    break
+                case 'ASSET#testImportAssetOne':
+                    standard = testImportOne
+                    break
+                case 'ASSET#testImportAssetTwo':
+                    standard = testImportTwo
+                    break
+                case 'ASSET#testImportAssetThree':
+                    standard = testImportThree
+                    break
+                case 'ASSET#testImportAssetFour':
+                    standard = testImportFour
+                    break
+            }
+            return standard
+        })
     })
 
-    it('should pass inheritanceGraph to step function', async () => {
-        await fetchImportsMessage({
-            payloads: [{
-                type: 'FetchImports',
-                importsFromAsset: [
-                    { assetId: 'ASSET#importTestOne', keys: ['testOne', 'testTwo'] },
-                    { assetId: 'ASSET#importTestTwo', keys: ['testThree'] }
-                ]
-            }],
-            messageBus
-        })
-        expect(JSON.parse((sfnClientMock.send.mock.calls[0][0].input as any).input).inheritanceNodes).toEqual([
-            { key: 'ASSET#importTestOne', address: { zone: 'Canon', fileName: 'testOne' } },
-            { key: 'ASSET#importTestTwo', address: { zone: 'Canon', fileName: 'testTwo' } },
-            { key: 'ASSET#importTestThree', address: { zone: 'Canon', fileName: 'testThree' } },
-            { key: 'ASSET#importTestFour', address: { zone: 'Canon', fileName: 'testFour' } }
-        ])
-        expect(JSON.parse((sfnClientMock.send.mock.calls[0][0].input as any).input).inheritanceEdges).toEqual([
-            { from: 'ASSET#importTestOne', to: 'ASSET#importTestTwo' },
-            { from: 'ASSET#importTestTwo', to: 'ASSET#importTestThree' },
-            { from: 'ASSET#importTestTwo', to: 'ASSET#importTestFour' }
-        ])
+    it('should return empty when passed no keys', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: [] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
+    })
+
+    it('should return element and stubs when passed non-import key', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: ['ROOM#testNonImport'] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
+    })
+
+    it('should recursive fetch one level of element and stubs when passed import key', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: ['ROOM#testImportOne'] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
+    })
+
+    it('should follow dynamic renames in imports', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: ['ROOM#testNonImportTwo'] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
+    })
+
+    it('should import multilevel and avoid colliding stub names', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: ['ROOM#testImportThree'] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
+    })
+
+    it('should properly stub out features in room description', async () => {
+        await fetchImportsMessage({ payloads: [{ type: 'FetchImports', importsFromAsset: [{ assetId: 'ASSET#testFinal', keys: ['ROOM#testRoomWithFeatures'] }] }], messageBus })
+        expect(JSON.parse((snsClientMock.send.mock.calls[0][0].input as any).Message)).toMatchSnapshot()
     })
 
 })
--- lambda/assets/fetchImportDefaults/recursiveFetchImports.ts
+++ lambda/assets/fetchImportDefaults/recursiveFetchImports.ts
@@ -1,92 +1,73 @@
 import { FetchImportsJSONHelper } from "./baseClasses"
 import { StandardForm } from "@tonylb/mtw-wml/ts/standardize"
 import { excludeUndefined } from "@tonylb/mtw-utilities/ts/lists"
-import { ExportItemContent, ImportItemContent } from "@tonylb/mtw-wml/ts/standardize/components/metaData"
-import { unique } from "@tonylb/mtw-wml/ts/list"
-import { stripImportAndExport } from "./utils"
+import { AssetUUID, ComponentUUID } from "@tonylb/mtw-base/ts/schema"
+import { StandardKey } from "@tonylb/mtw-wml/ts/standardize/components/reference"
+import { mapKeyToFormat } from "@tonylb/mtw-wml/ts/standardize/components/utils/references"
 
 type RecursiveFetchImportArgument = {
-    assetId: `ASSET#${string}`;
+    assetId: AssetUUID;
     jsonHelper: FetchImportsJSONHelper;
-    fullKeys: string[];
-    stubKeys: string[];
+    fullKeys: ComponentUUID[];
+    stubKeys: ComponentUUID[];
+    removeLocalKeys?: boolean;
 }
 
-//
-// syntheticStubKeyFactory examines a StandardForm and creates the first key of the
-// form 'Stub####' that does not conflict with an already-existing key
-//
-const syntheticStubKeyFactory = (keysAlready: string[]): `Stub${string}` => {
-    let stubIndex = 1
-    while(keysAlready.includes(`Stub${stubIndex}`)) {
-        stubIndex++
-    }
-    return `Stub${stubIndex}`
-}
-
-export const recursiveFetchImports = async ({ assetId, jsonHelper, fullKeys, stubKeys }: RecursiveFetchImportArgument): Promise<StandardForm> => {
+export const recursiveFetchImports = async ({ assetId, jsonHelper, fullKeys, stubKeys, removeLocalKeys }: RecursiveFetchImportArgument): Promise<StandardForm> => {
     const standard = await jsonHelper.get(assetId)
-    if (!standard) {
-        return new StandardForm(`<Asset key=(${assetId.split('#').slice(1)[0]}) />`)
-    }
-    const keysByExportAs = Object.values(standard.byId)
-        .reduce<Record<string, string>>((previous, component) => {
-            const exportAs = component.export instanceof ExportItemContent ? component.export._exportAs : undefined
-            return {
-                ...previous,
-                [exportAs ?? component.key]: component.key
-            }
-        }, {})
-    const allImportsByAssetId = Object.values(standard.byId)
-        .reduce<Record<string, Record<string, string>>>((previous, component) => {
-            const importItem = component.import
-            if (importItem instanceof ImportItemContent) {
-                return {
-                    ...previous,
-                    [importItem.assetId]: {
-                        ...(previous[importItem.assetId] ?? { fullKeys: {}, stubKeys: {} }),
-                        [importItem.fromKey]: component.key
-                    }
-                }
-            }
-            return previous
-        }, {})
 
-    const fullKeysMapped = fullKeys.map((key) => (keysByExportAs[key])).filter(excludeUndefined)
-    const stubKeysMapped = stubKeys.map((key) => (keysByExportAs[key])).filter(excludeUndefined)
-    const newStandard = standard.subset([
+    const subsetStandard = standard.subset([
         {
             requestType: 'Full',
-            keys: fullKeysMapped,
+            keys: fullKeys.map((key) => (new StandardKey(key))),
             cascadeConditions: [
                 { conditionType: 'Exit', cascadeType: 'ShortName' },
                 { conditionType: 'Link', cascadeType: 'ShortName' },
                 { conditionType: 'Position', cascadeType: 'ShortName' }
             ]
         },
-        { requestType: 'ShortName', keys: stubKeysMapped }
+        { requestType: 'ShortName', keys: stubKeys.map((key) => (new StandardKey(key))) }
     ])
-    const allStubKeys = Object.keys(standard.byId).filter((key) => (!fullKeysMapped.includes(key)))
+
+    //
+    // If removeLocalKeys is true then remove all non-universal keys from the subsetStandard
+    // in order to create newStandard. Beyond the first level of recursion, we are not interested
+    // in the local keys, as they are not relevant to the import process.
+    //
+    const newStandard = subsetStandard
+    if (removeLocalKeys) {
+        const allKeys = newStandard._components
+            .map((component) => (component._key))
+        newStandard._components = newStandard._components
+            .map((component) => {
+                const returnValue = component.clone()
+                returnValue._key = mapKeyToFormat('universal')(returnValue._key)
+                return returnValue.withMapping(allKeys).remapReferences('universal')
+            })
+    }
+    const allStubKeys = standard._components
+        .filter((component) => (!fullKeys.find((checkKey) => (new StandardKey(checkKey).equals(component._key)))))
+        .map((component) => (component.universalKey))
+        .filter(excludeUndefined)
 
     //
     // Check all components in the subset standardForm, and see whether they require imports. From
-    // that examination, make a record by AssetId of:
-    //    (a) What fullKeys and stubKeys need to be imported from the ancestor Asset, and
-    //    (b) How to map those keys (when received) to the names that they have in the child standardForm
+    // that examination, make a record by AssetId of what fullKeys and stubKeys need to be imported
+    // from the ancestor Asset
     //
-    const relevantImportsByAssetId = Object.values(newStandard.byId)
-        .reduce<Record<string, { fullKeys: Record<string, string>; stubKeys: Record<string, string> }>>((previous, component) => {
-            const importItem = component.import
-            if (importItem instanceof ImportItemContent) {
-                const fullOrStub = allStubKeys.includes(component.key) ? 'stubKeys' : 'fullKeys'
+    const relevantImportsByAssetId = newStandard._components
+        .reduce<Record<AssetUUID, { fullKeys: ComponentUUID[]; stubKeys: ComponentUUID[] }>>((previous, component) => {
+            const importItem = component._from
+            if (importItem) {
+                const fullOrStub = (component.universalKey && allStubKeys.includes(component.universalKey)) ? 'stubKeys' : 'fullKeys'
                 return {
                     ...previous,
-                    [importItem.assetId]: {
-                        ...(previous[importItem.assetId] ?? { fullKeys: {}, stubKeys: {} }),
-                        [fullOrStub]: {
-                            ...(previous[importItem.assetId]?.[fullOrStub] ?? {}),
-                            [importItem.fromKey]: component.key
-                        }
+                    [importItem]: {
+                        ...(previous[importItem] ?? { fullKeys: [], stubKeys: [] }),
+                        [fullOrStub]: [
+                            ...(previous[importItem]?.[fullOrStub] ?? []),
+                            component.universalKey
+                        ]
                     }
                 }
             }
@@ -99,119 +80,24 @@ export const recursiveFetchImports = async ({ assetId, jsonHelper, fullKeys, stu
     //
     const recursiveImports = (await Promise.all(Object.entries(relevantImportsByAssetId)
             .map(([assetId, { fullKeys, stubKeys }]) => (
-                recursiveFetchImports({ assetId: `ASSET#${assetId}`, jsonHelper, fullKeys: Object.keys(fullKeys), stubKeys: Object.keys(stubKeys) })
+                recursiveFetchImports({ assetId: assetId as AssetUUID, jsonHelper, fullKeys, stubKeys, removeLocalKeys: true })
             ))
         ))
 
     //
-    // TODO: Merge all localized imports forward to the current level
+    // Merge all localized imports forward to the current level
     //
+    newStandard._components.forEach((component) => { component._from = undefined })
     const merged = recursiveImports.reduce<StandardForm>(
         (previous, incoming) => {
-            //
-            // Translate internal keys in the recursive imports into local namespace, resolving name collisions (which can only
-            // happen with stub-keys) by replacing them with "Stub###" keys
-            //
-            const relevantImportMapping = relevantImportsByAssetId[incoming.key]
-            if (!relevantImportMapping) {
-                //
-                // Somehow the import failed to register, so ignore
-                //
-                return previous
-            }
-            const { fullKeys, stubKeys } = relevantImportMapping
-            //
-            // First, find all the entries in the imported asset which are fulfilling import requests from the
-            // main asset, and create renameKey arguments for those fields (possibly no-ops) to record how to
-            // rename those components in the incoming StandardForm, in order to be able to merge it with
-            // the results.
-            //
-            const translationRenamesForImports = [...Object.entries(fullKeys), ...Object.entries(stubKeys)].reduce<Parameters<StandardForm["renameKey"]>[0]>((accumulator, [exportKey, localKey]) => {
-                const matchComponent = Object.values(incoming.byId).find((component) => {
-                    if (component.export instanceof ExportItemContent) {
-                        return component.export.exportAs === exportKey
-                    }
-                    else {
-                        return component.key === exportKey
-                    }
-                })
-                if (!matchComponent) {
-                    return accumulator
-                }
-                return [
-                    ...accumulator,
-                    {
-                        fromKey: matchComponent.key,
-                        toKey: localKey
-                    }
-                ]
-            }, [])
-
-            //
-            // Next, search the incoming keys for those that are *not* yet represented in the map (which must be stubs
-            // generated by internal references in the import hierarchy). Those (a) can be named whatever is suitable,
-            // and (b) might currently confict with existing names in the results. Add a renameKey argument for each
-            // of these fields as well, either a no-op, or a rename to a synthetic key that doesn't conflict.
-            //
-            const keysMappedToImports = translationRenamesForImports.map(({ fromKey }) => (fromKey))
-            const translationRenamesForStubs = Object.values(incoming.byId)
-                .filter((component) => (!keysMappedToImports.includes(component.key)))
-                .reduce<Parameters<StandardForm["renameKey"]>[0]>((accumulator, component) => {
-                    //
-                    // If a stub is already something that the asset imports, use the name it is imported under
-                    //
-                    const exportAs = component.export instanceof ExportItemContent ? component.export.exportAs : component.key
-                    if (allImportsByAssetId[incoming.key][exportAs]) {
-                        return [
-                            ...accumulator,
-                            {
-                                fromKey: component.key,
-                                toKey: allImportsByAssetId[incoming.key][exportAs]
-                            }
-                        ]
-                    }
-                    //
-                    // Otherwise, add a new name that doesn't conflict, using the original name by preference,
-                    // and creating a synthetic stub as needed
-                    //
-                    const currentKeys = unique(
-                        Object.keys(previous.byId),
-                        translationRenamesForImports.map(({ toKey }) => (toKey)),
-                        accumulator.map(({ toKey }) => (toKey))
-                    )
-                    if (currentKeys.includes(component.key)) {
-                        return [
-                            ...accumulator,
-                            {
-                                fromKey: component.key,
-                                toKey: syntheticStubKeyFactory(currentKeys)
-                            }
-                        ]
-                    }
-                    else {
-                        return [
-                            ...accumulator,
-                            {
-                                fromKey: component.key,
-                                toKey: component.key
-                            }
-                        ]
-                    }
-                }, [])
-
-            //
-            // Finally, remove the no-ops, rename keys on the incoming StandardForm, and merge the results
-            // into it.
-            //
-            const finalRenames = [...translationRenamesForImports, ...translationRenamesForStubs]
-                .filter(({ fromKey, toKey }) => (fromKey !== toKey))
-            return stripImportAndExport(incoming.renameKey(finalRenames)).merge(previous)
+            incoming._components.forEach((component) => { component._from = undefined })
+            return incoming.merge(previous)
         },
         newStandard
     )
 
-    newStandard._byId = merged.byId
-    return newStandard
+    newStandard._components = merged._components
+    return newStandard.finalize()
 
 }
 
