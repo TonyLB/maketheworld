202514ce93f0e32da87ec4a6ef70f7c787309c9c
TonyLB <anthony.lowerbasch@gmail.com>
2025-07-24 07:42:38 -0400

    ISS5868: Updated primitives asset to use UUIDs rather than keys

--- lambda/initialize/primitives.ts
+++ lambda/initialize/primitives.ts
@@ -1,7 +1,7 @@
 export const primitivesData = `
     <Asset key=(primitives)>
-        <Room key=(VORTEX) />
-        <Knowledge key=(knowledgeRoot) />
+        <Room uuid=(VORTEX) />
+        <Knowledge uuid=(knowledgeRoot) />
     </Asset>
 `
 
