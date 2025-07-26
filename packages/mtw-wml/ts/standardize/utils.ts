6730fa85af27bf97d349cde9b2eef5c484754bcb
TonyLB <anthony.lowerbasch@gmail.com>
2025-07-26 10:21:25 -0400

    ISS5870: Deprecated removeNDJSONOnlyProperties helper function

--- packages/mtw-wml/ts/standardize/utils.ts
+++ packages/mtw-wml/ts/standardize/utils.ts
@@ -1,11 +1,1 @@
-import { SerializeNDJSONMixin, StandardComponentData } from "./baseClasses"
-
 export const isLegalKey = (value: string) => (value.match(/^[a-zA-Z\_][a-zA-Z0-9\_\.]+$/))
-
-export const removeNDJSONOnlyProperties = (props: StandardComponentData & SerializeNDJSONMixin): Omit<StandardComponentData & SerializeNDJSONMixin, 'universalKey' | 'from' | 'exportAs'> => {
-    return Object.assign({}, 
-        ...Object.entries(props)
-            .filter(([key]) => (!['universalKey', 'from', 'exportAs'].includes(key)))
-            .map(([key, value]) => ({ [key]: value }))
-    )
-}
