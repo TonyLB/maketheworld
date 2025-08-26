59cf01a388390d23075f460a5aa0f3cacb62a63f
TonyLB <anthony.lowerbasch@gmail.com>
2025-08-26 08:27:06 -0400

    ISS5982: Removed obsolete fetchImportDefaults from WML lambda

--- lambda/wml/app.ts
+++ lambda/wml/app.ts
@@ -1,9 +1,3 @@
-import { AssetWorkspaceAddress } from "@tonylb/mtw-asset-workspace"
-import { PublishCommand } from '@aws-sdk/client-sns'
-import { snsClient } from './clients';
-import { EphemeraAssetId } from '@tonylb/mtw-interfaces/ts/baseClasses';
-import { Graph } from '@tonylb/mtw-utilities/dist/graphStorage/utils/graph';
-import { fetchImports } from './fetchImportDefaults';
 import { parseWMLHandler } from './parseWML'
 import copyWML from './copyWML';
 import { resetWML } from './resetWML';
@@ -12,43 +6,6 @@ import applyEdit from "./applyEdit";
 import { checkLock, requestLock, yieldAtomicLock } from "./atomicLock";
 import delayPromise from "@tonylb/mtw-utilities/ts/dynamoDB/delayPromise";
 import internalCache from "./internalCache";
-import { ComponentUUID } from "@tonylb/mtw-base/ts/schema";
-
-const { FEEDBACK_TOPIC } = process.env
-
-type FetchImportsHandlerArguments = {
-    ConnectionId: string;
-    RequestId: string;
-    inheritanceNodes: { key: EphemeraAssetId; address: AssetWorkspaceAddress }[];
-    inheritanceEdges: { from: EphemeraAssetId; to: EphemeraAssetId }[];
-    payloads: { assetId: EphemeraAssetId; keys: ComponentUUID[] }[];
-}
-
-const fetchImportsHandler = async (event: FetchImportsHandlerArguments) => {
-    try {
-        const inheritanceGraph = new Graph<EphemeraAssetId, { key: EphemeraAssetId; address: AssetWorkspaceAddress }, {}>(Object.assign({}, ...event.inheritanceNodes.map(({ key, address }) => ({ [key]: { key, address } }))), event.inheritanceEdges, { address: {} as any })
-        return await fetchImports({
-            ConnectionId: event.ConnectionId,
-            RequestId: event.RequestId,
-            inheritanceGraph,
-            payloads: event.payloads
-        })
-    }
-    catch (error) {
-        await snsClient.send(new PublishCommand({
-            TopicArn: FEEDBACK_TOPIC,
-            Message: '{}',
-            MessageAttributes: {
-                RequestId: { DataType: 'String', StringValue: event.RequestId },
-                ConnectionIds: { DataType: 'String.Array', StringValue: JSON.stringify([event.ConnectionId]) },
-                Type: { DataType: 'String', StringValue: 'Error' },
-                Error: { DataType: 'String', StringValue: 'Internal error in FetchImports' }
-            }
-        }))
-        console.log(error)
-        throw error
-    }
-}
 
 export const handler = async (event: any) => {
 
@@ -71,8 +28,6 @@ export const handler = async (event: any) => {
             else {
                 return await resetWML(event)
             }
-        case 'fetchImports':
-            return await fetchImportsHandler(event)
         case 'requestLock':
             const lock = await requestLock(event.AssetId)
             return await checkLock(event.AssetId, lock)
