c6bf2aca0abac4d267af18e7da6f6ed4fa8c8db8
TonyLB <anthony.lowerbasch@gmail.com>
2026-01-03 17:48:45 -0500

    ISS6697: Added StandardMarkData

---
+++ packages/mtw-wml/ts/standardize/components/dataTypes/mark.ts
@@ -0,0 +1,27 @@
+import { RenderTree } from "@tonylb/mtw-base/ts/renderTree"
+import { StandardBaseData } from "./abstract"
+import { checkAll, checkTypes } from "./typeguards"
+import { StandardEditableData } from "@tonylb/mtw-base/ts/editable"
+
+export type StandardMarkData = {
+    tag: 'Mark';
+    shortName?: StandardEditableData<string>;
+    description?: RenderTree;
+} & StandardBaseData
+
+export const isStandardMarkData = (arg: any): arg is StandardMarkData => {
+    if (typeof arg !== 'object') {
+        return false
+    }
+
+    return checkAll(
+        ('tag' in arg && arg.tag === 'Mark'),
+        checkTypes(arg, {},
+        {
+            key: 'key',
+            universalKey: 'string',
+            shortName: 'literal',
+            description: 'renderTree'
+        })
+    )
+}
