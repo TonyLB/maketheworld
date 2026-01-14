6eaf696ceab009352f5928adf1cbcbbe01868947
TonyLB <anthony.lowerbasch@gmail.com>
2026-01-14 08:22:00 -0500

    ISS6782: Cleaned up obsolete schema utility functions

--- packages/mtw-wml/ts/schema/utils/index.ts
+++ packages/mtw-wml/ts/schema/utils/index.ts
@@ -1,26 +1,18 @@
-import { isSchemaTaggedMessageLegalContents, SchemaTag, SchemaTaggedMessageLegalContents } from "@tonylb/mtw-base/ts/schema"
+import { SchemaTag } from "@tonylb/mtw-base/ts/schema"
 import { EditWrappedStandardNode } from "../../standardize/baseClasses"
-import { GenericTree, GenericTreeNode, GenericTreeNodeFiltered, treeNodeTypeguard } from "@tonylb/mtw-base/ts/genericTree"
-import { isSchemaDescription, isSchemaName } from "@tonylb/mtw-base/ts/schema/example"
-import { isSchemaEdit, isSchemaRemove, isSchemaReplace, SchemaRemoveTag } from "@tonylb/mtw-base/ts/schema/edit"
+import { GenericTreeNode, treeNodeTypeguard } from "@tonylb/mtw-base/ts/genericTree"
+import { isSchemaEdit } from "@tonylb/mtw-base/ts/schema/edit"
 
-export const extractNameFromContents = (contents: GenericTree<SchemaTag>): GenericTree<SchemaTag> => {
-    return contents.map((item) => {
-        if (isSchemaName(item.data)) {
-            return item.children.filter(({ data }) => (isSchemaTaggedMessageLegalContents(data)))
-        }
-        return []
-    }).flat(1)
-}
-
-export const extractDescriptionFromContents = (contents: GenericTree<SchemaTag>): GenericTree<SchemaTag> => {
-    const returnValue = contents.map((item) => {
-        if (isSchemaDescription(item.data)) {
-            return item.children.filter(({ data }) => (isSchemaTaggedMessageLegalContents(data)))
-        }
-        return []
-    }).flat(1)
-    return returnValue
+//
+// unwrapSubject takes a schema node that might be a replace or remove, and returns the first tag in the tree hierarchy
+// that is *not* an edit tag (i.e., the subject content being edited)
+//
+export const unwrapSubject = <Extra extends {}>(node: GenericTreeNode<SchemaTag> | undefined): GenericTreeNode<SchemaTag> | undefined => {
+    if (!node) { return undefined }
+    if (treeNodeTypeguard(isSchemaEdit)(node)) {
+        return unwrapSubject<Extra>(node.children[0])
+    }
+    return node
 }
 
 //
@@ -45,22 +37,6 @@ export const deIndentWML = (wml: string): string => {
         .join('\n')
 }
 
-//
-// unwrapSubject takes a schema node that might be a replace or remove, and returns the first tag in the tree hierarchy
-// that is *not* an edit tag (i.e., the subject content being edited)
-//
-export const unwrapSubject = <Extra extends {}>(node: GenericTreeNode<SchemaTag> | undefined): GenericTreeNode<SchemaTag> | undefined => {
-    if (!node) { return undefined }
-    if (treeNodeTypeguard(isSchemaEdit)(node)) {
-        return unwrapSubject<Extra>(node.children[0])
-    }
-    return node
-}
-
-//
-// unwrapSubject takes a schema node that might be a replace or remove, and returns the first tag in the tree hierarchy
-// that is *not* an edit tag (i.e., the subject content being edited)
-//
 export const wrappedNodeTypeGuard = <SubType extends SchemaTag>(typeGuard: (value: SchemaTag) => value is SubType) => (node: GenericTreeNode<SchemaTag>): node is EditWrappedStandardNode<SubType, SchemaTag> => {
     if (treeNodeTypeguard(isSchemaEdit)(node)) {
         return node.children.reduce((previous, child) => (previous && wrappedNodeTypeGuard<SubType>(typeGuard)(child)), true)
@@ -68,64 +44,6 @@ export const wrappedNodeTypeGuard = <SubType extends SchemaTag>(typeGuard: (valu
     else {
         return typeGuard(node.data)
     }
-}
-
-export const ignoreWrapped = <F extends SchemaTag, C extends SchemaTag>(node: EditWrappedStandardNode<F, C> | undefined): GenericTreeNodeFiltered<F, C> | undefined => {
-    if (!node) { return undefined  }
-    if (treeNodeTypeguard(isSchemaReplace)(node) || treeNodeTypeguard(isSchemaRemove)(node)) {
-        const subject = unwrapSubject(node)
-        if (!subject) {
-            throw new Error('No subject in ignoreWrapped')
-        }
-        return { data: subject.data as F, children: [] }
-    }
-    return node as unknown as GenericTreeNodeFiltered<F, C>
-}
-
-//
-// RecursiveRemoveWrapped represents a node that may be recursively wrapped in Remove tags
-//
-export type RecursiveRemoveWrapped<V extends SchemaTag> = 
-    | GenericTreeNodeFiltered<V, SchemaTag>
-    | { data: SchemaRemoveTag, children: RecursiveRemoveWrapped<V>[] }
-
-//
-// filterEditableTree filters a schema tree to find nodes matching a typeguard,
-// preserving any Remove wrappers around those nodes
-//
-export const filterEditableTree = <V extends SchemaTag>({ tree, typeguard }: { tree: GenericTree<SchemaTag>; typeguard: (node: GenericTreeNode<SchemaTag>) => node is GenericTreeNodeFiltered<V, SchemaTag> }): RecursiveRemoveWrapped<V>[] => {
-    const result: RecursiveRemoveWrapped<V>[] = []
-    
-    for (const node of tree) {
-        // If this is a Remove node, recursively process its children
-        if (treeNodeTypeguard(isSchemaRemove)(node)) {
-            const filteredChildren = filterEditableTree({ tree: node.children, typeguard })
-            if (filteredChildren.length > 0) {
-                result.push({
-                    data: { tag: 'Remove' as const },
-                    children: filteredChildren
-                })
-            }
-        }
-        // If this node matches the typeguard directly, include it
-        else if (typeguard(node)) {
-            result.push(node)
-        }
-    }
-    
-    return result
-}
-
-export const stripTagFromTree = (tree: GenericTree<SchemaTag>, tag: SchemaTag["tag"]): GenericTree<SchemaTag> => {
-    return tree.map((node) => {
-        if (node.data.tag === tag) {
-            return stripTagFromTree(node.children, tag)
-        }
-        return [{
-            data: node.data,
-            children: stripTagFromTree(node.children, tag)
-        }]
-    }).flat(1)
 }
 
 export { findTaggedChildren } from './findTaggedChildren'
