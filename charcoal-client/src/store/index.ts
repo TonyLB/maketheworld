e9b3d8a0bb7ae220bdb99e1492eb4e9da233bc9c
TonyLB <anthony.lowerbasch@gmail.com>
2026-02-09 22:01:21 -0500

    ISS7029: Added wmlDataSource slice to client

--- charcoal-client/src/store/index.ts
+++ charcoal-client/src/store/index.ts
@@ -14,6 +14,7 @@ import perceptionCache from '../slices/perceptionCache'
 import { contentHeadersSlice, subscribeToContentHeaders } from '../slices/contentHeaders'
 import { libraryDataSourceSlice, subscribeToLibrary } from '../slices/libraryDataSource'
 import { playerDataSourceSlice } from '../slices/player/playerDataSource'
+import { wmlDataSourceSlice } from '../slices/wmlDataSource'
 
 export const store = configureStore({
     reducer: {
@@ -29,7 +30,8 @@ export const store = configureStore({
         perceptionCache,
         contentHeaders: contentHeadersSlice.reducer,
         libraryDataSource: libraryDataSourceSlice.reducer,
-        playerDataSource: playerDataSourceSlice.reducer
+        playerDataSource: playerDataSourceSlice.reducer,
+        wmlDataSource: wmlDataSourceSlice.reducer
     },
     middleware: [thunk]
 })
